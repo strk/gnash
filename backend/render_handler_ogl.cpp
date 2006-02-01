29dc56ab57bfa08cd2f2765a235c9eece0ee77f1
corfe <>
2006-02-01 05:22:15 +0000

    Tentative first implementation of line_style_width, with appropriate TODO message

--- backend/render_handler_ogl.cpp
+++ backend/render_handler_ogl.cpp
@@ -521,7 +521,9 @@ struct render_handler_ogl : public gnash::render_handler
 	
 	void	line_style_width(float width)
 	{
-		// WK: what to do here???
+		// TODO: OpenGL doesn't seem to handle very low-width lines well, even with anti-aliasing enabled
+		// But this is a start (20 TWIPS' width = 1 pixel's)
+		glLineWidth(width/20.0f);
 	}
 
 
