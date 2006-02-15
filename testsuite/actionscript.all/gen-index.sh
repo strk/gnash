5b9fe6a77ae8e65c4a350fa38d4dd43dbfd176ca
strk <>
2006-02-15 10:58:41 +0000

    Also generate wiki page for testcases

--- testsuite/actionscript.all/gen-index.sh
+++ testsuite/actionscript.all/gen-index.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+## generate index.html
 echo "<html><head><title>Test units</title></head><body>" > index.html
 for t in $@; do 
 	echo "<li><a href=$t>$t</a></li>" >> index.html; 
 done; 
 echo "</body></html>" >> index.html
+
+## generate list for the wiki
+echo > index.wiki
+for t in $@; do 
+	testname=`basename $t .swf`
+	{
+	echo "== $testname =="
+	echo 
+	echo "[http://www.gnu.org/software/gnash/testcases/v5/$t SWF5] "
+	echo "[http://www.gnu.org/software/gnash/testcases/v6/$t SWF6] "
+	echo "[http://www.gnu.org/software/gnash/testcases/v7/$t SWF7] "
+	echo "[http://www.gnu.org/software/gnash/testcases/v8/$t SWF8]"
+	echo 
+	} >> index.wiki
+done; 
