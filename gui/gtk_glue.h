dbafd9966f7c848512c7e801fff550662b2ed74b
bjacques <>
2006-05-11 23:18:56 +0000

    * configure.ac: Use GTK as the default GUI.         * gui/gtk_glue.h, gui/gtk_glue_cairo.{cpp,h},         gui/gtk_glue_gtkglext.{cpp,h}: Introduce an abstraction class         for Gtkglext and "GtkCairo" which connect (glue) GTK to the OpenGL         and Cairo renderers, respectively.         * gui/Makefile.am: Build the appropriate glue. 	* gui/README: Deprecate the SDL-based Firefox plugin. 	* gui/{gnash.cpp,gui.h,sdl.cpp}: No longer handle the OpenGL LOD bias 	parameter. It is now handled in the glue class. 	* gui/{gtk.cpp,gtksup.h}: Migrate to the new glue. 	* gui/gui.h: No longer handle the OpenGL LOD bias parameter. 	* gui/sdl.cpp: Scale mouse event coordinates according to the movie 	scale.

---
+++ gui/gtk_glue.h
@@ -0,0 +1,22 @@
+#include "gnash.h"
+
+#include <gtk/gtk.h>
+#include <gdk/gdkx.h>
+
+namespace gnash
+{
+
+class GtkGlue
+{
+  public:
+    virtual ~GtkGlue() { };
+    virtual bool init(int argc, char **argv[]) = 0;
+
+    virtual void prepDrawingArea(GtkWidget *drawing_area) = 0;
+    virtual render_handler* createRenderHandler() = 0;
+    virtual void render() = 0;
+  protected:
+    GtkWidget *_drawing_area;
+};
+
+} // namespace gnash
---
+++ gui/gtk_glue_gtkglext.cpp
@@ -0,0 +1,139 @@
+//
+//   Copyright (C) 2005, 2006 Free Software Foundation, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// Linking Gnash statically or dynamically with other modules is making
+// a combined work based on Gnash. Thus, the terms and conditions of
+// the GNU General Public License cover the whole combination.
+//
+// In addition, as a special exception, the copyright holders of Gnash give
+// you permission to combine Gnash with free software programs or
+// libraries that are released under the GNU LGPL and/or with Mozilla, 
+// so long as the linking with Mozilla, or any variant of Mozilla, is
+// through its standard plug-in interface. You may copy and distribute
+// such a system following the terms of the GNU GPL for Gnash and the
+// licenses of the other code concerned, provided that you include the
+// source code of that other code when and as the GNU GPL requires
+// distribution of source code. 
+//
+// Note that people who make modified versions of Gnash are not obligated
+// to grant this special exception for their modified versions; it is
+// their choice whether to do so.  The GNU General Public License gives
+// permission to release a modified version without this exception; this
+// exception also makes it possible to release a modified version which
+// carries forward this exception.
+//
+
+#include "gtk_glue_gtkglext.h"
+#include "log.h"
+
+
+using namespace std;
+
+
+namespace gnash
+{
+
+GtkGlExtGlue::GtkGlExtGlue()
+#ifdef FIX_I810_LOD_BIAS
+  : tex_lod_bias(-1.2f)
+#endif
+{
+}
+
+GtkGlExtGlue::~GtkGlExtGlue()
+{
+}
+
+bool
+GtkGlExtGlue::init(int argc, char **argv[])
+{
+#ifdef FIX_I810_LOD_BIAS
+    int c = getopt (argc, argv, "m:");
+    if (c == 'm') {
+      _tex_lod_bias = (float) atof(optarg);
+    }
+#endif
+
+    gtk_gl_init (&argc, argv);
+
+    gint major, minor;
+    gdk_gl_query_version (&major, &minor);
+    dbglogfile << "OpenGL extension version - "
+              << (int)major << "." << (int)minor << endl;
+
+    GdkGLConfigMode glcmode = (GdkGLConfigMode)(GDK_GL_MODE_RGB |
+                                                GDK_GL_MODE_DEPTH |
+                                                GDK_GL_MODE_DOUBLE);
+    _glconfig = gdk_gl_config_new_by_mode (glcmode);
+
+    if (!_glconfig) {
+      dbglogfile << "Cannot find the double-buffered visual." << endl;
+      dbglogfile << "Trying single-buffered visual." << endl;
+
+      glcmode = (GdkGLConfigMode)(GDK_GL_MODE_RGB | GDK_GL_MODE_DEPTH);
+      _glconfig = gdk_gl_config_new_by_mode (glcmode);
+      if (!_glconfig) {
+        dbglogfile << "No appropriate OpenGL-capable visual found." << endl;
+        gtk_main_quit(); // XXX
+      } else {
+        dbglogfile << "Got single-buffered visual." << endl;
+      }
+    } else {
+      dbglogfile << "Got double-buffered visual." << endl;
+    }
+
+    return true;
+}
+
+
+void
+GtkGlExtGlue::prepDrawingArea(GtkWidget *drawing_area)
+{
+    _drawing_area = drawing_area;
+    gtk_widget_set_gl_capability(_drawing_area, _glconfig,
+                                 NULL, TRUE, GDK_GL_RGBA_TYPE);
+}
+
+render_handler*
+GtkGlExtGlue::createRenderHandler()
+{
+    GdkGLContext *glcontext = gtk_widget_get_gl_context (_drawing_area);
+    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (_drawing_area);
+
+    // Attach our OpenGL context to the drawing_area.
+    gdk_gl_drawable_make_current(gldrawable, glcontext);
+
+    render_handler* renderer = create_render_handler_ogl();
+
+#ifdef FIX_I810_LOD_BIAS
+    glTexEnvf(GL_TEXTURE_FILTER_CONTROL_EXT, GL_TEXTURE_LOD_BIAS_EXT, _tex_lod_bias);
+#endif
+  return renderer;
+}
+
+void
+GtkGlExtGlue::render()
+{
+    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (_drawing_area);
+    if (gdk_gl_drawable_is_double_buffered (gldrawable)) {
+        gdk_gl_drawable_swap_buffers (gldrawable);
+    } else {
+      glFlush();
+    }
+}
+
+
+}
