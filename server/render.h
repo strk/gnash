b36bcd3a66e7432a44666d16158132a89a81bec2
strk <>
2006-05-25 10:08:36 +0000

    * server/render.h: doxygen comments.

--- server/render.h
+++ server/render.h
@@ -16,19 +16,30 @@
 
 
 namespace gnash {
+
+	/// Return currently registered render handler
 	render_handler*	get_render_handler();
 
+	/// Rendering operations 
+	//
+	/// Takes care of calling the currently registered
+	/// gnash::render_handler or fallback on a default
+	/// behaviour if no renderer is registered
+	///
 	namespace render
 	{
 		bitmap_info*	create_bitmap_info_empty();
-		bitmap_info*	create_bitmap_info_alpha(int w, int h, unsigned char* data);
+		bitmap_info*	create_bitmap_info_alpha(int w, int h,
+					unsigned char* data);
 		bitmap_info*	create_bitmap_info_rgb(image::rgb* im);
 		bitmap_info*	create_bitmap_info_rgba(image::rgba* im);
 		void	delete_bitmap_info(bitmap_info* bi);
 
-		// Bracket the displaying of a frame from a movie.
-		// Fill the background color, and set up default
-		// transforms, etc.
+		/// \brief
+		/// Bracket the displaying of a frame from a movie.
+		/// Fill the background color, and set up default
+		/// transforms, etc.
+		///
 		void	begin_display(
 			rgba background_color,
 			int viewport_x0, int viewport_y0,
@@ -40,24 +51,30 @@ namespace gnash {
 		void	set_matrix(const matrix& m);
 		void	set_cxform(const cxform& cx);
 
-		// Draw triangles using the current fill-style 0.
-		// Clears the style list after rendering.
+		/// \brief
+		/// Draw triangles using the current fill-style 0.
+		/// Clears the style list after rendering.
 		//
-		// coords is a list of (x,y) coordinate pairs, in
-		// triangle-strip order.  The type of the array should
-		// be float[vertex_count*2]
-		void	draw_mesh_strip(const int16_t coords[], int vertex_count);
+		/// coords is a list of (x,y) coordinate pairs, in
+		/// triangle-strip order.  The type of the array should
+		/// be float[vertex_count*2]
+		void	draw_mesh_strip(const int16_t coords[],
+				int vertex_count);
 
-		// Draw a line-strip using the current line style.
-		// Clear the style list after rendering.
+		/// \brief
+		/// Draw a line-strip using the current line style.
+		/// Clear the style list after rendering.
 		//
-		// Coords is a list of (x,y) coordinate pairs, in
-		// sequence.
-		void	draw_line_strip(const int16_t coords[], int vertex_count);
+		/// Coords is a list of (x,y) coordinate pairs, in
+		/// sequence.
+		void	draw_line_strip(const int16_t coords[],
+				int vertex_count);
 
 		void	fill_style_disable(int fill_side);
 		void	fill_style_color(int fill_side, rgba color);
-		void	fill_style_bitmap(int fill_side, const bitmap_info* bi, const matrix& m, render_handler::bitmap_wrap_mode wm);
+		void	fill_style_bitmap(int fill_side, const bitmap_info* bi,
+				const matrix& m,
+				render_handler::bitmap_wrap_mode wm);
 
 		void	line_style_disable();
 		void	line_style_color(rgba color);
@@ -67,12 +84,16 @@ namespace gnash {
 		void	end_submit_mask();
 		void	disable_mask();
 
-		// Special function to draw a rectangular bitmap;
-		// intended for textured glyph rendering.  Ignores
-		// current transforms.
-		void	draw_bitmap(const matrix& m, const bitmap_info* bi, const rect& coords, const rect& uv_coords, rgba color);
+		/// Special function to draw a rectangular bitmap;
+		//
+		/// intended for textured glyph rendering.  Ignores
+		/// current transforms.
+		void	draw_bitmap(const matrix& m, const bitmap_info* bi,
+				const rect& coords,
+				const rect& uv_coords, rgba color);
 
 	};	// end namespace render
+
 };	// end namespace gnash
 
 
