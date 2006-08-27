dd725812120a33ce16d0efa5adb8987b73d15ed3
nihilus <>
2006-08-27 15:28:02 +0000

    Yeah, free the mem pointed to...

--- server/parser/movie_def_impl.cpp
+++ server/parser/movie_def_impl.cpp
@@ -386,27 +386,23 @@ movie_def_impl::movie_def_impl(create_bitmaps_flag cbf,
 movie_def_impl::~movie_def_impl()
 {
     // Release our playlist data.
-    /*{for (int i = m_playlist.size(); i != 0; i--) // Optimized
+    {for (int i = m_playlist.size(); i != 0; i--) // Optimized
         {
             for (int j = m_playlist[i].size(); j != 0; j--)
                 {
                     delete m_playlist[i][j];
                 }
-        }}*/
-
-	m_playlist.clear();
+        }}
 	
     // Release init action data.
-    /*{for (size_t i = m_init_action_list.size(); i != 0; i--) //Optimized
+    {for (size_t i = m_init_action_list.size(); i != 0; i--) //Optimized
         {
             for (size_t j = m_init_action_list[i].size(); j != 0; j--)
                 {
                     delete m_init_action_list[i][j];
                 }
-        }}*/
-	
-	m_init_action_list.clear();
-	
+        }}
+		
 	// It's supposed to be cleaned up in read()
 	// TODO: join with loader thread instead ?
 	//assert(m_jpeg_in.get() == NULL);
