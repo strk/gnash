d953f10a3dbb5cf1c9ba3f0fec3aeab6f511a134
nihilus <>
2006-08-29 15:56:34 +0000

    *** empty log message ***

--- libbase/container.h
+++ libbase/container.h
@@ -623,7 +623,7 @@ private:
 };	// WIN32 hash end
 #else
 
-#include <hash_map>
+#include <ext/hash_map>
 template<class T, class U, class hash_functor = fixed_size_hash<T> >
 class hash : public __gnu_cxx::hash_map<T, U, hash_functor >
 {
