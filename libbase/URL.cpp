398e0b6853f1127979d2459534272b4a17a39740
nihilus <>
2006-09-14 00:57:57 +0000

    Reverted, if it's not broken don't tweak it!

--- libbase/URL.cpp
+++ libbase/URL.cpp
@@ -158,8 +158,7 @@ URL::URL(const string& absolute_url)
 		*ptr = '/';
 		++ptr;
 		*ptr = '\0';
-		set_workdir(buf);
-		URL cwd(get_workdir());
+		URL cwd(buf);
 		init_relative(absolute_url, cwd);
 	}
 }
