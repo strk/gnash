c93b2d9b58604ffbe8b1cb67eff21c80f23bdd03
bjacques <>
2006-09-17 18:28:57 +0000

    Don't treat local filenames as query strings (bug #17725).

--- libbase/URL.cpp
+++ libbase/URL.cpp
@@ -380,6 +380,14 @@ URL::split_querystring_from_path()
 		return;
 	}
 
+	size_t swfpos = _path.rfind(".swf");
+	if (swfpos != string::npos && swfpos > qmpos)
+	{
+		// If the questionmark precedes the file suffix, we're not dealing with a 
+		// query string.
+		return;
+	}
+
 	_querystring = _path.substr(qmpos+1);
 
 	// update _path
