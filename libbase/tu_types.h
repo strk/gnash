141fcb9fcaddac8fc4f079e01dc526384c5ee3a9
nihilus <>
2006-09-21 05:41:11 +0000

    __NetBSD__

--- libbase/tu_types.h
+++ libbase/tu_types.h
@@ -69,7 +69,7 @@ typedef __int64 int64_t;
 #endif
 
 #ifndef BYTE_ORDER
-#if defined(__sgi) || defined(SGI) || defined(__sgi__) || defined (__FreeBSD__) || defined (__FreeBSD) || defined (__OpenBSD) || defined (__OpenBSD__)
+#if defined(__sgi) || defined(SGI) || defined(__sgi__) || defined(__FreeBSD__) || defined(__FreeBSD) || defined(__OpenBSD) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__NetBSD)
 	#include <sys/endian.h>
 #else
 	#include <endian.h>
