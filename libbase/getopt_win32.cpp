7e1aa63bf962a6a55d8f6b692da1f25393d68250
nihilus <>
2006-09-25 12:16:43 +0000

    libbase/getopt.c => libbase/getopt_win32.cpp.

---
+++ libbase/getopt_win32.cpp
@@ -0,0 +1,78 @@
+/* $Id: getopt_win32.cpp,v 1.1 2006/09/25 13:16:44 nihilus Exp $ */
+
+char *optarg;			// global argument pointer
+int optind = 0;			// global argv index
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  int getopt (int argc, char *argv[], char *optstring)
+  {
+    char c;
+    char *cp;
+    static char *next = NULL;
+    if (optind == 0)
+      next = NULL;
+
+      optarg = NULL;
+
+    if (next == NULL || *next == '\0')
+      {
+	if (optind == 0)
+	  optind++;
+
+	if (optind >= argc || argv[optind][0] != '-'
+	    || argv[optind][1] == '\0')
+	  {
+	    optarg = NULL;
+	    if (optind < argc)
+	      optarg = argv[optind];
+	    return EOF;
+	  }
+
+	if (strcmp (argv[optind], "--") == 0)
+	  {
+	    optind++;
+	    optarg = NULL;
+	    if (optind < argc)
+	      optarg = argv[optind];
+	    return EOF;
+	  }
+
+	next = argv[optind] + 1;
+	optind++;
+      }
+
+    c = *next++;
+    cp = strchr (optstring, c);
+
+    if (cp == NULL || c == ':')
+      return '?';
+
+    cp++;
+    if (*cp == ':')
+      {
+	if (*next != '\0')
+	  {
+	    optarg = next;
+	    next = NULL;
+	  }
+	else if (optind < argc)
+	  {
+	    optarg = argv[optind];
+	    optind++;
+	  }
+	else
+	  {
+	    return '?';
+	  }
+      }
+
+    return c;
+  }
+
+#ifdef __cplusplus
+}
+#endif
