f3ebdaaa6868f4fe3ecef5059d35c86a40cf06b1
rsavoye <>
2006-10-07 13:05:44 +0000

    New files for AGG backend with framebuffer "no gui" support.

---
+++ backend/render_handler_tri.cpp
@@ -0,0 +1,356 @@
+// 
+//   Copyright (C) 2005, 2006 Free Software Foundation, Inc.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+// Linking Gnash statically or dynamically with other modules is making a
+// combined work based on Gnash. Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+//
+// As a special exception, the copyright holders of Gnash give you
+// permission to combine Gnash with free software programs or libraries
+// that are released under the GNU LGPL and with code included in any
+// release of Talkback distributed by the Mozilla Foundation. You may
+// copy and distribute such a system following the terms of the GNU GPL
+// for all but the LGPL-covered parts and Talkback, and following the
+// LGPL for the LGPL-covered parts.
+//
+// Note that people who make modified versions of Gnash are not obligated
+// to grant this special exception for their modified versions; it is their
+// choice whether to do so. The GNU General Public License gives permission
+// to release a modified version without this exception; this exception
+// also makes it possible to release a modified version which carries
+// forward this exception.
+// 
+//
+
+#include "render_handler_tri.h"
+
+#ifndef RENDER_HANDLER_H
+#error missing includes!
+#endif
+
+#ifndef GNASH_RENDER_HANDLER_TRI_H
+#error missing includes!
+#endif
+
+namespace gnash {
+
+
+// helper function for tri_cache_manager
+static int	sort_by_decreasing_error(const void* A, const void* B)
+{
+  const mesh_set*	a = *(const mesh_set* const *) A;
+  const mesh_set*	b = *(const mesh_set* const *) B;
+  float atol = a->get_error_tolerance(); 
+  float btol = b->get_error_tolerance(); 
+  
+  if (atol < btol) {
+    return 1;
+  } else if (atol > btol) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+//------------------------------------------------------------------------------
+
+mesh_set* tri_cache_manager::search_candidate(float max_error)  {
+
+  for (unsigned int i=0,n=m_cached_meshes.size(); i<n; i++) {
+  
+    mesh_set* candidate = m_cached_meshes[i];
+    float candidate_etol = candidate->get_error_tolerance();
+  
+    if (max_error > candidate_etol * 3.0f) {
+      // Mesh is too high-res; the remaining meshes are higher res,
+      // so stop searching and build an appropriately scaled mesg.
+      break;
+    }
+    
+    if (max_error > candidate_etol) {
+      // found it!
+      return candidate;
+    }
+  
+  } // for
+  
+  // when we come here it means that no candidate could be found... :-(
+  return NULL;
+  
+} // search_candidate
+  
+  
+/// Adds a mesh set to the cache. 
+void tri_cache_manager::add(mesh_set* m) {
+  m_cached_meshes.push_back(m);
+  sort_and_clean_meshes();
+} // add
+
+
+/// Maintain cached meshes. Clean out mesh_sets that haven't been used 
+/// recently, and make sure they're sorted from high error to low error.
+void tri_cache_manager::sort_and_clean_meshes() {
+
+  // Re-sort.
+  if (m_cached_meshes.size() > 0) {
+  
+    qsort(
+      &m_cached_meshes[0],
+      m_cached_meshes.size(),
+      sizeof(m_cached_meshes[0]),
+      sort_by_decreasing_error
+    );
+  
+    // TODO: The cache will grow forever, without any limit. Add code that
+    // limits the vector to a certain size. The older Gnash implementation
+    // appears to never have removed unused cache objects!
+  
+  }
+  
+  // Sort check omitted!
+
+} // sort_and_clean_meshes
+  
+
+
+//------------------------------------------------------------------------------
+
+void triangulating_render_handler::draw_glyph(shape_character_def *def,
+    const matrix& mat, rgba color, float pixel_scale) {
+    
+  // Make sure m_single_fill_styles contains the desired color
+  need_single_fill_style(color);
+    
+  // A glyph is a notmal character, so draw it normally
+  draw_shape_character(def, mat, m_neutral_cxform, pixel_scale, 
+    m_single_fill_styles, m_dummy_line_styles);
+}
+
+
+  
+  
+void triangulating_render_handler::draw_shape_character(shape_character_def *def, 
+  const matrix& mat,
+  const cxform& cx,
+  float pixel_scale,
+  const std::vector<fill_style>& fill_styles,
+  const std::vector<line_style>& line_styles) {    
+  tri_cache_manager* cman = get_cache_of(def);
+  mesh_set* m;
+  int from_cache;
+  
+  // Compute the error tolerance in object-space.
+  float	max_scale = mat.get_max_scale();
+  
+  if (fabsf(max_scale) < 1e-6f) {
+    // Scale is essentially zero.
+    return;
+  }    
+  
+  float	object_space_max_error = 
+    20.0f / max_scale / pixel_scale * s_curve_max_pixel_error;
+    
+    
+  // NOTE: gnash_debug_show_paths ommitted
+  
+  // Try to find a usable mesh set in the cache
+  m = cman->search_candidate(object_space_max_error);
+  from_cache = m != NULL;
+   
+  if (!from_cache) {
+    // no cache hit, construct a new mesh to handle this error tolerance.
+    m = new mesh_set(def, object_space_max_error * 0.75f);
+  };
+
+  // draw the mesh set    
+  draw_mesh_set(*m, mat, cx, fill_styles, line_styles, 1.0);
+    
+  
+  if (!from_cache) {
+    // add to cache (do this after drawing the mesh!)
+    cman->add(m);    
+  }
+  
+} // draw_shape_character
+
+
+
+void triangulating_render_handler::draw_mesh_set(const mesh_set& m, 
+  const matrix& mat, const cxform& cx,
+  const std::vector<fill_style> &fill_styles,
+  const std::vector<line_style> &line_styles, float ratio) {
+  
+  set_matrix(mat);
+  set_cxform(cx);
+  
+  // draw fills
+  for (unsigned int i = 0; i < m.m_meshes.size(); i++) {
+    const mesh& the_mesh = m.m_meshes[i];
+    const fill_style& the_style = fill_styles[i];    
+    
+    if (!the_mesh.m_triangle_strip.size()) continue; // nothing to draw
+    
+    apply_fill_style(the_style, 0, ratio);
+    draw_mesh_strip(&the_mesh.m_triangle_strip[0], 
+      the_mesh.m_triangle_strip.size() / 2);   
+  }
+  
+  // draw outlines
+  for (unsigned int i = 0; i < m.m_line_strips.size(); i++)
+  {
+    int	style = m.m_line_strips[i].get_style();
+    const line_strip& strip = m.m_line_strips[i];
+    
+    assert(strip.m_coords.size() > 1);
+    assert((strip.m_coords.size() & 1) == 0);
+    apply_line_style(line_styles[style], ratio);
+    
+    draw_line_strip(&strip.m_coords[0], strip.m_coords.size() >> 1);
+  }
+    
+  
+} // draw_mesh_set
+
+
+void triangulating_render_handler::apply_fill_style(const fill_style& style, 
+  int fill_side, float ratio) {
+
+  UNUSED(ratio);
+  
+  if (style.m_type == SWF::FILL_SOLID)
+  {
+    // 0x00: solid fill
+    fill_style_color(fill_side, style.m_color); 
+  }
+  else if (style.m_type == SWF::FILL_LINEAR_GRADIENT
+    || style.m_type == SWF::FILL_RADIAL_GRADIENT)
+  {
+    // 0x10: linear gradient fill
+    // 0x12: radial gradient fill
+    
+    style.need_gradient_bitmap();
+    
+    if (style.m_gradient_bitmap_info != NULL) {
+      fill_style_bitmap(
+        fill_side,
+        style.m_gradient_bitmap_info.get_ptr(),
+        style.m_gradient_matrix,
+        gnash::render_handler::WRAP_CLAMP);
+    }
+  }
+  else if (style.m_type == SWF::FILL_TILED_BITMAP
+    || style.m_type == SWF::FILL_CLIPPED_BITMAP
+    || style.m_type == SWF::FILL_TILED_BITMAP_HARD
+    || style.m_type == SWF::FILL_CLIPPED_BITMAP_HARD)
+  {
+    
+    // bitmap fill (either tiled or clipped)
+    
+    gnash::bitmap_info*	bi = NULL;
+    
+    if (style.m_bitmap_character != NULL)	{
+    
+      bi = style.m_bitmap_character->get_bitmap_info();
+      
+      if (bi != NULL)	{
+      
+        gnash::render_handler::bitmap_wrap_mode	
+          wmode = gnash::render_handler::WRAP_REPEAT;
+          
+        if (style.m_type == SWF::FILL_CLIPPED_BITMAP
+          || style.m_type == SWF::FILL_CLIPPED_BITMAP_HARD)
+        {
+          wmode = gnash::render_handler::WRAP_CLAMP;
+        }
+        fill_style_bitmap(
+          fill_side,
+          bi,
+          style.m_bitmap_matrix,
+          wmode);
+      }
+    }
+  }  
+
+} // apply_fill_stype
+
+
+void triangulating_render_handler::apply_line_style(const line_style& style, 
+  float ratio) {
+  
+  UNUSED(ratio);
+  line_style_color(style.m_color);
+  line_style_width(style.m_width);  
+} // apply_line_style
+
+
+
+void	triangulating_render_handler::draw_line_strip(const void* coords, 
+  int vertex_count, const rgba color) {
+  
+  line_style_color(color);
+  line_style_width(1);
+  draw_line_strip(coords, vertex_count);
+  
+}
+
+void  triangulating_render_handler::draw_poly(const point* corners, 
+  int corner_count, const rgba fill, const rgba outline) {
+
+  // Create points array to vertex array
+  int cno, vno=0;  
+  int16_t *vertex = (int16_t*)malloc((corner_count+1)*2*sizeof(int16_t));
+  for (cno=0; cno<corner_count; cno++) {
+    vertex[vno  ] = (float)corners[cno].m_x;
+    vertex[vno+1] = (float)corners[cno].m_y;
+    vno+=2;
+  }
+  // add one more point to close the polygon
+  vertex[vno  ] = vertex[0];
+  vertex[vno+1] = vertex[1];
+  
+  // fill the polygon
+  if (fill.m_a>0) {
+    fill_style_color(0, fill);
+    draw_mesh_strip(vertex, corner_count+1);
+  }
+  
+  // draw the polygon outline
+  if (outline.m_a>0) {
+    line_style_color(outline);
+    line_style_width(1.0f);
+    draw_line_strip(vertex, corner_count+1);
+  } 
+  
+  free(vertex);
+  
+} // draw_poly
+
+
+
+
+tri_cache_manager* triangulating_render_handler::get_cache_of(character_def* def) {
+
+  if (def->m_render_cache == NULL) {
+    def->m_render_cache = new tri_cache_manager;
+  }
+  
+  return (tri_cache_manager*) def->m_render_cache;
+
+} // get_cache_of
+ 
+
+
+} // namespace gnash
---
+++ gui/fb.cpp
@@ -0,0 +1,367 @@
+//
+//   Copyright (C) 2005, 2006 Free Software Foundation, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+// Linking Gnash statically or dynamically with other modules is making a
+// combined work based on Gnash. Thus, the terms and conditions of the GNU
+// General Public License cover the whole combination.
+//
+// As a special exception, the copyright holders of Gnash give you
+// permission to combine Gnash with free software programs or libraries
+// that are released under the GNU LGPL and with code included in any
+// release of Talkback distributed by the Mozilla Foundation. You may
+// copy and distribute such a system following the terms of the GNU GPL
+// for all but the LGPL-covered parts and Talkback, and following the
+// LGPL for the LGPL-covered parts.
+//
+// Note that people who make modified versions of Gnash are not obligated
+// to grant this special exception for their modified versions; it is their
+// choice whether to do so. The GNU General Public License gives permission
+// to release a modified version without this exception; this exception
+// also makes it possible to release a modified version which carries
+// forward this exception.
+//
+//
+
+// -----------------------------------------------------------------------------
+
+/// A Framebuffer-based GUI for Gnash. It required the AGG backend to render
+/// the frames. It operates in fullscreen mode and uses the screen mode 
+/// currently active.
+///
+/// Supported graphics modes:
+///
+///   Resolution: any
+///
+///   Pixel formats:
+///     8 bit: none yet
+///     15 bit: R5/G5/B5
+///     16 bit: R5/G6/B5
+///     24 bit: R8/G8/B8, B8/G8/R8
+///
+///
+/// Supported input devices:
+///
+///   None, yet. But may be added easily.    
+   
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <linux/fb.h>
+#include <unistd.h>
+
+#include "gnash.h"
+#include "gui.h"
+#include "fbsup.h"
+#include "log.h"
+
+//#define DEBUG_SHOW_FPS  // prints number of frames per second to STDOUT
+
+#ifdef DEBUG_SHOW_FPS
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+#endif
+//--
+
+
+namespace gnash
+{
+
+
+//---------------
+#ifdef DEBUG_SHOW_FPS
+double fps_timer=0;
+int fps_counter=0;
+void profile() {
+  int fd;
+  double uptime, idletime;
+  char buffer[20];
+  int readcount;
+
+  fd = open("/proc/uptime", O_RDONLY);
+  if (fd<0) return;
+  readcount = read(fd, buffer, sizeof(buffer)-1);
+  buffer[readcount]=0;
+  sscanf(buffer, "%lf %lf", &uptime, &idletime);
+  close(fd);
+  
+  fps_counter++;
+
+  if (fps_counter<2) {
+    fps_timer = uptime;
+    return;    
+  }
+  
+  printf("FPS: %.3f (%.2f)\n", fps_counter/(uptime-fps_timer), uptime-fps_timer);
+  
+}
+#endif
+//---------------
+
+FBGui::FBGui() : Gui()
+{
+}
+
+FBGui::FBGui(unsigned long xid, float scale, bool loop, unsigned int depth)
+	: Gui(xid, scale, loop, depth)
+{
+	fd 			= -1;
+	fbmem 	= NULL;
+	buffer  = NULL;
+}
+
+FBGui::~FBGui()
+{
+	if (fd>0) {
+		log_msg("Closing framebuffer device\n");
+		close(fd);
+	}
+
+	if (buffer) {
+		log_msg("Free'ing offscreen buffer\n");
+		free(buffer);
+	}
+}
+
+
+/// For 8 bit (palette / LUT) modes, sets a grayscale palette. This GUI 
+/// currently does not support palette modes. 
+bool FBGui::set_grayscale_lut8()
+{
+  #define TO_16BIT(x) (x | (x<<8))
+
+  struct fb_cmap cmap;
+  int i;
+
+  log_msg("LUT8: Setting up colormap\n");
+
+  cmap.start=0;
+  cmap.len=256;
+  cmap.red = (__u16*)malloc(CMAP_SIZE);
+  cmap.green = (__u16*)malloc(CMAP_SIZE);
+  cmap.blue = (__u16*)malloc(CMAP_SIZE);
+  cmap.transp = NULL;
+
+  for (i=0; i<256; i++) {
+
+    int r = i;
+    int g = i;
+    int b = i;
+
+    cmap.red[i] = TO_16BIT(r);
+    cmap.green[i] = TO_16BIT(g);
+    cmap.blue[i] = TO_16BIT(b);
+
+  }
+
+  if (ioctl(fd, FBIOPUTCMAP, &cmap)) {
+    log_error("LUT8: Error setting colormap: %s\n", strerror(errno));
+    return false;
+  }
+
+  return true;
+
+  #undef TO_16BIT
+}
+
+bool FBGui::init(int argc, char **argv[])
+{
+  // Open the framebuffer device
+  fd = open("/dev/fb0", O_RDWR);
+  if (fd<0) {
+    log_error("Could not open framebuffer device: %s", strerror(errno));
+    return false;
+  }
+  
+  // Load framebuffer properties
+  ioctl(fd, FBIOGET_VSCREENINFO, &var_screeninfo);
+  ioctl(fd, FBIOGET_FSCREENINFO, &fix_screeninfo);
+  log_msg("Framebuffer device uses %d bytes of memory.\n",
+  	fix_screeninfo.smem_len);
+  log_msg("Video mode: %dx%d with %d bits per pixel.\n",
+    var_screeninfo.xres, var_screeninfo.yres, var_screeninfo.bits_per_pixel);
+
+  // map framebuffer into memory
+  fbmem = (unsigned char *)
+    mmap(0, fix_screeninfo.smem_len, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
+#ifdef DOUBLE_BUFFER
+  // allocate offscreen buffer
+  buffer = (unsigned char*)malloc(fix_screeninfo.smem_len);
+  memset(buffer, 0, fix_screeninfo.smem_len);
+#endif  
+
+#ifdef PIXELFORMAT_LUT8
+  // Set grayscale for 8 bit modes
+  if (var_screeninfo.bits_per_pixel==8) {
+  	if (!set_grayscale_lut8())
+  		return false;
+  }
+#endif
+
+  // Ok, now initialize AGG
+  return initialize_renderer();
+}
+
+bool FBGui::initialize_renderer() {
+  int _width 		= var_screeninfo.xres;
+  int _height 	= var_screeninfo.yres;
+  int _bpp = var_screeninfo.bits_per_pixel;
+  int _size = fix_screeninfo.smem_len;   // TODO: should recalculate!  
+  unsigned char *_mem;
+  
+  
+  #ifdef DOUBLE_BUFFER
+  _mem = buffer;
+  #else
+  _mem = fbmem;
+  #endif
+  
+  
+  _renderer = NULL;
+  
+  // choose apropriate pixel format
+  
+  log_msg("red channel: %d / %d", var_screeninfo.red.offset, 
+    var_screeninfo.red.length);
+  log_msg("green channel: %d / %d", var_screeninfo.green.offset, 
+    var_screeninfo.green.length);
+  log_msg("blue channel: %d / %d", var_screeninfo.blue.offset, 
+    var_screeninfo.blue.length);
+  
+  // 15 bits RGB (hicolor)
+  if ((var_screeninfo.red.offset==10)
+   && (var_screeninfo.red.length==5)
+   && (var_screeninfo.green.offset==5)
+   && (var_screeninfo.green.length==5)
+   && (var_screeninfo.blue.offset==0)
+   && (var_screeninfo.blue.length==5) ) {
+   
+    _renderer = create_render_handler_agg("RGB555", 
+      _mem, _size, _width, _height, _bpp);
+      
+  } else   
+  // 16 bits RGB (hicolor)
+  if ((var_screeninfo.red.offset=11)
+   && (var_screeninfo.red.length==5)
+   && (var_screeninfo.green.offset==5)
+   && (var_screeninfo.green.length==6)
+   && (var_screeninfo.blue.offset==0)
+   && (var_screeninfo.blue.length==5) ) {
+   
+    _renderer = create_render_handler_agg("RGB565", 
+      _mem, _size, _width, _height, _bpp);
+      
+  } else   
+  // 24 bits RGB (truecolor)
+  if ((var_screeninfo.red.offset==16)
+   && (var_screeninfo.red.length==8)
+   && (var_screeninfo.green.offset==8)
+   && (var_screeninfo.green.length==8)
+   && (var_screeninfo.blue.offset==0)
+   && (var_screeninfo.blue.length==8) ) {
+   
+    _renderer = create_render_handler_agg("RGB24", 
+      _mem, _size, _width, _height, _bpp);
+      
+  } else   
+  // 24 bits BGR (truecolor)
+  if ((var_screeninfo.red.offset==0)
+   && (var_screeninfo.red.length==8)
+   && (var_screeninfo.green.offset==8)
+   && (var_screeninfo.green.length==8)
+   && (var_screeninfo.blue.offset==16)
+   && (var_screeninfo.blue.length==8) ) {
+   
+    _renderer = create_render_handler_agg("BGR24", 
+      _mem, _size, _width, _height, _bpp);
+      
+  } else {
+    log_msg("ERROR: The pixel format of your framebuffer is not supported.");
+  }
+
+  assert(_renderer!=NULL);
+
+  set_render_handler(_renderer);
+
+  return true;
+}
+
+bool FBGui::run(void *)
+{
+	while (true) {
+		// sleep for _interval milliseconds
+    // TODO: Instead of sleeping, use a timer to compensate render time		
+		usleep(_interval*1000);
+		Gui::advance_movie(this);
+	}
+	return true;
+}
+
+void FBGui::renderBuffer()
+{
+
+#ifdef DOUBLE_BUFFER
+	// TODO: Copy only updated parts of the screen!
+	memcpy(fbmem, buffer, var_screeninfo.xres*var_screeninfo.yres*
+    (var_screeninfo.bits_per_pixel/8));
+#endif
+	
+#ifdef DEBUG_SHOW_FPS
+	profile();
+#endif
+}
+
+bool FBGui::createWindow(int width, int height)
+{
+  // Framebuffer has no windows... :-)
+
+	return true;
+}
+
+bool FBGui::createMenu()
+{
+  // no menu support! 
+	return true;
+}
+
+bool FBGui::setupEvents()
+{
+  // events currently not supported!
+	return true;
+}
+
+void FBGui::setCallback(unsigned int interval)
+{
+	_interval = interval;
+}
+
+void FBGui::setTimeout(unsigned int timeout)
+{
+
+}
+
+
+// end of namespace gnash
+}
