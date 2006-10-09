00d7ffb526e0fbe6c1a65d6d760f92c175fbc98b
strk <>
2006-10-09 20:00:04 +0000

    * gui/gtk.cpp, gui/gtksup.h: fixed run() method to honour           gui.h interface (removed argument, unused anyway)

--- gui/gtksup.h
+++ gui/gtksup.h
@@ -73,7 +73,7 @@ class DSOEXPORT GtkGui : public Gui
     virtual bool init(int argc, char **argv[]);
     virtual bool createWindow(int width, int height);
     virtual bool createWindow(const char *title, int width, int height);
-    virtual bool run(void *arg);    
+    virtual bool run();    
     virtual bool createMenu();
     virtual bool setupEvents();
     virtual void renderBuffer();
