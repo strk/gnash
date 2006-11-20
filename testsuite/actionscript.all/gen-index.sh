d0ae588c03e3464a81098338d0427c11e735af66
strk <>
2006-11-20 12:33:47 +0000

    * testsuite/actionscript.all/gen-index.sh: add more info in the versioned SWF index page.

--- testsuite/actionscript.all/gen-index.sh
+++ testsuite/actionscript.all/gen-index.sh
@@ -2,8 +2,22 @@
 
 ## generate index.html
 {
-echo "<html><head><title>Test units</title></head><body>" 
-echo "<a href=alltests.swf>all tests in a single page</a>"
+echo "<html><head><title>Gnash - ActionScript tests</title></head><body>" 
+echo "<h1>Gnash - ActionScript tests</h1>"
+echo "<h2>All tests in a single SWF movie</h2>"
+echo "<p>"
+echo "<ul>"
+echo "<li><a href=alltests.swf>alltests.swf</a></li>"
+echo "</ul>"
+echo "</p>"
+echo "<p>"
+echo "This test is likely to overflow the 'visual traces' window."
+echo "If this is the case (you can't see the final report about number"
+echo "of succeeded / failed tests) consider running the single tests "
+echo "instead. We'll need to fix this."
+echo "</p>"
+echo "<h2>One SWF for each test</H2>"
+echo "<p>"
 echo "<ul>"
 for t in $@; do 
 	if test "$t" = "alltests.swf"; then
@@ -11,6 +25,7 @@ for t in $@; do
 	fi
 	echo "<li><a href=$t>$t</a></li>" 
 done; 
+echo "</p>"
 echo "</body></html>" 
 } > index.html
 
