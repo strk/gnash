0f79eae087f8692bccf093226970451fe387e34b
strk <>
2006-11-30 00:13:27 +0000

    * testsuite/actionscript.all/gen-index-wiki.sh:           include a link to the source code.

--- testsuite/actionscript.all/gen-index-wiki.sh
+++ testsuite/actionscript.all/gen-index-wiki.sh
@@ -8,6 +8,9 @@ for t in $@; do
 	{
 	echo "== $testname =="
 	echo 
+	echo "Source: http://cvs.savannah.gnu.org/viewcvs/gnash/testsuite/actionscript.all/${testname}.as?root=gnash"
+	echo 
+	echo "Targets: "
 	echo "[http://gnash.lulu.com/testcases/v5/${testname}-v5.swf SWF5] "
 	echo "[http://gnash.lulu.com/testcases/v6/${testname}-v6.swf SWF6] "
 	echo "[http://gnash.lulu.com/testcases/v7/${testname}-v7.swf SWF7] "
