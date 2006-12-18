c86dea8dad6ba723bc20e022185548444b3a4a44
strk <>
2006-12-18 11:19:20 +0000

    * testsuite/check.h: added stubs for xpass/xfail and defined           xcheck/xcheck_equals/xcheck_equals_label (unfortunately           xfail/xpass are not in latest dejagnu release so we'll either           have to force dejagnu upgrade or always use the stubs instead...           latter being not a bad idea IMHO).

--- testsuite/check.h
+++ testsuite/check.h
@@ -9,18 +9,21 @@
 #include <iostream>
 #include <string>
 
+// TODO: ensure dejagnu.h defines xpass/xfail too (seems a new feature :()
 #ifdef HAVE_DEJAGNU_H
 #include "dejagnu.h"
 
 #define info(x) note x
 
 #else
-#warning "You should install DejaGnu! Using stubs for pass/fail..."
+#warning "You should install DejaGnu! Using stubs for pass/fail/xpass/xfail..."
 class TestState 
 {
  public:
   void pass(std::string s) { std::cout << "PASSED: " << s << std::endl;  };
+  void xpass(std::string s) { std::cout << "XPASSED: " << s << std::endl;  };
   void fail(std::string s) { std::cout << "FAILED: " << s << std::endl;  };
+  void xfail(std::string s) { std::cout << "XFAILED: " << s << std::endl;  };
 };
 
 #define info(x) { printf("NOTE: "); printf x; putchar('\n'); }
@@ -28,6 +31,7 @@ class TestState
 #endif
 
 TestState _runtest;
+
 #define check_equals_label(label, expr, expected) \
 	{ \
 		std::stringstream ss; \
@@ -47,7 +51,28 @@ TestState _runtest;
 		} \
 	}
 
+#define xcheck_equals_label(label, expr, expected) \
+	{ \
+		std::stringstream ss; \
+		if ( label != "" ) ss << label << ": "; \
+		if ( expr == expected ) \
+		{ \
+			ss << #expr << " == " << expected; \
+			ss << " [" << __FILE__ << ":" << __LINE__ << "]"; \
+			_runtest.xpass(ss.str().c_str()); \
+		} \
+		else \
+		{ \
+			ss << #expr << " == '" << expr << "' (expected: " \
+				<< expected << ")"; \
+			ss << " [" << __FILE__ << ":" << __LINE__ << "]"; \
+			_runtest.xfail(ss.str().c_str()); \
+		} \
+	}
+
 #define check_equals(expr, expected) check_equals_label("", expr, expected)
+
+#define xcheck_equals(expr, expected) xcheck_equals_label("", expr, expected)
 
 #define check(expr) \
 	{ \
@@ -58,6 +83,18 @@ TestState _runtest;
 			_runtest.pass(ss.str().c_str()); \
 		} else { \
 			_runtest.fail(ss.str().c_str()); \
+		} \
+	}
+
+#define xcheck(expr) \
+	{ \
+		std::stringstream ss; \
+		ss << #expr; \
+		ss << " [" << __FILE__ << ":" << __LINE__ << "]"; \
+		if ( expr ) { \
+			_runtest.xpass(ss.str().c_str()); \
+		} else { \
+			_runtest.xfail(ss.str().c_str()); \
 		} \
 	}
 
