d7b2bfb4f55921b7481f6ec384265cf59aa3270b
strk <>
2006-12-18 09:36:58 +0000

    * testsuite/misc-ming.all/timeline_var_test.c: turn into           a self-contained test (still not Dejagnu.swf based).

--- testsuite/misc-ming.all/timeline_var_test.c
+++ testsuite/misc-ming.all/timeline_var_test.c
@@ -20,62 +20,94 @@
  *
  ***********************************************************************
  * zoulunkai,  zoulunkai@gmail.com
- * Test case for variable defined in main timeline:
+ * Test case for variable defined in main timeline;
+ *
+ * Movie has 4 frames defined as FRAME_COUNT;
  ***********************************************************************/
 
 
 #include <stdio.h>
 #include <ming.h>
 
-#define OUTPUT_FILENAME "timeline_var_test.swf"
 
+#define OUTPUT_FILENAME "timeline_var_test.swf"
+const int  FRAME_COUNT  = 4;
 
 SWFAction  action_in_frame1()
 {
-	SWFAction ac;
-	ac = compileSWFActionCode( "trace(var_at_frame2);" );
-	return ac;
+  SWFAction ac;
+  ac = compileSWFActionCode(" \
+      loop_back = 0; \
+  ");
+  return ac;
 }
+
 
 SWFAction  action_in_frame2()
 {
-	SWFAction ac;
-	ac = compileSWFActionCode( "var var_at_frame2 = \"var_defined_at_frame2\";" );
-	return ac;
+  SWFAction ac;
+  ac = compileSWFActionCode(" \
+     if(loop_back == 0) \
+     { \
+        if ( var_at_frame3 == undefined ) \
+            trace(\"PASSED: var_at_frame3 == undefined \" ); \
+        else \
+            trace(\"FAILED: var_at_frame3 == undefined \" ); \
+     } \
+     else \
+     { \
+         if ( var_at_frame3 == \"var_defined_at_frame3\" ) \
+             trace(\"XPASSED: var_at_frame3 == var_defined_at_frame3\" ); \
+         else \
+             trace(\"XFAILED: var_at_frame3 == var_defined_at_frame3\" ); \
+     } \
+  ");
+  return ac;
 }
 
 SWFAction  action_in_frame3()
 {
-	SWFAction ac;
-	ac = compileSWFActionCode( "gotoAndStop(1);" );
-	return ac;
+  SWFAction ac;
+  ac = compileSWFActionCode(" \
+      var var_at_frame3 = \"var_defined_at_frame3\"; \
+  ");
+  return ac;
 }
+
+SWFAction  action_in_frame4()
+{
+  SWFAction ac;
+  ac = compileSWFActionCode(" \
+    if ( ++loop_back < 2 ) gotoAndPlay(2); \
+  " );
+  return ac;
+}
+
 
 int main()
 {
-	SWFMovie  movie;
-	SWFAction ac;
+  SWFMovie  movie;
+  SWFAction ac[FRAME_COUNT];
+  int i;
 
+  Ming_init();
+  movie = newSWFMovie();
 
-	Ming_init();
-	movie = newSWFMovie();
+  // Add frame ActionScipts to frames
+  ac[0] = action_in_frame1();
+  ac[1] = action_in_frame2();
+  ac[2] = action_in_frame3();
+  ac[3] = action_in_frame4();
+  
+  for(i=0; i<FRAME_COUNT; i++)
+  {
+    SWFMovie_add(movie, (SWFBlock)ac[i]);
+    SWFMovie_nextFrame(movie); 
+  }
+  
+  // save files
+  puts("Saving " OUTPUT_FILENAME );
+  SWFMovie_save(movie, OUTPUT_FILENAME);
 
-	// Add frame ActionScipts to frames
-	ac = action_in_frame1();
-	SWFMovie_add(movie, (SWFBlock)ac);
-	SWFMovie_nextFrame(movie); 
-	
-	ac = action_in_frame2();
-	SWFMovie_add(movie, (SWFBlock)ac);
-	SWFMovie_nextFrame(movie); 
-	
-	ac = action_in_frame3();
-	SWFMovie_add(movie, (SWFBlock)ac);
-	SWFMovie_nextFrame(movie); 
-	
-	//save files
-	puts("Saving " OUTPUT_FILENAME );
-	SWFMovie_save(movie, OUTPUT_FILENAME);
-
-	return 0;
+  return 0;
 }
