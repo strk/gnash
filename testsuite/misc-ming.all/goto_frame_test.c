fdaf75026e1b248083645729e085b4bb849da80e
zoulunkai <>
2007-01-31 08:14:53 +0000

    add test for consecutive goto

--- testsuite/misc-ming.all/goto_frame_test.c
+++ testsuite/misc-ming.all/goto_frame_test.c
@@ -21,8 +21,6 @@
  *
  * Test for ActionGotoFrame 
  *
- * the root movie has 4 frames which contains another 3-framed movieClip named mc_red.
- * At _root's 2st frame, tell mc_red to goto 3rd frame and stop.
  */
 
 #include <stdlib.h>
@@ -44,7 +42,7 @@ main(int argc, char** argv)
 
   const char *srcdir=".";
   if ( argc>1 ) 
-    srcdir=argv[1];
+      srcdir=argv[1];
   else
   {
       fprintf(stderr, "Usage: %s <mediadir>\n", argv[0]);
@@ -67,8 +65,14 @@ main(int argc, char** argv)
   SWFMovieClip_add(mc_red, (SWFBlock)sh_red);  
   SWFMovieClip_nextFrame(mc_red);//1st frame
   SWFMovieClip_nextFrame(mc_red);//2st frame
-  add_clip_actions(mc_red, "var flag = \"action_executed\"; ");
+  add_clip_actions(mc_red, "var flag = \"action_executed\"; \
+                            var x = \"mc_red.frame3\";\
+                            play();");
   SWFMovieClip_nextFrame(mc_red);//3nd frame
+  
+  add_clip_actions(mc_red, "x = \"mc_red.frame4\"; \
+                            stop();");
+  SWFMovieClip_nextFrame(mc_red);//4th frame
   
   SWFDisplayItem it_red;
   it_red = SWFMovie_add(mo, (SWFBlock)mc_red);  
@@ -76,16 +80,23 @@ main(int argc, char** argv)
   SWFDisplayItem_setName(it_red, "mc_red");
   add_actions(mo, " check_equals(mc_red._currentframe, 1);  \
                     mc_red.gotoAndStop(3); \
-                    check_equals(mc_red._currentframe, 3);");
-                    
+                    check_equals(mc_red._currentframe, 3); \
+                    _root.goAndStop(3); ");               
   SWFMovie_nextFrame(mo); //2nd frame
-
+  
+  add_actions(mo, " _root.gotoAndStop(4); \
+                    mc_red.x = \"_root.frame3\"; ");
   SWFMovie_nextFrame(mo); //3nd frame
-
+  
+  add_actions(mo, " _root.gotoAndStop(5); \
+                    mc_red.x = \"_root.frame4\"; ");
+  SWFMovie_nextFrame(mo); //4nd frame
+  
   //checks
   check_equals(mo, "_root.mc_red.flag", "'action_executed'");
+  check_equals(mo, "_root.mc_red.x", "'_root.frame4'");
   add_actions(mo, " _root.totals(); stop(); ");
-  SWFMovie_nextFrame(mo); //4th frame
+  SWFMovie_nextFrame(mo); //5th frame
 
   //Output movie
   puts("Saving " OUTPUT_FILENAME );
