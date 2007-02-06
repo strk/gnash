4bddacdb17d3b00d244e4e333aee92cbc06cc93e
martinwguy <>
2007-02-06 18:16:41 +0000

    Undo garbling of GPL boilerplate

--- server/asobj/Number.cpp
+++ server/asobj/Number.cpp
@@ -2,7 +2,9 @@
 //   Copyright (C) 2005, 2006 Free Software Foundation, Inc.
 // 
 // This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by // the Free Software Foundation; either version 2 of the License, or // (at your option) any later version.
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
 // 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +18,7 @@
 //
 //
 
-/* $Id: Number.cpp,v 1.12 2007/02/06 18:02:52 martinwguy Exp $ */
+/* $Id: Number.cpp,v 1.13 2007/02/06 18:16:41 martinwguy Exp $ */
 
 // Implementation of ActionScript Number class.
 
