1caa13947fb03f50616fce116a551c042644a4ce
martinwguy <>
2007-02-08 16:39:37 +0000

    Only do string-to-number conversion when text value is required; moved number to text conversion out of class definition into its own function; added text conversion of NaN and Infinities.

--- server/asobj/Number.cpp
+++ server/asobj/Number.cpp
@@ -18,7 +18,7 @@
 //
 //
 
-/* $Id: Number.cpp,v 1.13 2007/02/06 18:16:41 martinwguy Exp $ */
+/* $Id: Number.cpp,v 1.14 2007/02/08 16:39:37 martinwguy Exp $ */
 
 // Implementation of ActionScript Number class.
 
@@ -31,10 +31,12 @@
 #include "builtin_function.h" // need builtin_function
 
 #include <sstream>
+#include <cmath>
 
 namespace gnash {
 
 // Forward declarations
+static void number_val_to_str(double val, char *str);
 //static void number_to_string(const fn_call& fn);
 
 static void
@@ -82,91 +84,115 @@ public:
 		as_object(getNumberInterface()),
 		_val(val)
 	{
-		// Printing formats:
-		//
-		// If _val > 1, Print up to 15 significant digits, then switch
-		// to scientific notation, rounding at the last place and
-		// omitting trailing zeroes.
-		// e.g. for 9*.1234567890123456789
-		// ...
-		// 9999.12345678901
-		// 99999.123456789
-		// 999999.123456789
-		// 9999999.12345679
-		// 99999999.1234568
-		// 999999999.123457
-		// 9999999999.12346
-		// 99999999999.1235
-		// 999999999999.123
-		// 9999999999999.12
-		// 99999999999999.1
-		// 999999999999999
-		// 1e+16
-		// 1e+17
-		// ...
-		// e.g. for 1*.111111111111111111111111111111111111
-		// ...
-		// 1111111111111.11
-		// 11111111111111.1
-		// 111111111111111
-		// 1.11111111111111e+15
-		// 1.11111111111111e+16
-		// ...
-		// For values < 1, print up to 4 leading zeroes after the
-		// deciman point, then switch to scientific notation with up
-		// to 15 significant digits, rounding with no trailing zeroes
-		// e.g. for 1.234567890123456789 * 10^-i:
-		// 1.23456789012346
-		// 0.123456789012346
-		// 0.0123456789012346
-		// 0.00123456789012346
-		// 0.000123456789012346
-		// 0.0000123456789012346
-		// 0.00000123456789012346
-		// 1.23456789012346e-6
-		// 1.23456789012346e-7
-		// ...
-		//
-		// If the value is negative, just add a '-' to the start; this
-		// does not affect the precision of the printed value.
-		//
-		// This almost corresponds to printf("%.15g") format, except
-		// that %.15g switches to scientific notation at e-05 not e-06,
-		// and %g always prints at least two digits for the exponent.
+	}
 
-		// The following code gives the same results as Adobe player
-		// except for
-		// 9.99999999999999[39-61] e{-2,-3}. Adobe prints these as
-		// 0.0999999999999999 and 0.00999999999999 while we print them
-		// as 0.1 and 0.01
-		// These values are at the limit of a double's precision,
-		// for example, in C,
-		// .99999999999999938 printfs as
-		// .99999999999999933387 and
-		// .99999999999999939 printfs as
-		// .99999999999999944489
-		// so this behaviour is probably too compiler-dependent to
-		// reproduce exactly.
-		//
-		// There may be some milage in comparing against
-		// 0.00009999999999999995 and
-		// 0.000009999999999999995 instead.
+	// override from as_object
+	const char* get_text_value() const
+	{
+		number_val_to_str(_val, _str);
+		return _str;
+	}
 
-		if (std::fabs(_val) >= 0.0001 ||
-		    std::fabs(_val) < 0.00001) {
-			char *cp;
+	// override from as_object
+	double get_numeric_value() const
+	{
+		return _val;
+	}
 
-			sprintf(_str, "%.15g", _val);
-			// Remove a leading zero from 2-digit exponent if any
-			if ((cp = strchr(_str, 'e')) != NULL &&
-			    cp[2] == '0') {
-				// We can't use strcpy() cos its src&dest can't
-				// overlap. However, this can only be "...e+0n"
-				// or ...e-0n;  3+digit exponents never have
-				// leading 0s.
-				cp[2] = cp[3]; cp[3] = '\0';
-			}
-		} else {
+	as_value get_primitive_value() const 
+	{
+		return _val;
+	}
+
+};
+
+// Convert numeric value to string value
+static void
+number_val_to_str(double _val, char *_str)
+{
+	// Printing formats:
+	//
+	// If _val > 1, Print up to 15 significant digits, then switch
+	// to scientific notation, rounding at the last place and
+	// omitting trailing zeroes.
+	// e.g. for 9*.1234567890123456789
+	// ...
+	// 9999.12345678901
+	// 99999.123456789
+	// 999999.123456789
+	// 9999999.12345679
+	// 99999999.1234568
+	// 999999999.123457
+	// 9999999999.12346
+	// 99999999999.1235
+	// 999999999999.123
+	// 9999999999999.12
+	// 99999999999999.1
+	// 999999999999999
+	// 1e+16
+	// 1e+17
+	// ...
+	// e.g. for 1*.111111111111111111111111111111111111
+	// ...
+	// 1111111111111.11
+	// 11111111111111.1
+	// 111111111111111
+	// 1.11111111111111e+15
+	// 1.11111111111111e+16
+	// ...
+	// For values < 1, print up to 4 leading zeroes after the
+	// deciman point, then switch to scientific notation with up
+	// to 15 significant digits, rounding with no trailing zeroes
+	// e.g. for 1.234567890123456789 * 10^-i:
+	// 1.23456789012346
+	// 0.123456789012346
+	// 0.0123456789012346
+	// 0.00123456789012346
+	// 0.000123456789012346
+	// 0.0000123456789012346
+	// 0.00000123456789012346
+	// 1.23456789012346e-6
+	// 1.23456789012346e-7
+	// ...
+	//
+	// If the value is negative, just add a '-' to the start; this
+	// does not affect the precision of the printed value.
+	//
+	// This almost corresponds to printf("%.15g") format, except
+	// that %.15g switches to scientific notation at e-05 not e-06,
+	// and %g always prints at least two digits for the exponent.
+
+	// The following code gives the same results as Adobe player
+	// except for
+	// 9.99999999999999[39-61] e{-2,-3}. Adobe prints these as
+	// 0.0999999999999999 and 0.00999999999999 while we print them
+	// as 0.1 and 0.01
+	// These values are at the limit of a double's precision,
+	// for example, in C,
+	// .99999999999999938 printfs as
+	// .99999999999999933387 and
+	// .99999999999999939 printfs as
+	// .99999999999999944489
+	// so this behaviour is probably too compiler-dependent to
+	// reproduce exactly.
+	//
+	// There may be some milage in comparing against
+	// 0.00009999999999999995 and
+	// 0.000009999999999999995 instead.
+
+	// Handle non-numeric values.
+	// "printf" gives "nan", "inf", "-inf", so we check explicitly
+	switch (std::fpclassify(_val)) {
+	case FP_NAN:
+		strcpy(_str, "NaN");
+		break;
+	case FP_INFINITE:
+		// isinf() cannot be relied on to distinguish -Infinity
+		strcpy(_str, _val < 0 ? "-Infinity" : "Infinity");
+		break;
+	default:	// FP_ZERO, FP_NORMAL and FP_SUBNORMAL
+		if (std::fabs(_val) < 0.0001 &&
+		    std::fabs(_val) >= 0.00001) {
 			// This is the range for which %.15g gives scientific
 			// notation but for which we must give decimal.
 			// We can't easily use %f bcos it prints a fixed number
@@ -180,7 +206,7 @@ public:
 			
 			sprintf(_str, "%.15g", _val * 10.0);
 			if ((cp = strchr(_str, '.')) == NULL || cp[1] != '0') {
-				log_error("Internal error: cannot find \".0\" in %s for %.15g\n", _str, _val);
+				log_error("Internal error: Cannot find \".0\" in \%s for %.15g\n", _str, _val);
 				// Just give it to them raw instead
 				sprintf(_str, "%.15g", _val);
 			} else {
@@ -206,29 +232,23 @@ public:
 				*cp = c;
 #endif
 			}
-			
+		} else {
+			// Regular case
+			char *cp;
+
+			sprintf(_str, "%.15g", _val);
+			// Remove a leading zero from 2-digit exponent if any
+			if ((cp = strchr(_str, 'e')) != NULL &&
+			    cp[2] == '0') {
+				// We can't use strcpy() cos its src&dest can't
+				// overlap. However, this can only be "...e+0n"
+				// or ...e-0n;  3+digit exponents never have
+				// leading 0s.
+				cp[2] = cp[3]; cp[3] = '\0';
+			}
 		}
 	}
-
-	// override from as_object
-	const char* get_text_value() const
-	{
-		return _str;
-	}
-
-	// override from as_object
-	double get_numeric_value() const
-	{
-		return _val;
-	}
-
-	as_value get_primitive_value() const 
-	{
-		return get_numeric_value();
-	}
-
-};
-
+}
 
 static void
 number_ctor(const fn_call& fn)
