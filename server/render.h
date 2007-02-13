b0efd53ae898dccd36d09bc37facdb90a1b6a868
strk <>
2007-02-13 14:00:13 +0000

    * server/render.h: had doxygen comments point           to the correct place (render_handler).

--- server/render.h
+++ server/render.h
@@ -27,37 +27,37 @@ namespace gnash {
 	/// gnash::render_handler or fallback on a default
 	/// behaviour if no renderer is registered
 	///
+	/// NOTE: A cleaner implementation would be implementing
+	///       the default behaviour in the render_handler class
+	///	  itself, rather then making it an "abstract" class.
+	///	  Anyway, having proxy calls turned out to be somewhat
+	///	  useful while tracking rendering calls, to have a central
+	///	  place to add traces. We might change this in the future
+	///	  to improve performance (proxy calls are not inlined).
+	///
 	namespace render
 	{
 
-		/// \brief
-		/// Create a bitmap_info so that it contains an alpha texture
-		/// with the given data (1 byte per texel).
+		/// See render_handler::create_bitmap_info_alpha (in backend/render_handler.h)
 		bitmap_info*	create_bitmap_info_alpha(int w, int h,
 					unsigned char* data);
 
-		/// \brief
-		/// Given an image, returns a pointer to a bitmap_info struct
-		/// that can later be passed to fill_styleX_bitmap(), to set a
-		/// bitmap fill style.
-		///
+		/// See render_handler::create_bitmap_info_rgb (in backend/render_handler.h)
 		bitmap_info*	create_bitmap_info_rgb(image::rgb* im);
 
-		/// \brief
-		/// Given an image, returns a pointer to a bitmap_info struct
-		/// that can later be passed to fill_style_bitmap(), to set a
-		/// bitmap fill style.
-		//
-		/// This version takes an image with an alpha channel.
-		///
+		/// See render_handler::create_bitmap_info_rgba (in backend/render_handler.h)
 		bitmap_info*	create_bitmap_info_rgba(image::rgba* im);
 
+		/// See render_handler::get_scale (in backend/render_handler.h)
 		void get_scale(point& scale);
 
-		/// Delete the given bitmap info struct.
+		/// See render_handler::delete_bitmap_info (in backend/render_handler.h)
 		void	delete_bitmap_info(bitmap_info* bi);
 
 		/// The different video frame formats
+		//
+		/// TODO: remove this duplication, original is in gnash::render_handler
+		///
 		enum video_frame_format
 		{
 			NONE,
@@ -65,27 +65,26 @@ namespace gnash {
 			RGB
 		};
 
-		/// Returns the format the current renderer wants videoframes in.
+		/// See render_handler::videoFrameFormat (in backend/render_handler.h)
 		int videoFrameFormat();
 
-		/// Draws the video frames
+		/// See render_handler::drawVideoFrame (in backend/render_handler.h)
 		void drawVideoFrame(image::image_base* frame, const matrix* mat, const rect* bounds);
 
-		/// \brief
-		/// Bracket the displaying of a frame from a movie.
-		/// Fill the background color, and set up default
-		/// transforms, etc.
-		///
+		/// See render_handler::begin_display (in backend/render_handler.h)
 		void	begin_display(
 			rgba background_color,
 			int viewport_x0, int viewport_y0,
 			int viewport_width, int viewport_height,
 			float x0, float x1, float y0, float y1);
+
+		/// See render_handler::end_display (in backend/render_handler.h)
 		void	end_display();
 
-		// Geometric and color transforms for mesh
-		// and line_strip rendering.
+		/// See render_handler::set_matrix (in backend/render_handler.h)
 		void	set_matrix(const matrix& m);
+
+		/// See render_handler::set_cxform (in backend/render_handler.h)
 		void	set_cxform(const cxform& cx);
 
 		/// \brief
@@ -95,42 +94,42 @@ namespace gnash {
 		/// coords is a list of (x,y) coordinate pairs, in
 		/// triangle-strip order.  The type of the array should
 		/// be float[vertex_count*2]
+		///
 		void	draw_mesh_strip(const int16_t coords[],
 				int vertex_count);
 
+		/// See render_handler::draw_line_strip (in backend/render_handler.h)
 		void	draw_line_strip(const int16_t coords[],
 				int vertex_count, const rgba color);
 
-    void  draw_poly(const point* corners, int corner_count, const rgba fill, 
-      const rgba outline);
+		/// See render_handler::draw_poly (in backend/render_handler.h)
+		void  draw_poly(const point* corners, int corner_count,
+				const rgba fill, const rgba outline);
       
-    void draw_shape_character(shape_character_def *def, 
-      character *inst);
+		/// See render_handler::draw_shape_character (in backend/render_handler.h)
+		void draw_shape_character(shape_character_def *def,
+				character *inst);
       
-    void draw_glyph(shape_character_def *def,
-      const matrix& mat,
-      rgba color,
-      float pixel_scale);
+		/// See render_handler::draw_glyph (in backend/render_handler.h)
+		void draw_glyph(shape_character_def *def, const matrix& mat,
+				rgba color, float pixel_scale);
 
-    bool allow_glyph_textures();				
+		/// See render_handler::allow_glyph_textures (in backend/render_handler.h)
+		bool allow_glyph_textures();				
 				
-    /// \brief
-    /// Proxies call to the registered render_handler, or
-    /// return true if no renderer is registered.
-    //
-    /// See render_handler::bounds_in_clipping_area
-    ///
-    bool bounds_in_clipping_area(const rect& bounds);
+		/// See render_handler::bounds_in_clipping_area (in backend/render_handler.h)
+		bool bounds_in_clipping_area(const rect& bounds);
 				
-
+		/// See render_handler::begin_submit_mask (in backend/render_handler.h)
 		void	begin_submit_mask();
+
+		/// See render_handler::end_submit_mask (in backend/render_handler.h)
 		void	end_submit_mask();
+
+		/// See render_handler::disable_mask (in backend/render_handler.h)
 		void	disable_mask();
 
-		/// Special function to draw a rectangular bitmap;
-		//
-		/// intended for textured glyph rendering.  Ignores
-		/// current transforms.
+		/// See render_handler::draw_bitmap (in backend/render_handler.h)
 		void	draw_bitmap(const matrix& m, const bitmap_info* bi,
 				const rect& coords,
 				const rect& uv_coords, rgba color);
