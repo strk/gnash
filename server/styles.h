b370d233971a3db3c8d5ac9eae65a423da87c980
strk <>
2007-02-14 01:40:27 +0000

    Provide a constructor with explicit values for line_style

--- server/styles.h
+++ server/styles.h
@@ -5,7 +5,7 @@
 
 // line style types.
 
-/* $Id: styles.h,v 1.18 2007/02/13 15:19:32 strk Exp $ */
+/* $Id: styles.h,v 1.19 2007/02/14 01:40:27 strk Exp $ */
 
 #ifndef GNASH_STYLES_H
 #define GNASH_STYLES_H
@@ -32,9 +32,34 @@ class line_style : public base_line_style
 {
 public:
 	line_style();
+
+	/// Construct a line style with explicit values
+	///
+	/// @param width
+	///	Thickness of line, in TWIPS. 
+	///	Zero for hair line
+	///
+	/// @param color
+	///	Line color
+	///
+	line_style(uint16_t width, const rgba& color)
+		:
+		m_width(width),
+		m_color(color)
+	{
+	}
+
+	/// Read the line style from an SWF stream
+	//
+	/// Stream is assumed to be positioned at 
+	/// the right place.
+	///
 	void	read(stream* in, int tag_type);
 	
+	/// Return thickness of the line, in TWIPS
 	uint16_t	get_width() const { return m_width; }
+
+	/// Return line color and alpha
 	const rgba&	get_color() const { return m_color; }
 	
 private:
