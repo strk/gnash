4796163af26f2b039c753f39234a29fd94c175b8
strk <>
2007-02-16 23:24:34 +0000

    * testsuite/misc-ming.all/consecutive_goto_frame_test.c:           Visually trace action execution, use C-style comments.

--- testsuite/misc-ming.all/consecutive_goto_frame_test.c
+++ testsuite/misc-ming.all/consecutive_goto_frame_test.c
@@ -66,20 +66,24 @@ main(int argc, char** argv)
 
   dejagnuclip = get_dejagnu_clip((SWFBlock)get_default_font(srcdir), 10, 0, 0, 800, 600);
   SWFMovie_add(mo, (SWFBlock)dejagnuclip);
+  SWFMovie_nextFrame(mo); /* 1st frame -  so we can use _root.note */
   
   mc_red = newSWFMovieClip();
   sh_red = make_fill_square (0, 300, 60, 60, 255, 0, 0, 255, 0, 0);
   SWFMovieClip_add(mc_red, (SWFBlock)sh_red);  
-  add_clip_actions(mc_red, " x = \"as_in_frm1_of_mc_red\"; \
-                             gotoAndStop(2); ");
-  SWFMovieClip_nextFrame(mc_red);//1st frame
-  add_clip_actions(mc_red, " x = \"as_in_frm2_of_mc_red\"; \
-                             gotoAndStop(3); ");
-  SWFMovieClip_nextFrame(mc_red);//2nd frame
-  add_clip_actions(mc_red, " x = \"as_in_frm3_of_mc_red\"; \
-                             gotoAndStop(4); ");           
-  SWFMovieClip_nextFrame(mc_red);//3rd frame
-  SWFMovieClip_nextFrame(mc_red);//4th frame
+  add_clip_actions(mc_red, " _root.note('frm1 of mc_red - gotoAndStop(2)'); "
+		  	"x = 'as_in_frm1_of_mc_red'; "
+                        "gotoAndStop(2); ");
+  SWFMovieClip_nextFrame(mc_red); /* 1st frame */
+  add_clip_actions(mc_red, " _root.note('frm2 of mc_red - gotoAndStop(3)'); "
+		  	"x = 'as_in_frm2_of_mc_red'; "
+                        "gotoAndStop(3); ");
+  SWFMovieClip_nextFrame(mc_red); /* 2nd frame */
+  add_clip_actions(mc_red, " _root.note('frm3 of mc_red - gotoAndStop(4)'); "
+		  	"x = 'as_in_frm3_of_mc_red'; "
+                        "gotoAndStop(4); ");           
+  SWFMovieClip_nextFrame(mc_red); /* 3rd frame */
+  SWFMovieClip_nextFrame(mc_red); /* 4th frame */
   
   
   SWFDisplayItem it_red;
@@ -87,28 +91,31 @@ main(int argc, char** argv)
   SWFDisplayItem_setDepth(it_red, 3); 
   SWFDisplayItem_setName(it_red, "mc_red");
   
-  add_actions(mo, " mc_red.x = \"as_in_frm1_of_root\"; \
-                    gotoAndStop(2); ");               
-  SWFMovie_nextFrame(mo); //1st frame
+  add_actions(mo, " _root.note('frm2 of root - gotoAndStop(3)'); "
+		  "mc_red.x = 'as_in_frm2_of_root'; "
+                  "gotoAndStop(3); ");
+  SWFMovie_nextFrame(mo); /* 2nd frame */
   
-  // mc_red.x has been set after playing the 1st frame, check it here
-  add_actions(mo, " xcheck_equals(mc_red.x, \"as_in_frm1_of_mc_red\"); \
-                    mc_red.x = \"as_in_frm2_of_root\"; \
-                    gotoAndStop(3); ");               
-  SWFMovie_nextFrame(mo); //2nd frame
+  /* mc_red.x has been set after playing the 1st frame, check it here */
+  add_actions(mo, " xcheck_equals(mc_red.x, 'as_in_frm1_of_mc_red'); "
+		  " _root.note('frm3 of root - gotoAndStop(4)');"
+                  "mc_red.x = 'as_in_frm3_of_root'; "
+                  "gotoAndStop(4); ");               
+  SWFMovie_nextFrame(mo); /* 3rd frame */
   
-   // mc_red.x has been set again after playing the 2nd frame, check it again
-  add_actions(mo, " xcheck_equals(mc_red.x, \"as_in_frm2_of_mc_red\"); \
-                    mc_red.x = \"as_in_frm3_of_root\"; \
-                    gotoAndStop(4); ");               
-  SWFMovie_nextFrame(mo); //3rd frame
+  /* mc_red.x has been set again after playing the 2nd frame, check it again */
+  add_actions(mo, " xcheck_equals(mc_red.x, 'as_in_frm2_of_mc_red'); "
+		  " _root.note('frm4 of root - gotoAndStop(5)');"
+                  " mc_red.x = \"as_in_frm4_of_root\"; "
+                  " gotoAndStop(5); ");
+  SWFMovie_nextFrame(mo); /* 4th frame */
   
-   // mc_red.x has been set again after playing the 3rd frame, check it again
+  /* mc_red.x has been set again after playing the 3rd frame, check it again */
   xcheck_equals(mo, "mc_red.x", "'as_in_frm3_of_mc_red'");
   add_actions(mo, " _root.totals(); stop(); ");
-  SWFMovie_nextFrame(mo); //4th frame
+  SWFMovie_nextFrame(mo); /* 5th frame */
 
-  //Output movie
+  /* Output movie */
   puts("Saving " OUTPUT_FILENAME );
   SWFMovie_save(mo, OUTPUT_FILENAME);
 
