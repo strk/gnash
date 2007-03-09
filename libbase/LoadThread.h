1392951c8462a46a02d839a8fb1c1ac9b33eec14
tgc <>
2007-03-09 23:15:57 +0000

    * libbase/LoadThread.h: Documented the class.

--- libbase/LoadThread.h
+++ libbase/LoadThread.h
@@ -16,7 +16,7 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
-// $Id: LoadThread.h,v 1.1 2007/03/09 14:38:29 tgc Exp $
+// $Id: LoadThread.h,v 1.2 2007/03/09 23:15:57 tgc Exp $
 #include <boost/thread/thread.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/bind.hpp>
@@ -24,18 +24,41 @@
 #include "tu_file.h"
 #include <memory>
 
+/// \brief
+/// The LoadThread class can be used to download from a file
+/// or stream using a thread, without having to block.
+/// When the object is created it starts a thread which downloads
+/// from the tu_file given as an argument to the constructor, and
+/// keeps downloading until the download is complete. It is possible
+/// for the object owner to query for data, position, loaded data,
+/// total data while downloading, without blocking. Though if there has 
+/// not been downloaded enough data to accomendate a request (seek/read)
+/// it will block until the data is present (curl_adaptor behavoir).
+///
+/// When using the LoadThread, all access to the tu_file should be
+/// done through LoadThread, or it will likely break.
+///
+/// @todo When we read from a real movie stream (rtmp) we might
+/// want to use a cirkular-buffer.
+
 class LoadThread
 {
 
 public:
+	/// Creating the object starts the threaded loading 
+	/// of the stream/file passed as an argument.
 	LoadThread(tu_file* stream);
 	~LoadThread();
 
 	/// Put read pointer at given position
+	/// Will block if there is not enough data buffered,
+	/// and wait until enough data is present.
 	bool seek(size_t pos);
 
 	/// Read 'bytes' bytes into the given buffer.
-	/// Return number of actually read bytes
+	/// Return number of actually read bytes.
+	/// Will block if there is not enough data buffered,
+	/// and wait until enough data is present.
 	size_t read(void *dst, size_t bytes);
 
 	/// Return true if EOF has been reached
