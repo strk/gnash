d103319a513bde58e5463084445641d6078af941
strk <>
2007-04-02 09:47:36 +0000

    * testsuite/samples/: Makefile.am, gotoFrameOnKeyEvent-TestRunner.cpp:           First stub at an automated test runner for gotoFrameOnKeyEvent.           TODO item in comments.

---
+++ testsuite/samples/gotoFrameOnKeyEvent-TestRunner.cpp
@@ -0,0 +1,81 @@
+/* 
+ *   Copyright (C) 2007 Free Software Foundation, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ */ 
+
+#define INPUT_FILENAME "gotoFrameOnKeyEvent.swf"
+
+#include "MovieTester.h"
+#include "GnashException.h"
+#include "sprite_instance.h"
+#include "character.h"
+#include "dlist.h"
+#include "container.h"
+#include "log.h"
+
+#include "check.h"
+#include <string>
+#include <cassert>
+
+using namespace gnash;
+using namespace std;
+
+int
+main(int /*argc*/, char** /*argv*/)
+{
+	string filename = string(SRCDIR) + string("/") + string(INPUT_FILENAME);
+	auto_ptr<MovieTester> t;
+
+	gnash::LogFile& dbglogfile = gnash::LogFile::getDefaultInstance();
+	dbglogfile.setVerbosity(1);
+
+	try
+	{
+		t.reset(new MovieTester(filename));
+	}
+	catch (const GnashException& e)
+	{
+		std::cerr << "Error initializing MovieTester: " << e.what() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	
+	MovieTester& tester = *t;
+
+	// TODO: check why we need this !!
+	//       I wouldn't want the first advance to be needed
+	tester.advance();
+
+	sprite_instance* root = tester.getRootMovie();
+	assert(root);
+
+	check_equals(root->get_frame_count(), 7);
+
+	check_equals(root->get_current_frame(), 0);
+	tester.advance();
+	check_equals(root->get_current_frame(), 0);
+
+	tester.pressKey(key::_1);
+
+	// TODO: check if it is correct to wait for next ::advance before expecting gotoFrame actions
+	//       execution !!
+
+	xcheck_equals(root->get_current_frame(), 1);
+	tester.advance();
+	check_equals(root->get_current_frame(), 1);
+
+}
+
