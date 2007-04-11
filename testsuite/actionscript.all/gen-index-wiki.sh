f7520898048e4e7417756401930874882fe83bc5
ann <>
2007-04-11 13:04:58 +0000

    Changed all references of 'gnash.lulu.com' to 'www.gnashdev.org'.

--- testsuite/actionscript.all/gen-index-wiki.sh
+++ testsuite/actionscript.all/gen-index-wiki.sh
@@ -11,10 +11,10 @@ for t in $@; do
 	echo "Source: http://cvs.savannah.gnu.org/viewcvs/gnash/testsuite/actionscript.all/${testname}.as?root=gnash"
 	echo 
 	echo "Targets: "
-	echo "[http://gnash.lulu.com/testcases/v5/${testname}-v5.swf SWF5] "
-	echo "[http://gnash.lulu.com/testcases/v6/${testname}-v6.swf SWF6] "
-	echo "[http://gnash.lulu.com/testcases/v7/${testname}-v7.swf SWF7] "
-	echo "[http://gnash.lulu.com/testcases/v8/${testname}-v8.swf SWF8]"
+	echo "[http://www.gnashdev.org/testcases/v5/${testname}-v5.swf SWF5] "
+	echo "[http://www.gnashdev.org/testcases/v6/${testname}-v6.swf SWF6] "
+	echo "[http://www.gnashdev.org/testcases/v7/${testname}-v7.swf SWF7] "
+	echo "[http://www.gnashdev.org/testcases/v8/${testname}-v8.swf SWF8]"
 	echo 
 	} 
 done; 
