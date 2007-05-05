e2e560bf68c95dd175752ac27ea252e981df1f88
strk <>
2007-05-05 17:00:35 +0000

    * testsuite/samples/clip_as_button2-TestRunner.cpp: add pixel           color checking.

--- testsuite/samples/clip_as_button2-TestRunner.cpp
+++ testsuite/samples/clip_as_button2-TestRunner.cpp
@@ -70,6 +70,14 @@ main(int /*argc*/, char** /*argv*/)
 	string msg_botleft = "big movie clip pressed";
 	string msg_botright = "small movie clip pressed";
 
+	rgba yellow(255, 255, 102, 255);
+	rgba gray(159, 159, 159, 255);
+	rgba dark_cyan(25,160,133,255);
+	rgba white(255, 255, 255, 255);
+	rgba blue(51, 0, 204, 255);
+	rgba cyan(0, 255, 204, 255);
+	rgba green(0,255,102,255);
+
 	const character* text = tester.findDisplayItemByDepth(*root, 3+character::staticDepthOffset);
 	check(text);
 	check_equals(string(text->get_text_value()), msg_empty);
@@ -78,117 +86,147 @@ main(int /*argc*/, char** /*argv*/)
 	// click in top-left movie clip circle
 	tester.movePointerTo(176, 92);
 	check(tester.isMouseOverMouseEntity());
-	//TODO: check color under the pixel is yellow
+	// check color under the pixel is yellow
+	check_pixel(176, 92, 2, yellow, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.pressMouseButton();
-	//TODO: check color under the pixel is gray
+	// TODO: check invalidated bounds !
+	// check color under the pixel is gray
+	check_pixel(176, 92, 2, gray, 1);
 	check_equals(string(text->get_text_value()), msg_topleft);
 	tester.depressMouseButton();
-	//TODO: check color under the pixel is yellow
+	// check color under the pixel is yellow
+	check_pixel(176, 92, 2, yellow, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 
 	// click "near" the top-left clip
 	// (ie: inside it's boundin box, but not on the shape)
 	tester.movePointerTo(143, 60);
 	check(!tester.isMouseOverMouseEntity());
-	//TODO: check color under the pixel is white
+	// check color under the pixel is white
+	check_pixel(143, 60, 2, white, 1);
 	tester.pressMouseButton();
-	//TODO: check color under the pixel is white
+	// check color under the pixel is white
+	check_pixel(143, 60, 2, white, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.depressMouseButton();
 
 	// click in top-right movie clip circle
 	tester.movePointerTo(427, 98);
 	check(tester.isMouseOverMouseEntity());
-	//TODO: check color under the pixel is blue
+	// check color under the pixel is blue
+	check_pixel(427, 98, 2, blue, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.pressMouseButton();
-	//TODO: check color under the pixel is green
+	// check color under the pixel is green
+	check_pixel(427, 98, 2, green, 1);
 	check_equals(string(text->get_text_value()), msg_topright);
 	tester.depressMouseButton();
-	//TODO: check color under the pixel is blue
+	// check color under the pixel is blue
+	check_pixel(427, 98, 2, blue, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 
 	// click "near" the top-right clip
 	// (ie: inside it's boundin box, but not on the shape)
 	tester.movePointerTo(385, 56);
 	check(!tester.isMouseOverMouseEntity());
-	//TODO: check color under the pixel is white
+	// check color under the pixel is white
+	check_pixel(385, 56, 2, white, 1);
 	tester.pressMouseButton();
-	//TODO: check color under the pixel is white
+	// check color under the pixel is white
+	check_pixel(385, 56, 2, white, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.depressMouseButton();
 
 	// click in bottom-left movie clip square
 	tester.movePointerTo(68, 281);
 	check(tester.isMouseOverMouseEntity());
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(68, 281, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.pressMouseButton();
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(68, 281, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_botleft);
 	tester.depressMouseButton();
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(68, 281, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 
 	// click near the bottom-left movie clip circle
 	tester.movePointerTo(140, 251);
 	check(tester.isMouseOverMouseEntity());
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(140, 251, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.pressMouseButton();
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(140, 251, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_botleft);
 	tester.depressMouseButton();
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(140, 251, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 
 	// click in the bottom-left movie clip circle
 	tester.movePointerTo(168, 283);
 	check(tester.isMouseOverMouseEntity());
-	//TODO: check color under the pixel is yellow
+	// check color under the pixel is yellow
+	check_pixel(168, 283, 2, yellow, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.pressMouseButton();
-	//TODO: check color under the pixel is yellow
+	// check color under the pixel is yellow
+	check_pixel(168, 283, 2, yellow, 1);
 	check_equals(string(text->get_text_value()), msg_botleft);
 	tester.depressMouseButton();
-	//TODO: check color under the pixel is yellow
+	// check color under the pixel is yellow
+	check_pixel(168, 283, 2, yellow, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 
 	// click in bottom-right movie clip square
 	tester.movePointerTo(330, 284);
 	check(!tester.isMouseOverMouseEntity());
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(330, 284, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.pressMouseButton();
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(330, 284, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.depressMouseButton();
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(330, 284, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 
 	// click near the bottom-right movie clip circle
 	check(!tester.isMouseOverMouseEntity());
 	tester.movePointerTo(404, 252);
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(404, 252, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.pressMouseButton();
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(404, 252, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.depressMouseButton();
-	//TODO: check color under the pixel is light blue
+	// check color under the pixel is light blue
+	check_pixel(404, 252, 2, cyan, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 
 	// click in the bottom-right movie clip circle
 	tester.movePointerTo(434, 291);
 	check(tester.isMouseOverMouseEntity());
-	//TODO: check color under the pixel is yellow
+	// check color under the pixel is yellow
+	check_pixel(434, 291, 2, yellow, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 	tester.pressMouseButton();
-	//TODO: check color under the pixel is yellow
+	// TODO: check invalidated bounds !
+	// check color under the pixel is dark cyan
+	check_pixel(434, 291, 2, dark_cyan, 1);
 	check_equals(string(text->get_text_value()), msg_botright);
 	tester.depressMouseButton();
-	//TODO: check color under the pixel is yellow
+	// check color under the pixel is yellow
+	check_pixel(434, 291, 2, yellow, 1);
 	check_equals(string(text->get_text_value()), msg_empty);
 
 }
