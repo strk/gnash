45c625161e1d7a6feff3b9b6cd7c90aac91b2201
strk <>
2007-05-11 04:03:47 +0000

    * testsuite/misc-ming.all/Makefile.am: enabled           displaylist_depths_test6.swf runner         * testsuite/misc-ming.all/displaylist_depths_test6.c:           Some comments cleanup, and an additional expected failure           (slightly unrelated to test focus, I'd say).

--- testsuite/misc-ming.all/displaylist_depths_test6.c
+++ testsuite/misc-ming.all/displaylist_depths_test6.c
@@ -161,7 +161,7 @@ main(int argc, char** argv)
     " } "
     
     // Static3 refers to same instance
-    "check_equals(static3.myThing, 'guess');" 
+    "xcheck_equals(static3.myThing, 'guess');" // gnash fails as it create a new instance
     // immune to MOVE after swap
     "xcheck_equals(static3._x, 10);" 
     "check_equals(static3.getDepth(), -16381);" 
