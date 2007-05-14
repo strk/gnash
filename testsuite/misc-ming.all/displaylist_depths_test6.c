bd4fb98233d185f0cb0262c9a9d59d2414ed09a3
strk <>
2007-05-14 11:58:29 +0000

    Add name of the test on the wiki

--- testsuite/misc-ming.all/displaylist_depths_test6.c
+++ testsuite/misc-ming.all/displaylist_depths_test6.c
@@ -19,7 +19,7 @@
 /*
  * Zou Lunkai, zoulunkai@gmail.com
  *
- * Test how swapDepth affects DisplayList refresh on gotoAndPlay(current-X).
+ * Test "Jumping backward to the midle of a character's lifetime after swap and swap back"
  *
  * run as ./displaylist_depths_test6
  *
