521c0ce2eacb859013ae9a36d8970108da104bd4
rsavoye <>
2007-05-15 16:04:16 +0000

    Include memory so auto_ptr is defined.

--- testsuite/libbase/CurlStreamTest.cpp
+++ testsuite/libbase/CurlStreamTest.cpp
@@ -14,8 +14,6 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-// 
-
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -23,6 +21,7 @@
 #include "curl_adapter.h"
 #include "tu_file.h"
 
+#include <memory>               // for auto_ptr
 #include <cstdio>
 #include <iostream>
 #include <cassert>
