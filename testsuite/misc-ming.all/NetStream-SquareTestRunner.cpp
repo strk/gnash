7ac598fec76395343cf3fd241850a717f4fb4d88
strk <>
2007-05-16 15:13:05 +0000

    * testsuite/misc-ming.all/: Makefile.am,           NetStream-SquareTestRunner.cpp, Video-EmbedSquareTestRunner.cpp:           Stub video test runners.

---
+++ testsuite/misc-ming.all/NetStream-SquareTestRunner.cpp
@@ -0,0 +1,59 @@
+/* 
+ *   Copyright (C) 2007 Free Software Foundation, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ */ 
+
+#define INPUT_FILENAME "NetStream-SquareTest.swf"
+
+#include "MovieTester.h"
+#include "sprite_instance.h"
+#include "character.h"
+#include "dlist.h"
+#include "container.h"
+#include "log.h"
+
+#include "check.h"
+#include <string>
+#include <cassert>
+
+using namespace gnash;
+using namespace std;
+
+int
+main(int /*argc*/, char** /*argv*/)
+{
+	string filename = string(TGTDIR) + string("/") + string(INPUT_FILENAME);
+	MovieTester tester(filename);
+
+	gnash::LogFile& dbglogfile = gnash::LogFile::getDefaultInstance();
+	dbglogfile.setVerbosity(1);
+
+	sprite_instance* root = tester.getRootMovie();
+	assert(root);
+
+	// Just loop twice, so to catch crashes...
+	// TODO: lots of more tests :)
+	size_t framecount = root->get_frame_count();
+	for (size_t i=0; i<framecount*2; ++i)
+	{
+		check_equals(root->get_current_frame(), i%framecount);
+		tester.advance();
+	}
+
+
+}
+
