c41a37180e0d8e6737865b02e885b092812dc591
ann <>
2007-05-28 14:40:03 +0000

    Fix the copyright notice for this file too.

--- server/asobj/NetStream.h
+++ server/asobj/NetStream.h
@@ -10,6 +10,7 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -18,7 +19,7 @@
 //
 //
 
-/*  $Id: NetStream.h,v 1.35 2007/05/28 15:24:59 tgc Exp $ */
+/*  $Id: NetStream.h,v 1.36 2007/05/28 15:40:03 ann Exp $ */
 
 #ifndef __NETSTREAM_H__
 #define __NETSTREAM_H__
