6b981219b2e9ec875476ec02881a1b2fed59d980
nihilus <>
2007-07-03 18:44:07 +0000

    Fixed bogus #ifdef -> #if

--- libbase/GC.cpp
+++ libbase/GC.cpp
@@ -16,7 +16,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-/* $Id: GC.cpp,v 1.5 2007/07/03 16:06:20 strk Exp $ */
+/* $Id: GC.cpp,v 1.6 2007/07/03 19:44:07 nihilus Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -73,7 +73,7 @@ GC::~GC()
 void
 GC::cleanUnreachable()
 {
-#ifdef GNASH_GC_DEBUG > 1
+#if (GNASH_GC_DEBUG > 1)
 	size_t deleted = 0;
 	log_debug(_("GC %p: SWEEP SCAN"), (void*)this);
 #endif
