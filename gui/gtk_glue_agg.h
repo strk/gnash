0f18c7d2876bf0509fbb24e7dabc59e1879a3ef8
strk <>
2007-07-31 15:43:47 +0000

    don't miss to include config.h

--- gui/gtk_glue_agg.h
+++ gui/gtk_glue_agg.h
@@ -14,9 +14,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+//
 
-//
-//
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 #include "gtk_glue.h"
 
