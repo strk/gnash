faeceb6a6e7293ca0fc3db9ca39f42d025d85aa4
nihilus <>
2007-08-27 10:58:46 +0000

    defined exp2() as it's missing on some BSDen. Thx to deanna for the patch.

--- libbase/utility.h
+++ libbase/utility.h
@@ -114,6 +114,8 @@ inline float flerp(float a, float b, float f) { return (b - a) * f + a; }
 const float LN_2 = 0.693147180559945f;
 // the overridden log(f) will use logf IFF f is a float
 inline float	log2(float f) { return std::log(f) / LN_2; }
+//exp2 might be missing on Net-/OpenBSD.
+inline float	exp2(double d, double x) { return std::pow((double)d, double(x)); }
 inline int	fchop( float f ) { return (int) f; }	// replace w/ inline asm if desired
 inline int	frnd(float f) { return fchop(f + 0.5f); }	// replace with inline asm if desired
 
