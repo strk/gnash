dcd9502e53ccdbf9aaa49b78ce2bc9ba67b0228a
bwy <>
2008-03-28 10:13:26 +0000

    Add FSF to copyright holders.
    Is this correct? The modifications are fairly trivial at the moment,
    but there are some differences.

--- libbase/arg_parser.h
+++ libbase/arg_parser.h
@@ -1,39 +1,40 @@
 //  Arg_parser - A POSIX/GNU command line argument parser.
 //    Copyright (C) 2006, 2007, 2008 Antonio Diaz Diaz.
-
+//    Copyright (C) 2008 Free Software Foundation, Inc.
+//
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
-
+//
 //    This program is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //    GNU General Public License for more details.
-
+//
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
+//
+//
 //  Arg_parser reads the arguments in `argv' and creates a number of
 //    option codes, option arguments and non-option arguments.
-
+//
 //    In case of error, `error' returns a non-empty error message.
-
+//
 //    `options' is an array of `struct Option' terminated by an element
 //    containing a code which is zero. A null name means a short-only
 //    option. A code value outside the unsigned char range means a
 //    long-only option.
-
+//
 //    Arg_parser normally makes it appear as if all the option arguments
 //    were specified before all the non-option arguments for the purposes
 //    of parsing, even if the user of your program intermixed option and
 //    non-option arguments. If you want the arguments in the exact order
 //    the user typed them, call `Arg_parser' with `in_order' = true.
-
+//
 //    The argument `--' terminates all options; any following arguments are
 //    treated as non-option arguments, even if they begin with a hyphen.
-
+//
 //    The syntax for optional option arguments is `-<short_option><argument>'
 //    (without whitespace), or `--<long_option>=<argument>'.
 
