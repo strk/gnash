f1aac08c3fee09c57f61840cd14fea68083cc6c2
nihilus <>
2008-03-29 16:13:28 +0000

    reinterprer_cast from pthread_self(), albeit I'm pretty sure we should return pthread_t

--- libbase/utility.h
+++ libbase/utility.h
@@ -241,9 +241,9 @@ inline unsigned long int /* pthread_t */ get_thread_id(void)
     return reinterpret_cast<unsigned long int>(pthread_self());
 #else
 # ifdef _WIN32
-    return GetCurrentThreadId();
+    return reinterpret_cast<unsigned long int>(GetCurrentThreadId());
 # else
-    return getpid();
+    return reinterpret_cast<unsigned long int>(getpid());
 # endif
 #endif
 }
