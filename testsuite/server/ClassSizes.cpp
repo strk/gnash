adb3a1842ed14083f063357cc180dc36d886a8d0
strk <>
2008-04-24 12:42:59 +0000

    size of long and double

--- testsuite/server/ClassSizes.cpp
+++ testsuite/server/ClassSizes.cpp
@@ -46,6 +46,9 @@ main(int /*argc*/, char** /*argv*/)
 	std::cout << "sizeof(int): " << (sizeof(int)) << std::endl;
 	std::cout << "sizeof(float): " << (sizeof(float)) << std::endl;
 
+	std::cout << "sizeof(long): " << (sizeof(long)) << std::endl;
+	std::cout << "sizeof(double): " << (sizeof(double)) << std::endl;
+
 	std::cout << "sizeof(as_value): " << (sizeof(as_value)) << std::endl;
 
 	std::cout << "sizeof(Property): " << (sizeof(Property)) << std::endl;
