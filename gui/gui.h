8f31ab2d483f8b80ef573f747c5c28d4ceae32fd
Benjamin Wolsey <bwy@benjaminwolsey.de>
2008-07-16 07:44:50 +0200

    Change header guard.

--- gui/gui.h
+++ gui/gui.h
@@ -15,8 +15,8 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-#ifndef HAVE_GUI_H
-#define HAVE_GUI_H
+#ifndef GNASH_GUI_H
+#define GNASH_GUI_H
 
 #ifdef HAVE_CONFIG_H
 #include "gnashconfig.h"
