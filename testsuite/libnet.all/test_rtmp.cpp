9a2cbd85015f1780ef127c98c8c4bba964b956f6
rob@welcomehome.org <>
2008-08-02 16:57:37 -0600

    Don't crash if we get a bad pointer returned.

--- testsuite/libnet.all/test_rtmp.cpp
+++ testsuite/libnet.all/test_rtmp.cpp
@@ -197,8 +197,12 @@ test_split()
     // message.
     Buffer *buf1 = hex2mem("04 00 00 00 00 00 b8 14 01 00 00 00 02 00 08 6f 6e 53 74 61 74 75 73 00 00 00 00 00 00 00 00 00 05 03 00 05 6c 65 76 65 6c 02 00 06 73 74 61 74 75 73 00 04 63 6f 64 65 02 00 14 4e 65 74 53 74 72 65 61 6d 2e 50 6c 61 79 2e 52 65 73 65 74 00 0b 64 65 73 63 72 69 70 74 69 6f 6e 02 00 2d 50 6c 61 79 69 6e 67 20 61 6e 64 20 72 65 73 65 74 74 69 6e 67 20 67 61 74 65 30 36 5f 74 61 62 6c 61 6e 5f 62 63 75 65 75 5f 30 31 2e c4 00 07 64 65 74 61 69 6c 73 02 00 16 67 61 74 65 30 36 5f 74 61 62 6c 61 6e 5f 62 63 75 65 75 5f 30 31 00 08 63 6c 69 65 6e 74 69 64 00 41 bf e4 78 30 00 00 00 00 00 09");
     RTMP::queues_t *queues1 = client.split(buf1, 128);
-    if (queues1->size() == 0) {
+    if (queues1 == 0) {
         notest = true;
+    } else {
+        if (queues1->size() == 0) {
+            notest = true;
+        }
     }
     
     if (notest) {
@@ -228,20 +232,28 @@ test_split()
         }
     }
 
-    if ((queues1->size() == 0) && (client[4].size() == 0)) {
-        runtest.pass("RTMP::split() 1st queue empty");
+    if (notest) {
+        runtest.untested("RTMP::split(1st packet header) of 2");        
     } else {
-        runtest.fail("RTMP::split() 1st queue empty" );
+        if ((queues1->size() == 0) && (client[4].size() == 0)) {
+            runtest.pass("RTMP::split() 1st queue empty");
+        } else {
+            runtest.fail("RTMP::split() 1st queue empty" );
+        }
     }
-    
 
     // Test a bigger buffer with more messages in it.
-    // This one is a ChunkSize->Ping->Invoke->Ping
+    // This one is a ChunkSize->Ping->Invoke->Ping.
     Buffer *buf2 = hex2mem("02 00 00 00 00 00 04 01 00 00 00 00 00 00 00 80 02 00 00 00 00 00 06 04 00 00 00 00 00 04 00 00 00 01 04 00 00 00 00 00 b8 14 01 00 00 00 02 00 08 6f 6e 53 74 61 74 75 73 00 00 00 00 00 00 00 00 00 05 03 00 05 6c 65 76 65 6c 02 00 06 73 74 61 74 75 73 00 04 63 6f 64 65 02 00 14 4e 65 74 53 74 72 65 61 6d 2e 50 6c 61 79 2e 52 65 73 65 74 00 0b 64 65 73 63 72 69 70 74 69 6f 6e 02 00 2d 50 6c 61 79 69 6e 67 20 61 6e 64 20 72 65 73 65 74 74 69 6e 67 20 67 61 74 65 30 36 5f 74 61 62 6c 61 6e 5f 62 63 75 65 75 5f 30 31 2e 02 00 00 00 00 00 06 04 00 00 00 00 00 00 00 00 00 01 c4 00 07 64 65 74 61 69 6c 73 02 00 16 67 61 74 65 30 36 5f 74 61 62 6c 61 6e 5f 62 63 75 65 75 5f 30 31 00 08 63 6c 69 65 6e 74 69 64 00 41 d8 fb 78 56 00 00 00 00 00 09");
     RTMP::queues_t *queues2 = client.split(buf2, 128);
-    if (queues2->size() == 0) {
+    if (queues2 == 0) {
         notest = true;
+    } else {
+        if (queues2->size() == 0) {
+            notest = true;
+        }
     }
+    
     if (notest) {
         runtest.fail("RTMP::split(5 packets into 4)");
     } else {
@@ -313,11 +325,15 @@ test_split()
         }
     }
 
-    if ((queues2->size() == 0) && (client[2].size() == 0)
-        && (client[4].size() == 0)) {
-        runtest.pass("RTMP::split() 2nd queue empty");
+    if (notest) {
+        runtest.untested("RTMP::split() 2nd queue empty" );
     } else {
-        runtest.fail("RTMP::split() 2nd queue empty" );
+        if ((queues2->size() == 0) && (client[2].size() == 0)
+            && (client[4].size() == 0)) {
+            runtest.pass("RTMP::split() 2nd queue empty");
+        } else {
+            runtest.fail("RTMP::split() 2nd queue empty" );
+        }
     }
 
 // Try a much more complex packet, similar to the previous one, but with more
@@ -326,9 +342,14 @@ test_split()
 //    ...............onStatus.............level...status..code...NetStream.Play.Start..description..'Started playing gate06_tablan_bcueu_01...clie......'.......xF....?j.....@....?..O.]...............................;...../..rP.....K.......m......,......%......................B........M.<.$.....`.......i..9..C..J..........%..........G....2Np.".1`@................;.ntid.A..xV.....
     Buffer *buf3 = hex2mem("05 00 00 00 00 00 90 14 01 00 00 00 02 00 08 6f 6e 53 74 61 74 75 73 00 00 00 00 00 00 00 00 00 05 03 00 05 6c 65 76 65 6c 02 00 06 73 74 61 74 75 73 00 04 63 6f 64 65 02 00 14 4e 65 74 53 74 72 65 61 6d 2e 50 6c 61 79 2e 53 74 61 72 74 00 0b 64 65 73 63 72 69 70 74 69 6f 6e 02 00 27 53 74 61 72 74 65 64 20 70 6c 61 79 69 6e 67 20 67 61 74 65 30 36 5f 74 61 62 6c 61 6e 5f 62 63 75 65 75 5f 30 31 2e 00 08 63 6c 69 65 07 00 00 00 00 00 27 09 01 00 00 00 14 00 78 46 0f 14 0f 14 3f 6a ff ff 00 08 9f 40 10 9f f8 8b 3f fd b2 4f fb 5d c0 00 00 00 00 00 00 00 00 00 00 00 00 08 00 00 00 00 00 00 08 01 00 00 00 08 00 00 00 00 01 3b 08 01 00 00 00 2f ff fb 72 50 00 00 00 00 00 4b 00 00 00 00 07 e0 09 6d 00 00 00 00 00 01 2c 00 00 00 00 1f 80 25 b4 00 00 00 ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff fc 0c 87 42 80 ec c8 b0 0e 90 c2 12 4d 90 3c 18 24 16 01 88 03 e1 60 1a 1a a0 1a 09 9c 1a 69 a1 10 39 06 8d 43 02 c3 4a 12 0b 00 c8 1f 0b 00 d8 16 00 25 9f ff ff fe c1 a0 00 00 ff 8a 47 80 80 0e 1e 32 4e 70 f1 22 ed 31 60 40 f8 02 00 00 00 00 00 04 01 00 00 00 00 00 00 01 3b c5 6e 74 69 64 00 41 d8 fb 78 56 00 00 00 00 00 09");
     RTMP::queues_t *queues3 = client.split(buf3, 128);
-    if (queues3->size() == 0) {
+    if (queues3 == 0) {
         notest = true;
-    }    
+    } else {
+        if (queues3->size() == 0) {
+            notest = true;
+        }
+    }
+    
     if (notest) {
         runtest.untested("RTMP::split(6 complex packets into )");
     } else {
