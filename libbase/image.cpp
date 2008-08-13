b22aeb6d620594a7d431e0dbc9702944655f8df7
Benjamin Wolsey <bwy@benjaminwolsey.de>
2008-08-13 17:01:08 +0200

    Add FSF copyright to image.{h,cpp}, as they aren't much like the original any more.

--- libbase/image.cpp
+++ libbase/image.cpp
@@ -1,9 +1,23 @@
-// image.cpp	-- Thatcher Ulrich <tu@tulrich.com> 2002
+// Image.cpp: image data class for Gnash.
+// 
+//   Copyright (C) 2006, 2007, 2008 Free Software Foundation, Inc.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
 
-// This source code has been donated to the Public Domain.  Do
-// whatever you want with it.
-
-// Handy image utilities for RGB surfaces.
+// Based on the public domain work of Thatcher Ulrich <tu@tulrich.com> 2002
 
 #include <cstring>
 #include <memory>		// for auto_ptr
