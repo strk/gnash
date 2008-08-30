4ea7d169e7fb966d3373e1690840a2d0fe003b9a
Sandro Santilli <strk@keybit.net>
2008-08-30 11:44:45 +0200

    log unimplemented for blend modes, bitmap filters and bitmap caching

--- libcore/swf/PlaceObject2Tag.cpp
+++ libcore/swf/PlaceObject2Tag.cpp
@@ -411,12 +411,32 @@ PlaceObject2Tag::readPlaceObject3(SWFStream& in)
     {
         Filters v; // TODO: Attach the filters to the display object.
         filter_factory::read(in, true, &v);
+	// at time of writing no renderer supports bitmap filters
+	LOG_ONCE( log_unimpl("Bitmap filters") );
     }
 
     if ( hasBlendMode() )
     {
         in.ensureBytes(1);
         blend_mode = in.read_u8();
+	// 0 or 1 : normal
+	// 2 : layer
+	// 3 : multiply
+	// 4 : screen
+	// 5 : lighten
+	// 6 : darken
+	// 7 : add
+	// 8 : subtract
+	// 9 : difference
+	// 10 : invert
+	// 11 : alpha
+	// 12 : erase
+	// 13 : overlay
+	// 14 : hardlight
+	// 15 to 255 reserved	
+	//
+	// at time of writing no renderer supports blend modes
+	LOG_ONCE( log_unimpl("Blend mode") );
     }
 
     if ( hasBitmapCaching() )
@@ -425,6 +445,8 @@ PlaceObject2Tag::readPlaceObject3(SWFStream& in)
         // is broken, it is probably here!
         in.ensureBytes(1);
         bitmask = in.read_u8();
+	// at time of writing no renderer supports bitmap caching 
+	LOG_ONCE( log_unimpl("Bitmap caching") );
     }
 
     if ( hasClipActions() )
