4157dfa65e9f3bc402caeb536f626aaef2175eaf
Sandro Santilli <strk@keybit.net>
2008-09-16 21:00:59 +0200

    Use named strings where appropriate, shake code a bit in array

--- libcore/array.cpp
+++ libcore/array.cpp
@@ -1410,7 +1410,7 @@ array_new(const fn_call& fn)
     log_action(_("array_new called, nargs = %d"), fn.nargs);
     );
 
-    boost::intrusive_ptr<as_array_object>    ao = new as_array_object;
+    boost::intrusive_ptr<as_array_object> ao = new as_array_object;
 
     if (fn.nargs == 0)
     {
@@ -1447,7 +1447,7 @@ attachArrayProperties(as_object& proto)
     as_c_function_ptr gettersetter;
 
     gettersetter = &array_length;
-    proto.init_property("length", *gettersetter, *gettersetter);
+    proto.init_property(NSV::PROP_LENGTH, *gettersetter, *gettersetter);
 }
 
 static void
@@ -1530,6 +1530,25 @@ getArrayInterface()
     return proto.get();
 }
 
+static as_function*
+getArrayConstructor(VM& vm)
+{
+    // This is going to be the global Array "class"/"function"
+    static as_function* ar=0;
+
+    if ( ar == NULL )
+    {
+        vm.registerNative(array_new, 252, 0);
+        ar = new builtin_function(&array_new, getArrayInterface());
+        vm.addStatic(ar);
+
+        // Attach static members
+        attachArrayStatics(*ar);
+    }
+
+    return ar;
+}
+
 // this registers the "Array" member on a "Global"
 // object. "Array" is a constructor, thus an object
 // with .prototype full of exported functions + 
@@ -1538,22 +1557,8 @@ getArrayInterface()
 void
 array_class_init(as_object& glob)
 {
-    // This is going to be the global Array "class"/"function"
-    static boost::intrusive_ptr<as_function> ar=NULL;
-
-    if ( ar == NULL )
-    {
-        VM& vm = glob.getVM();
-        vm.registerNative(array_new, 252, 0);
-        ar = new builtin_function(&array_new, getArrayInterface());
-        vm.addStatic(ar.get());
-
-        // Attach static members
-        attachArrayStatics(*ar);
-    }
-
     // Register _global.Array
-    glob.init_member("Array", ar.get());
+    glob.init_member("Array", getArrayConstructor(glob.getVM()));
 }
 
 void
