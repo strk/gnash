1d3355617ea8a98c3a8f83ab4fff80ccf06443d9
Benjamin Wolsey <bwy@benjaminwolsey.de>
2008-10-08 12:47:01 +0200

    Add simple MovieTester-based runner for EmbeddedSound, enable automatic run of test.

---
+++ testsuite/misc-ming.all/EmbeddedSoundTest-Runner.cpp
@@ -0,0 +1,58 @@
+/* 
+ *   Copyright (C) 2007, 2008 Free Software Foundation, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ */ 
+
+#define INPUT_FILENAME "EmbeddedSoundTest.swf"
+
+#include "MovieTester.h"
+#include "sprite_instance.h"
+#include "character.h"
+#include "DisplayList.h"
+#include "log.h"
+
+#include "check.h"
+#include <string>
+#include <cassert>
+
+using namespace gnash;
+using namespace std;
+
+int
+main(int /*argc*/, char** /*argv*/)
+{
+	string filename = string(TGTDIR) + string("/") + string(INPUT_FILENAME);
+	MovieTester tester(filename);
+
+	gnash::LogFile& dbglogfile = gnash::LogFile::getDefaultInstance();
+	dbglogfile.setVerbosity(1);
+
+	sprite_instance* root = tester.getRootMovie();
+	assert(root);
+
+    const size_t framecount = root->get_frame_count();
+
+	while (true)
+	{
+        if (root->get_current_frame() + 1 == framecount) break;
+
+		tester.advance();
+	}
+
+}
+
