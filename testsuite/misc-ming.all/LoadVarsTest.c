d54e60131eab4f646b802387a9e3665b25ac3533
Benjamin Wolsey <bwy@benjaminwolsey.de>
2008-10-26 10:15:50 +0100

    Add more tests and an empty file for loading tests.

--- testsuite/misc-ming.all/LoadVarsTest.c
+++ testsuite/misc-ming.all/LoadVarsTest.c
@@ -30,192 +30,241 @@
 int
 main(int argc, char** argv)
 {
-	SWFMovie mo;
-	const char *srcdir=".";
-	SWFMovieClip  dejagnuclip;
+    SWFMovie mo;
+    const char *srcdir=".";
+    SWFMovieClip  dejagnuclip;
 
-	char loadvars[1048];
+    char loadvars[1048];
+    char loademptyvars[1048];
 
-	/*********************************************
-	 *
-	 * Initialization
-	 *
-	 *********************************************/
+    /*********************************************
+     *
+     * Initialization
+     *
+     *********************************************/
 
-	if ( argc>1 ) srcdir=argv[1];
-	else
-	{
-		fprintf(stderr, "Usage: %s\n", argv[0]);
-		return 1;
-	}
+    if ( argc>1 ) srcdir=argv[1];
+    else
+    {
+       fprintf(stderr, "Usage: %s\n", argv[0]);
+       return 1;
+    }
 
+    sprintf(loadvars, "e = l.load('%s/vars2.txt');", srcdir);
+    sprintf(loademptyvars, "e = l.load('%s/empty.txt');", srcdir);
+    puts("Setting things up");
 
-	sprintf(loadvars, "e = l.load('%s/vars2.txt');", srcdir);
-	puts("Setting things up");
+    Ming_init();
+    Ming_useSWFVersion (OUTPUT_VERSION);
+    Ming_setScale(20.0); /* let's talk pixels */
 
-	Ming_init();
-	Ming_useSWFVersion (OUTPUT_VERSION);
-	Ming_setScale(20.0); /* let's talk pixels */
- 
-	mo = newSWFMovie();
-	SWFMovie_setRate(mo, 12);
-	SWFMovie_setDimension(mo, 640, 400);
+    mo = newSWFMovie();
+    SWFMovie_setRate(mo, 12);
+    SWFMovie_setDimension(mo, 640, 400);
 
-	/*********************************************
-	 *
-	 * Body
-	 *
-	 *********************************************/
+    /*********************************************
+     *
+     * Body
+     *
+     *********************************************/
 
-	dejagnuclip = get_dejagnu_clip((SWFBlock)get_default_font(srcdir),
-			10, 0, 80, 800, 600);
-	SWFMovie_add(mo, (SWFBlock)dejagnuclip);
+    dejagnuclip = get_dejagnu_clip((SWFBlock)get_default_font(srcdir),
+                 10, 0, 80, 800, 600);
+    SWFMovie_add(mo, (SWFBlock)dejagnuclip);
 
-	/// Construct LoadVars and backup methods.
-	add_actions(mo, "l = new LoadVars;"
-			"odatB = LoadVars.prototype.onData;"
-			"olB = LoadVars.prototype.onLoad;"
-			"odecB = LoadVars.prototype.decode;"
-			"loadString = '';"
-			"decodeString = '';"
-			"dataString = '';");
+    add_actions(mo, 
+                 "odatB = LoadVars.prototype.onData;"
+                 "olB = LoadVars.prototype.onLoad;"
+                 "odecB = LoadVars.prototype.decode;"
+                 "loadString = '';"
+                 "decodeString = '';"
+                 "dataString = '';");
 
-	add_actions(mo, "ourLoad = function(arg) {"
-			"loadString += 'onLoad called with ' + typeof(arg)" 
-			"+ ' argument ' + arg;"
-			"play();"
-			"};");
+    add_actions(mo, "ourLoad = function(arg) {"
+                 "loadString += 'onLoad called with ' + typeof(arg)" 
+                 "+ ' argument ' + arg;"
+                 "play();"
+                 "};");
 
-	add_actions(mo, "ourData = function(arg) {"
-			"dataString += 'onData called with ' + typeof(arg) "
-			"+ ' argument ' + arg;"
-			"play();"
-			"};");
+    add_actions(mo, "ourData = function(arg) {"
+                 "dataString += 'onData called with ' + typeof(arg) "
+                 "+ ' argument ' + arg;"
+                 "play();"
+                 "};");
 
-	
-	add_actions(mo, "decodeCalled = 0;");
-	add_actions(mo, "ourDecode = function(arg) {"
-			"decodeString += 'decode called with ' + typeof(arg)"
-			"+ ' argument ' + arg;"
-			"decodeCalled++;"
-			"};");
+   
+    add_actions(mo, "decodeCalled = 0;");
+    add_actions(mo, "ourDecode = function(arg) {"
+                 "decodeString += 'decode called with ' + typeof(arg)"
+                 "+ ' argument ' + arg;"
+                 "decodeCalled++;"
+                 "};");
 
-	/// The decode method is stays overwritten to see where it gets
-	/// called from. We don't call it ourselves. Don't forget to 
-	/// overwrite it again when the LoadVars object is construct again.
-	add_actions(mo, "l.decode = ourDecode;");
+    /// The decode method is overwritten to see where it gets
+    /// called from. We don't call it ourselves. Don't forget to 
+    /// overwrite it again when the LoadVars object is constructed again.
 
+    /// What happens when load fails?
+    //
+    /// The onLoad method is called from the default implementation of
+    /// onData. The "loaded" property is immediately set to false on a 
+    /// call to "load".
+   
+    SWFMovie_nextFrame(mo);
 
-	/// What happens when load fails?
-	//
-	/// The onLoad method is called from the default implementation of onData.
-	
-	SWFMovie_nextFrame(mo);
+    /// onData
+    add_actions(mo, "l = new LoadVars;");
+    check_equals(mo, "l.loaded", "undefined");
+    add_actions(mo, "l.decode = ourDecode;");
 
-	/// onData
-	add_actions(mo, "l.onData = ourData;"
-			"dataString = '';"
-			"e = l.load('fail');");
-	check_equals(mo, "e", "true");
-	add_actions(mo, "stop();");
+    add_actions(mo, "l.onData = ourData;"
+                 "dataString = '';"
+                 "e = l.load('fail');");
+    check_equals(mo, "e", "true");
+    add_actions(mo, "stop();");
 
-	/// Wait for data before proceeding to next frame.
-	SWFMovie_nextFrame(mo);
-	
-	// Check result, restore builtin method.
-	xcheck_equals(mo, "dataString",
-			"'onData called with undefined argument undefined'");
-	add_actions(mo, "l.onData = odatB;");
-
-	/// onLoad
-	add_actions(mo, "l.onLoad = ourLoad;"
-			"loadString = '';"
-			"e = l.load('fail');");
-	check_equals(mo, "e", "true");
-	add_actions(mo, "stop();");
-
-	SWFMovie_nextFrame(mo);
-		
-	xcheck_equals(mo, "loadString",
-			"'onLoad called with boolean argument false'");
-	add_actions(mo, "l.onLoad = olB;");
-
-    /// Both onData and onLoad
-	add_actions(mo, "l.onLoad = ourLoad;"
-            "l.onData = ourData;"
-			"loadString = '';"
-            "dataString = '';"
-			"e = l.load('fail');");
-	check_equals(mo, "e", "true");
-	add_actions(mo, "stop();");
-
-	SWFMovie_nextFrame(mo);
-		
-	check_equals(mo, "loadString",
-			"''");
-	xcheck_equals(mo, "dataString",
-			"'onData called with undefined argument undefined'");
-	add_actions(mo, "l.onLoad = olB;");
+    /// Wait for data before proceeding to next frame.
+    SWFMovie_nextFrame(mo);
+   
+    // Check result, restore state.
+    xcheck_equals(mo, "dataString",
+                 "'onData called with undefined argument undefined'");
+    check_equals(mo, "l.loaded", "false");
     add_actions(mo, "l.onData = odatB;");
 
-	/// What happens when load succeeds?
-	//
-	/// Both methods are called separately.
+    /// onLoad
+    add_actions(mo, "l = new LoadVars;");
+    check_equals(mo, "l.loaded", "undefined");
+    add_actions(mo, "l.decode = ourDecode;");
 
-	// onData
-	add_actions(mo, "l = new LoadVars; l.onData = ourData;"
-            "l.decode = ourDecode;"
-			"dataString = '';");
-	add_actions(mo, loadvars);
-	check_equals(mo, "e", "true");
-	add_actions(mo, "stop();");
+    add_actions(mo, "l.onLoad = ourLoad;"
+                 "loadString = '';"
+                 "e = l.load('fail');");
+    check_equals(mo, "e", "true");
+    add_actions(mo, "stop();");
 
-	SWFMovie_nextFrame(mo);
+    SWFMovie_nextFrame(mo);
+          
+    xcheck_equals(mo, "loadString",
+                 "'onLoad called with boolean argument false'");
+    check_equals(mo, "l.loaded", "false");
+    add_actions(mo, "l.onLoad = olB;");
+
+    /// Both onData and onLoad
+    add_actions(mo, "l = new LoadVars;");
+    check_equals(mo, "l.loaded", "undefined");
+    add_actions(mo, "l.decode = ourDecode;");
+
+    add_actions(mo, "l.onLoad = ourLoad;"
+                    "l.onData = ourData;"
+                    "loadString = '';"
+                    "dataString = '';"
+                    "e = l.load('fail');");
+    check_equals(mo, "e", "true");
+    add_actions(mo, "stop();");
+
+    SWFMovie_nextFrame(mo);
+          
+    check_equals(mo, "loadString", "''");
+    xcheck_equals(mo, "dataString",
+                 "'onData called with undefined argument undefined'");
+    check_equals(mo, "l.loaded", "false");
+    add_actions(mo, "l.onLoad = olB;"
+                    "l.onData = odatB;");
+
+    /// What happens when load succeeds?
+    //
+    /// Both methods are called separately. The "loaded" property is set
+    /// to true only when "onData" isn't overwritten.
+
+    // onData
+    add_actions(mo, "l = new LoadVars;");
+    check_equals(mo, "l.loaded", "undefined");
+    add_actions(mo, "l.decode = ourDecode;");
+
+    add_actions(mo, "l.onData = ourData;"
+                     "dataString = '';");
+    add_actions(mo, loadvars);
+    check_equals(mo, "e", "true");
+    add_actions(mo, "stop();");
+
+    SWFMovie_nextFrame(mo);
     // check_equals is too braindead to do this without escaping.
-	xcheck_equals(mo, "escape(dataString)",
-			"'onData%20called%20with%20string%20argument%20v2%5Fvar1%3D"
-            "val1%26v2%5Fvar2%3Dval2%26%0A'");
-	add_actions(mo, "l.onData = odatB;");
+    xcheck_equals(mo, "escape(dataString)",
+           "'onData%20called%20with%20string%20argument%20v2%5Fvar1%3D"
+        "val1%26v2%5Fvar2%3Dval2%26%0A'");
+    check_equals(mo, "l.loaded", "false");
+    add_actions(mo, "l.onData = odatB;");
 
     check_equals(mo, "decodeCalled", "0");
     check_equals(mo, "decodeString", "''");
 
-	// onLoad
-	add_actions(mo, "l = new LoadVars; l.onLoad = ourLoad;"
-            "l.decode = ourDecode;"
-			"loadString = '';");
-	add_actions(mo, loadvars);
-	check_equals(mo, "e", "true");
-	add_actions(mo, "stop();");
+    // onLoad
+    add_actions(mo, "l = new LoadVars;");
+    check_equals(mo, "l.loaded", "undefined");
+    add_actions(mo, "l.decode = ourDecode;");
 
-	SWFMovie_nextFrame(mo);
-	check_equals(mo, "loadString",
-			"'onLoad called with boolean argument true'");
-	add_actions(mo, "l.onLoad = olB;");
+    add_actions(mo, "l.onLoad = ourLoad;"
+                    "loadString = '';");
+    add_actions(mo, loadvars);
+    check_equals(mo, "e", "true");
+    add_actions(mo, "stop();");
+
+    SWFMovie_nextFrame(mo);
+    check_equals(mo, "loadString",
+                 "'onLoad called with boolean argument true'");
+    add_actions(mo, "l.onLoad = olB;");
 
     /// decode is called from onData (i.e. it's called when we overwrite
     /// onLoad, not onData).
     xcheck_equals(mo, "decodeCalled", "1");
     // check_equals is too braindead to do this without escaping.
-	xcheck_equals(mo, "escape(decodeString)",
-			"'decode%20called%20with%20string%20argument%20v2%5Fvar1%3D"
-            "val1%26v2%5Fvar2%3Dval2%26%0A'");
+    xcheck_equals(mo, "escape(decodeString)",
+              "'decode%20called%20with%20string%20argument%20v2%5Fvar1%3D"
+        "val1%26v2%5Fvar2%3Dval2%26%0A'");
 
-	/// End of tests.
 
-	add_actions(mo, "totals(15);");
-	add_actions(mo, "stop();");
+    /// What happens on load of empty data?
+    //
+    /// Same as a load failure.
 
-	/*****************************************************
-	 *
-	 * Output movie
-	 *
-	 *****************************************************/
+    // onData
+    add_actions(mo, "l = new LoadVars;");
+    check_equals(mo, "l.loaded", "undefined");
+    add_actions(mo, "l.decode = ourDecode;");
 
-	puts("Saving " OUTPUT_FILENAME );
+    add_actions(mo, "l.onData = ourData;"
+                     "dataString = '';");
+    add_actions(mo, loademptyvars);
+    check_equals(mo, "e", "true");
+    add_actions(mo, "stop();");
 
-	SWFMovie_save(mo, OUTPUT_FILENAME);
+    SWFMovie_nextFrame(mo);
+    // check_equals is too braindead to do this without escaping.
+    xcheck_equals(mo, "escape(dataString)",
+           "'onData%20called%20with%20undefined%20argument%20undefined'");
+    check_equals(mo, "l.loaded", "false");
+    add_actions(mo, "l.onData = odatB;");
 
-	return 0;
+    // No more calls to decode.
+    check_equals(mo, "decodeCalled", "1");
+
+
+    /// End of tests.
+
+   add_actions(mo, "totals(29);");
+   add_actions(mo, "stop();");
+
+   /*****************************************************
+    *
+    * Output movie
+    *
+    *****************************************************/
+
+   puts("Saving " OUTPUT_FILENAME );
+
+   SWFMovie_save(mo, OUTPUT_FILENAME);
+
+   return 0;
 }
 
