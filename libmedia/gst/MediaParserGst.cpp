ec1790d0f5ccf4ca5625667e91655b159d3f6209
strk <strk@gnash>
2008-11-08 19:01:44 +0100

    Don't throw an exception if no streams were found at construction time, it's not required anymore is it?

--- libmedia/gst/MediaParserGst.cpp
+++ libmedia/gst/MediaParserGst.cpp
@@ -86,9 +86,11 @@ MediaParserGst::MediaParserGst(std::auto_ptr<IOChannel> stream)
 
     log_debug(_("Needed %d dead iterations to detect audio type."), counter);
     
+#if 0
     if (! (_videoInfo.get() || _audioInfo.get()) ) {
         throw MediaException(_("MediaParserGst failed to detect any stream types."));    
     }
+#endif
     
     if (!gst_element_set_state (_bin, GST_STATE_PLAYING) == GST_STATE_CHANGE_SUCCESS) {
         throw MediaException(_("MediaParserGst could not change element state"));
