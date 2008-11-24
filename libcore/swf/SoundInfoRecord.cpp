8802d87640a76e30e893eb709405b534cd3cad85
strk <strk@gnash>
2008-11-24 09:58:09 +0100

    Don't warn about unsupported syncNoMultiple in parser, as the parser does support it

--- libcore/swf/SoundInfoRecord.cpp
+++ libcore/swf/SoundInfoRecord.cpp
@@ -41,10 +41,6 @@ SoundInfoRecord::read(SWFStream& in)
 	hasOutPoint = flags & (1 << 1); 
 	hasInPoint  = flags & (1 << 0);  
     
-    if (noMultiple) {
-        LOG_ONCE(log_unimpl("syncNoMultiple flag in SoundInfo record"));
-    }
-
     in.ensureBytes(hasInPoint * 4 + hasOutPoint * 4 + hasLoops * 2);
 
 	if (hasInPoint) {
