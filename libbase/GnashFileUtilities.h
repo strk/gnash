fed7241c0d7f881161ea2f35eb02c12520c0546d
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-01-27 13:31:10 +0100

    Copyright boilerplate and header descriptions.

--- libbase/GnashFileUtilities.h
+++ libbase/GnashFileUtilities.h
@@ -1,17 +1,30 @@
-// Compatibility system filesystem headers for Gnash
+// GnashFileUtilities.h     File handling for Gnash
+// 
+//   Copyright (C) 2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
-// Include this file for stat(), fstat(), lstat(), dup()
 
+/// This file should also be included for the following system functions:
+//
+/// stat()
+/// fstat()
+/// lstat()
+/// dup()
 #ifndef GNASH_FILE_UTILITIES_H
 #define GNASH_FILE_UTILITIES_H
-
-/// Always include unistd.h unless compiling under MSVC++
-//
-/// This isn't always necessary on GNU/Linux under gcc, but it's still good
-/// practice to include it.
-//
-/// @todo A configure 'test' (a #define in gnashconfig.h) might be a better
-///       way of checking for compiler.
 
 #include "gnashconfig.h"
 #if !defined(_MSC_VER)
