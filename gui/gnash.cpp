14f1bb06a0a26df505e8683a3cfe9b7729605fa9
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-02-06 16:30:47 +0100

    Rename no-menu to hide-menubar, as it doesn't hide the right-click menu.
    Drop mystery 'm' option.

--- gui/gnash.cpp
+++ gui/gnash.cpp
@@ -124,7 +124,7 @@ cout << _("Usage: gnash [options] movie_file.swf") << endl
     << endl
     << _("  --max-advances num       Exit after specified number of advances") << endl
     << _("  --fullscreen             Start in fullscreen mode") << endl
-    << _("  --no-menu                Start without displaying the menu bar ") << endl
+    << _("  --hide-menubar           Start without displaying the menu bar ") << endl
     << endl
     << _("Keys:") << endl
     << endl
@@ -196,9 +196,8 @@ parseCommandLine(int argc, char* argv[], gnash::Player& player)
         { 's', "scale",         Arg_parser::yes },
         { 256, "max-advances",  Arg_parser::yes },
         { 257, "fullscreen",    Arg_parser::no  },
-        { 258, "no-menu",       Arg_parser::no  },                
+        { 258, "hide-menubar",  Arg_parser::no  },                
         { 'c', 0,               Arg_parser::no  },
-        { 'm', 0,               Arg_parser::yes }, // What is this for?
         { 'd', "delay",         Arg_parser::yes },
         { 'x', "xid",           Arg_parser::yes },
         { 'r', "render-mode",   Arg_parser::yes },
