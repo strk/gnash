1f8779315173f1d923b8c51937300c3504718859
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-03-05 17:57:54 +0100

    Rename and re-scope some types.

--- libcore/debugger.cpp
+++ libcore/debugger.cpp
@@ -325,7 +325,7 @@ void
 Debugger::disassemble(const unsigned char *data)
 {
 //    GNASH_REPORT_FUNCTION;
-    as_arg_t fmt = ARG_HEX;
+    ArgumentType = ARG_HEX;
     ActionType action_id = static_cast<ActionType>(data[0]);
     int val = 0;
     string str;
