da901e0dbb09bf77484a9ce0ebe4aa7bcc203bf9
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-04-16 18:56:54 +0200

    Don't use prophelper for DropShadowFilter.

--- libcore/asobj/flash/filters/DropShadowFilter_as.cpp
+++ libcore/asobj/flash/filters/DropShadowFilter_as.cpp
@@ -20,11 +20,6 @@
 #include "DropShadowFilter.h"
 #include "VM.h"
 #include "builtin_function.h"
-
-// These _must_ be defined.
-#define phelp_helper DropShadowFilter_as
-#define phelp_class DropShadowFilter
-#include "prophelper.h"
 #include "BitmapFilter_as.h"
 
 namespace gnash {
@@ -32,67 +27,278 @@ namespace gnash {
 class DropShadowFilter_as : public as_object, public DropShadowFilter
 {
 public:
-    phelp_gs(distance);
-    phelp_gs(angle);
-    phelp_gs(color);
-    phelp_gs(alpha);
-    phelp_gs(blurX);
-    phelp_gs(blurY);
-    phelp_gs(strength);
-    phelp_gs(quality);
-    phelp_gs(inner); 
-    phelp_gs(knockout);
-    phelp_gs(hideObject);
+    static as_value distance_gs(const fn_call& fn);
+    static as_value angle_gs(const fn_call& fn);
+    static as_value color_gs(const fn_call& fn);
+    static as_value alpha_gs(const fn_call& fn);
+    static as_value blurX_gs(const fn_call& fn);
+    static as_value blurY_gs(const fn_call& fn);
+    static as_value strength_gs(const fn_call& fn);
+    static as_value quality_gs(const fn_call& fn);
+    static as_value inner_gs(const fn_call& fn);
+    static as_value knockout_gs(const fn_call& fn);
+    static as_value hideObject_gs(const fn_call& fn);
+    static as_value bitmap_clone(const fn_call& fn);
 
-    phelp_i(bitmap_clone);
 
+    DropShadowFilter_as(as_object *obj)
+        :
+        as_object(obj)
+    {}
+    
+    static as_object* Interface();
+    static void attachInterface(as_object& o);
+    static void attachProperties(as_object& o);
+    static void registerCtor(as_object& global);
+    static as_value ctor(const fn_call& fn);
+    
 private:
-    phelp_base_def;
+    static boost::intrusive_ptr<as_object> s_interface;
+    static boost::intrusive_ptr<builtin_function> s_ctor;
+
 };
 
-phelp_base_imp((bitmapFilter_interface()), DropShadowFilter)
 
-// Filters are property based.
-phelp_i_attach_begin
-phelp_i_replace(clone, bitmap_clone);
-phelp_i_attach_end
+boost::intrusive_ptr<as_object> DropShadowFilter_as::s_interface;
 
-phelp_gs_attach_begin
-phelp_gs_attach(distance);
-phelp_gs_attach(angle);
-phelp_gs_attach(color);
-phelp_gs_attach(alpha);
-phelp_gs_attach(blurX);
-phelp_gs_attach(blurY);
-phelp_gs_attach(strength);
-phelp_gs_attach(quality);
-phelp_gs_attach(inner);
-phelp_gs_attach(knockout);
-phelp_gs_attach(hideObject);
-phelp_gs_attach_end
+boost:: intrusive_ptr<builtin_function> DropShadowFilter_as::s_ctor;
 
-phelp_property(float, number<float>, distance)
-phelp_property(float, number<float>, angle)
-phelp_property(boost::uint32_t, number<boost::uint32_t>, color)
-phelp_property(boost::uint8_t, number<boost::uint8_t>, alpha)
-phelp_property(float, number<float>, blurX)
-phelp_property(float, number<float>, blurY)
-phelp_property(float, number<float>, strength)
-phelp_property(boost::uint8_t, number<boost::uint8_t>, quality)
-phelp_property(bool, bool, inner)
-phelp_property(bool, bool, knockout)
-phelp_property(bool, bool, hideObject)
-
-easy_clone(DropShadowFilter_as)
-
-as_value
-DropShadowFilter_as::ctor(const fn_call& /*fn*/)
-{
-    boost::intrusive_ptr<as_object> obj = new DropShadowFilter_as(DropShadowFilter_as::Interface());
-    DropShadowFilter_as::attachProperties(*obj);
-
-    return as_value(obj.get());
+as_object*
+DropShadowFilter_as::Interface() {
+    if (DropShadowFilter_as::s_interface == NULL) {
+        DropShadowFilter_as::s_interface = new as_object (bitmapFilter_interface());
+    VM::get().addStatic(DropShadowFilter_as::s_interface.get());
+    DropShadowFilter_as::attachInterface(*DropShadowFilter_as::s_interface);
+    }
+    return DropShadowFilter_as::s_interface.get();
 }
 
-} // Namespace gnash
+void
+DropShadowFilter_as::registerCtor(as_object& global) {
+    if (DropShadowFilter_as::s_ctor != NULL) return;
+    DropShadowFilter_as::s_ctor = new builtin_function(&DropShadowFilter_as::ctor, DropShadowFilter_as::Interface());
+    VM::get().addStatic(DropShadowFilter_as::s_ctor.get());
+    DropShadowFilter_as::attachInterface(*DropShadowFilter_as::s_ctor);
+    global.init_member("DropShadowFilter" , DropShadowFilter_as::s_ctor.get());
+}
 
+void
+DropShadowFilter_class_init(as_object& global)
+{
+    DropShadowFilter_as::registerCtor(global);
+}
+
+
+void DropShadowFilter_as::attachInterface(as_object& o) {
+    boost::intrusive_ptr<builtin_function> gs;
+
+    o.set_member(VM::get().getStringTable().find("clone"), new builtin_function(bitmap_clone));
+
+}
+
+void
+DropShadowFilter_as::attachProperties(as_object& o)
+{
+    boost::intrusive_ptr<builtin_function> gs;
+
+    gs = new builtin_function(DropShadowFilter_as::distance_gs, NULL);
+    o.init_property("distance" , *gs, *gs);
+
+    gs = new builtin_function(DropShadowFilter_as::angle_gs, NULL);
+    o.init_property("angle" , *gs, *gs);
+
+    gs = new builtin_function(DropShadowFilter_as::color_gs, NULL);
+    o.init_property("color" , *gs, *gs);
+
+    gs = new builtin_function(DropShadowFilter_as::alpha_gs, NULL);
+    o.init_property("alpha" , *gs, *gs);
+
+    gs = new builtin_function(DropShadowFilter_as::blurX_gs, NULL);
+    o.init_property("blurX" , *gs, *gs);
+
+    gs = new builtin_function(DropShadowFilter_as::blurY_gs, NULL);
+    o.init_property("blurY" , *gs, *gs);
+
+    gs = new builtin_function(DropShadowFilter_as::strength_gs, NULL);
+    o.init_property("strength" , *gs, *gs);
+
+    gs = new builtin_function(DropShadowFilter_as::quality_gs, NULL);
+    o.init_property("quality" , *gs, *gs);
+
+    gs = new builtin_function(DropShadowFilter_as::inner_gs, NULL);
+    o.init_property("inner" , *gs, *gs);
+
+    gs = new builtin_function(DropShadowFilter_as::knockout_gs, NULL);
+    o.init_property("knockout" , *gs, *gs);
+
+    gs = new builtin_function(DropShadowFilter_as::hideObject_gs, NULL);
+    o.init_property("hideObject" , *gs, *gs);
+
+}
+
+as_value
+DropShadowFilter_as::distance_gs(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    if (fn.nargs == 0) {
+		return as_value(ptr->m_distance );
+    }
+    float sp_distance = fn.arg(0).to_number<float> ();
+    ptr->m_distance = sp_distance;
+    return as_value();
+}
+
+
+as_value
+DropShadowFilter_as::angle_gs(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    if (fn.nargs == 0) {
+		return as_value(ptr->m_angle );
+    }
+    float sp_angle = fn.arg(0).to_number<float> ();
+    ptr->m_angle = sp_angle;
+    return as_value();
+}
+
+
+as_value
+DropShadowFilter_as::color_gs(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    if (fn.nargs == 0) {
+		return as_value(ptr->m_color );
+    }
+    boost::uint32_t sp_color = fn.arg(0).to_number<boost::uint32_t> ();
+    ptr->m_color = sp_color;
+    return as_value();
+}
+
+
+as_value
+DropShadowFilter_as::alpha_gs(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    if (fn.nargs == 0) {
+		return as_value(ptr->m_alpha );
+    }
+    boost::uint8_t sp_alpha = fn.arg(0).to_number<boost::uint8_t> ();
+    ptr->m_alpha = sp_alpha;
+    return as_value();
+}
+
+
+as_value
+DropShadowFilter_as::blurX_gs(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    if (fn.nargs == 0) {
+		return as_value(ptr->m_blurX );
+    }
+    float sp_blurX = fn.arg(0).to_number<float> ();
+    ptr->m_blurX = sp_blurX;
+    return as_value();
+}
+
+
+as_value
+DropShadowFilter_as::blurY_gs(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    if (fn.nargs == 0) {
+		return as_value(ptr->m_blurY );
+    }
+    float sp_blurY = fn.arg(0).to_number<float> ();
+    ptr->m_blurY = sp_blurY;
+    return as_value();
+}
+
+
+as_value
+DropShadowFilter_as::strength_gs(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    if (fn.nargs == 0) {
+		return as_value(ptr->m_strength );
+    }
+    float sp_strength = fn.arg(0).to_number<float> ();
+    ptr->m_strength = sp_strength;
+    return as_value();
+}
+
+
+as_value
+DropShadowFilter_as::quality_gs(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    if (fn.nargs == 0) {
+		return as_value(ptr->m_quality );
+    }
+    boost::uint8_t sp_quality = fn.arg(0).to_number<boost::uint8_t> ();
+    ptr->m_quality = sp_quality;
+    return as_value();
+}
+
+
+as_value
+DropShadowFilter_as::inner_gs(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    if (fn.nargs == 0) {
+		return as_value(ptr->m_inner );
+    }
+    bool sp_inner = fn.arg(0).to_bool ();
+    ptr->m_inner = sp_inner;
+    return as_value();
+}
+
+
+as_value
+DropShadowFilter_as::knockout_gs(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    if (fn.nargs == 0) {
+		return as_value(ptr->m_knockout );
+    }
+    bool sp_knockout = fn.arg(0).to_bool ();
+    ptr->m_knockout = sp_knockout;
+    return as_value();
+}
+
+
+as_value
+DropShadowFilter_as::hideObject_gs(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    if (fn.nargs == 0) {
+		return as_value(ptr->m_hideObject );
+    }
+    bool sp_hideObject = fn.arg(0).to_bool ();
+    ptr->m_hideObject = sp_hideObject;
+    return as_value();
+}
+
+as_value
+DropShadowFilter_as::bitmap_clone(const fn_call& fn)
+{
+    boost::intrusive_ptr<DropShadowFilter_as> ptr = ensureType<DropShadowFilter_as>(fn.this_ptr);
+    boost::intrusive_ptr<DropShadowFilter_as> obj = new DropShadowFilter_as(*ptr);
+    boost::intrusive_ptr<as_object> r = obj;
+    r->set_prototype(ptr->get_prototype());
+    r->copyProperties(*ptr);
+    return as_value(r);
+ }
+
+as_value
+DropShadowFilter_as::ctor(const fn_call& )
+{
+    boost::intrusive_ptr<as_object> obj = new DropShadowFilter_as(DropShadowFilter_as::Interface());
+
+    DropShadowFilter_as::attachProperties(*obj);
+
+
+    return as_value(obj.get());
+
+}
+
+}
