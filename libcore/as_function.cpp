4362cfce9372e6065d991fcd723de83297795d07
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-04-23 14:49:08 +0200

    Clean up as_function.

--- libcore/as_function.cpp
+++ libcore/as_function.cpp
@@ -33,75 +33,50 @@
 
 namespace gnash {
 
-// should be static, probably
-as_value function_apply(const fn_call& fn);
-as_value function_call(const fn_call& fn);
-static as_object* getFunctionPrototype();
-static as_value function_ctor(const fn_call& fn);
-
-/* 
- * This function returns the singleton
- * instance of the ActionScript Function object
- * prototype, which is what the AS Function class
- * exports, thus what each AS function instance inherit.
- *
- * The returned object can be accessed by ActionScript
- * code through Function.__proto__.prototype.
- * User AS code can add or modify members of this object
- * to modify behaviour of all Function AS instances.
- *
- * FIXME: do not use a static specifier for the proto
- * object, as multiple runs of a single movie should
- * each use a 'clean', unmodified, version of the
- * prototype. What should really happen is that this
- * prototype gets initializated by initialization of
- * the Function class itself, which would be a member
- * of the _global object for each movie instance.
- * 
- */
-static as_object* getFunctionPrototype()
-{
-	// Make sure the prototype is always
-	// alive (static boost::intrusive_ptr<> should ensure this)
-	static boost::intrusive_ptr<as_object> proto;
-
-	if ( proto.get() == NULL ) {
-
-		// Initialize Function prototype
-		proto = new as_object();
-
-		// We initialize the __proto__ member separately, as getObjectInterface
-		// will end up calling getFunctionPrototype again and we want that
-		// call to return the still-not-completely-constructed prototype rather
-		// then create a new one. 
-		proto->set_prototype(getObjectInterface());
-
-		VM::get().addStatic(proto.get());
-
-		const int flags = as_prop_flags::dontDelete | 
-                          as_prop_flags::dontEnum | 
-                          as_prop_flags::onlySWF6Up; 
-
-		proto->init_member("apply", new builtin_function(function_apply), flags);
-		proto->init_member("call", new builtin_function(function_call), flags);
-	}
-
-	return proto.get();
-
+// Forward declarations
+namespace {
+    as_value function_apply(const fn_call& fn);
+    as_value function_call(const fn_call& fn);
+    as_object* getFunctionPrototype();
+    as_value function_ctor(const fn_call& fn);
 }
 
-static as_value
-function_ctor(const fn_call& /* fn */)
+// This function returns the singleton
+// instance of the ActionScript Function object
+// prototype, which is what the AS Function class
+// exports, thus what each AS function instance inherit.
+// 
+// The returned object can be accessed by ActionScript
+// code through Function.__proto__.prototype.
+// User AS code can add or modify members of this object
+// to modify behaviour of all Function AS instances.
+// 
+// FIXME: do not use a static specifier for the proto
+// object, as multiple runs of a single movie should
+// each use a 'clean', unmodified, version of the
+// prototype. What should really happen is that this
+// prototype gets initializated by initialization of
+// the Function class itself, which would be a member
+// of the _global object for each movie instance.
+
+as_function::as_function(as_object* iface)
+	:
+	as_object()
 {
-	boost::intrusive_ptr<as_object> func = new as_object(getFunctionPrototype());
-	//log_debug(_("User tried to invoke new Function()"));
-	return as_value(func.get());
+	int flags = as_prop_flags::dontDelete |
+	            as_prop_flags::dontEnum |
+	            as_prop_flags::onlySWF6Up;
+
+    init_member(NSV::PROP_uuPROTOuu, as_value(getFunctionPrototype()), flags);
+
+	if (iface) {
+		iface->init_member(NSV::PROP_CONSTRUCTOR, this); 
+		init_member(NSV::PROP_PROTOTYPE, as_value(iface));
+	}
 }
 
 as_function::as_function()
 	:
-	// all functions inherit from global Function class
-	//as_object(getFunctionPrototype())
 	as_object()
 {
 	int flags = as_prop_flags::dontDelete |
@@ -111,29 +86,9 @@ as_function::as_function()
 }
 
 
-// What if we want a function to inherit from Object instead ?
-as_function::as_function(as_object* iface)
-	:
-	// all functions inherit from global Function class
-	//as_object(getFunctionPrototype())
-	as_object()
-{
-	int flags = as_prop_flags::dontDelete |
-	            as_prop_flags::dontEnum |
-	            as_prop_flags::onlySWF6Up;
-	init_member(NSV::PROP_uuPROTOuu, as_value(getFunctionPrototype()), flags);
-
-	if ( iface )
-	{
-		iface->init_member(NSV::PROP_CONSTRUCTOR, this); 
-		init_member(NSV::PROP_PROTOTYPE, as_value(iface));
-	}
-}
-
 void
 as_function::setPrototype(as_object* proto)
 {
-	//_properties = proto;
 	init_member(NSV::PROP_PROTOTYPE, as_value(proto));
 }
 
@@ -163,26 +118,134 @@ as_function::getPrototype()
 	return proto.to_object();
 }
 
-/* static public */
 boost::intrusive_ptr<builtin_function>
 as_function::getFunctionConstructor()
 {
 	static boost::intrusive_ptr<builtin_function> func = NULL;
 	if ( ! func )
 	{
-		func = new builtin_function(
-			function_ctor, // function constructor doesn't do anything
-			getFunctionPrototype(), // exported interface
-			true // use "this" as constructor
-			);
+		func = new builtin_function(function_ctor, getFunctionPrototype(),
+                true);
 		VM::get().addStatic(func.get());
 	}
 	return func;
 }
 
-/*
- * Initialize the "Function" member of a _global object.
- */
+boost::intrusive_ptr<as_object>
+as_function::constructInstance(const as_environment& env,
+	std::auto_ptr<std::vector<as_value> > args)
+{
+
+#ifndef GNASH_USE_GC
+	assert(get_ref_count() > 0);
+#endif // GNASH_USE_GC
+
+	int swfversion = env.getVM().getSWFVersion();
+
+	boost::intrusive_ptr<as_object> newobj;
+
+	as_value us;
+	
+    get_member(NSV::PROP_PROTOTYPE, &us);
+	
+    bool has_proto = !us.is_undefined();
+
+    // a built-in class takes care of assigning a prototype
+    // TODO: change this
+    if (isBuiltin()) {
+		IF_VERBOSE_ACTION (
+            log_action(_("it's a built-in class"));
+		)
+
+		fn_call fn(0, env, args);
+		as_value ret;
+
+		try {
+			ret = call(fn);
+		}
+        catch (GnashException& ex) {
+            // Catching a std::exception here can mask all sorts of bad 
+            // behaviour, as (for instance) a poorly constructed string may
+            // smash the stack, throw an exception, but not abort.
+            // This is very effective at confusing debugging tools.
+            // We only throw GnashExceptions. A std::bad_alloc may also be
+            // reasonable, but anything else shouldn't be caught here.
+			log_debug("Native function called as constructor threw exception: "
+                    "%s", ex.what());
+		}
+
+		if (ret.is_object()) newobj = ret.to_object();
+		else {
+			log_debug("Native function called as constructor returned %s", ret);
+			newobj = new as_object();
+		}
+
+		assert(newobj); // we assume builtin functions do return objects !!
+
+		// Add a __constructor__ member to the new object, but only for SWF6 up
+		// (to be checked). NOTE that we assume the builtin constructors
+		// won't set __constructor__ to some other value...
+		int flags = as_prop_flags::dontEnum | 
+                    as_prop_flags::onlySWF6Up; 
+
+		newobj->init_member(NSV::PROP_uuCONSTRUCTORuu, as_value(this), flags);
+
+        // Also for SWF5+ only?
+		if (swfversion < 7) {
+			newobj->init_member(NSV::PROP_CONSTRUCTOR, as_value(this), flags);
+		}
+
+    }
+	else {
+		// Set up the prototype.
+		as_value proto;
+
+		// We can safely call as_object::get_member here as member name is 
+		// a literal string in lowercase. (we should likely avoid calling
+		// get_member as a whole actually, and use a getProto() or similar
+		// method directly instead) TODO
+		/*bool func_has_prototype=*/ get_member(NSV::PROP_PROTOTYPE, &proto);
+
+		// User could have dropped the prototype.
+		// see construct-properties-#.swf from swfdec testsuite
+
+		IF_VERBOSE_ACTION(
+            log_action(_("constructor prototype is %s"), proto);
+		);
+
+		// Create an empty object, with a ref to the constructor's prototype.
+		newobj = new as_object(proto.to_object());
+
+		// Add a __constructor__ member to the new object, but only for SWF6 up
+		// (to be checked)
+        // Can delete, hidden in swf5 
+		int flags = as_prop_flags::dontEnum | 
+                    as_prop_flags::onlySWF6Up; 
+
+		newobj->init_member(NSV::PROP_uuCONSTRUCTORuu, this, flags);
+
+		if (swfversion < 7) {
+			newobj->init_member(NSV::PROP_CONSTRUCTOR, this, flags);
+		}
+
+		// Super is computed from the object we're constructing,
+		// It will work as long as we did set it's __proto__ and __constructor__
+		// properties already.
+		as_object* super = newobj->get_super();
+
+		// Call the actual constructor function; new_obj is its 'this'.
+
+		// We don't need the function result.
+		fn_call fn(newobj.get(), env, args, super);
+		call(fn);
+	}
+
+	if (!has_proto) set_member(NSV::PROP_PROTOTYPE, as_value(newobj));
+    
+	return newobj;
+}
+
+
 void
 function_class_init(as_object& global)
 {
@@ -194,8 +257,49 @@ function_class_init(as_object& global)
                     as_prop_flags::dontDelete | 
                     as_prop_flags::onlySWF6Up;
 	global.init_member("Function", func.get(), swf6flags);
+}
+
+namespace {
+
+as_object*
+getFunctionPrototype()
+{
+	static boost::intrusive_ptr<as_object> proto;
+
+	if (proto.get() == NULL) {
+
+		// Initialize Function prototype
+		proto = new as_object();
+
+		// We initialize the __proto__ member separately, as getObjectInterface
+		// will end up calling getFunctionPrototype again and we want that
+		// call to return the still-not-completely-constructed prototype rather
+		// then create a new one. 
+		proto->set_prototype(getObjectInterface());
+
+		VM::get().addStatic(proto.get());
+
+		const int flags = as_prop_flags::dontDelete | 
+                          as_prop_flags::dontEnum | 
+                          as_prop_flags::onlySWF6Up; 
+
+		proto->init_member("apply", new builtin_function(function_apply),
+                flags);
+		proto->init_member("call", new builtin_function(function_call), flags);
+	}
+
+	return proto.get();
 
 }
+
+as_value
+function_ctor(const fn_call& /* fn */)
+{
+	boost::intrusive_ptr<as_object> func = 
+        new as_object(getFunctionPrototype());
+	return as_value(func.get());
+}
+
 
 as_value
 function_apply(const fn_call& fn)
@@ -340,119 +444,6 @@ function_call(const fn_call& fn)
 
 }
 
-boost::intrusive_ptr<as_object>
-as_function::constructInstance(const as_environment& env,
-	std::auto_ptr<std::vector<as_value> > args)
-{
-
-#ifndef GNASH_USE_GC
-	assert(get_ref_count() > 0);
-#endif // GNASH_USE_GC
-
-	int swfversion = env.getVM().getSWFVersion();
-
-	boost::intrusive_ptr<as_object> newobj;
-
-	as_value us;
-	
-    get_member(NSV::PROP_PROTOTYPE, &us);
-	
-    bool has_proto = !us.is_undefined();
-
-    // a built-in class takes care of assigning a prototype
-    // TODO: change this
-    if (isBuiltin()) {
-		IF_VERBOSE_ACTION (
-            log_action(_("it's a built-in class"));
-		)
-
-		fn_call fn(0, env, args);
-		as_value ret;
-
-		try {
-			ret = call(fn);
-		}
-        catch (GnashException& ex) {
-            // Catching a std::exception here can mask all sorts of bad 
-            // behaviour, as (for instance) a poorly constructed string may
-            // smash the stack, throw an exception, but not abort.
-            // This is very effective at confusing debugging tools.
-            // We only throw GnashExceptions. A std::bad_alloc may also be
-            // reasonable, but anything else shouldn't be caught here.
-			log_debug("Native function called as constructor threw exception: "
-                    "%s", ex.what());
-		}
-
-		if (ret.is_object()) newobj = ret.to_object();
-		else {
-			log_debug("Native function called as constructor returned %s", ret);
-			newobj = new as_object();
-		}
-
-		assert(newobj); // we assume builtin functions do return objects !!
-
-		// Add a __constructor__ member to the new object, but only for SWF6 up
-		// (to be checked). NOTE that we assume the builtin constructors
-		// won't set __constructor__ to some other value...
-		int flags = as_prop_flags::dontEnum | 
-                    as_prop_flags::onlySWF6Up; 
-
-		newobj->init_member(NSV::PROP_uuCONSTRUCTORuu, as_value(this), flags);
-
-        // Also for SWF5+ only?
-		if (swfversion < 7) {
-			newobj->init_member(NSV::PROP_CONSTRUCTOR, as_value(this), flags);
-		}
-
-    }
-	else {
-		// Set up the prototype.
-		as_value proto;
-
-		// We can safely call as_object::get_member here as member name is 
-		// a literal string in lowercase. (we should likely avoid calling
-		// get_member as a whole actually, and use a getProto() or similar
-		// method directly instead) TODO
-		/*bool func_has_prototype=*/ get_member(NSV::PROP_PROTOTYPE, &proto);
-
-		// User could have dropped the prototype.
-		// see construct-properties-#.swf from swfdec testsuite
-
-		IF_VERBOSE_ACTION(
-            log_action(_("constructor prototype is %s"), proto);
-		);
-
-		// Create an empty object, with a ref to the constructor's prototype.
-		newobj = new as_object(proto.to_object());
-
-		// Add a __constructor__ member to the new object, but only for SWF6 up
-		// (to be checked)
-        // Can delete, hidden in swf5 
-		int flags = as_prop_flags::dontEnum | 
-                    as_prop_flags::onlySWF6Up; 
-
-		newobj->init_member(NSV::PROP_uuCONSTRUCTORuu, this, flags);
-
-		if (swfversion < 7) {
-			newobj->init_member(NSV::PROP_CONSTRUCTOR, this, flags);
-		}
-
-		// Super is computed from the object we're constructing,
-		// It will work as long as we did set it's __proto__ and __constructor__
-		// properties already.
-		as_object* super = newobj->get_super();
-
-		// Call the actual constructor function; new_obj is its 'this'.
-
-		// We don't need the function result.
-		fn_call fn(newobj.get(), env, args, super);
-		call(fn);
-	}
-
-	if (!has_proto) set_member(NSV::PROP_PROTOTYPE, as_value(newobj));
-    
-	return newobj;
-}
-
-} // end of gnash namespace
+} // anonymous namespace
+} // gnash namespace
 
