a21dccc81c8d1b4d5b2d091f494c7b5dafe075d7
Ben Limmer <ben@ubuntu>
2009-05-27 15:35:46 -0600

    updated internal testsuites (limited implementation)

---
+++ testsuite/as3/internal.all/flash_as.hx
@@ -0,0 +1,27 @@
+//FIXME: we should rewrite this (maybe in c) to check for the presence of
+//all the appropriate testsuites
+
+//flash_as.hx:  ActionScript 3 "flash" class, for Gnash 
+//internal testing.
+//
+//
+//   Copyright (C) 2009 Free Software Foundation, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+// This test case must be processed by CPP before compiling to include the
+//  DejaGnu.hx header file for the testing framework support.
+
