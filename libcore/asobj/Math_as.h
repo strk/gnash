bee4a77687281f7de6a108ac4222c66f148e535a
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-06-15 16:08:58 +0200

    Correct header guard.

--- libcore/asobj/Math_as.h
+++ libcore/asobj/Math_as.h
@@ -16,8 +16,8 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
-#ifndef __GMATH_H__
-#define __GMATH_H__
+#ifndef GNASH_ASOBJ_MATH_H
+#define GNASH_ASOBJ_MATH_H
 
 #include "impl.h"
 #include "as_object.h"
