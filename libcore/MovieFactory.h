0ae20d5de5db3c86a3133de0b0b90e61355500e9
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-06-15 17:47:34 +0200

    Add documentation from old gnash.h to MovieFactory.

--- libcore/MovieFactory.h
+++ libcore/MovieFactory.h
@@ -36,11 +36,80 @@ namespace gnash {
 class MovieFactory
 {
 public:
-    
+   
+    /// Create a gnash::movie_definition from the given URL
+    //
+    /// The URL can correspond to either a JPEG or SWF file.
+    ///
+    /// This is just like create_movie(), except that it checks the
+    /// "library" to see if a movie of this name has already been
+    /// created, and returns that movie if so.  Also, if it creates
+    /// a new movie, it adds it back into the library.
+    ///
+    /// The "library" is used when importing symbols from external
+    /// movies, so this call might be useful if you want to
+    /// explicitly load a movie that you know exports symbols
+    /// (e.g. fonts) to other movies as well.
+    ///
+    /// @@ this explanation/functionality could be clearer!
+    ///
+    /// This calls add_ref() on the newly created definition; call
+    /// drop_ref() when you're done with it.
+    /// Or use boost::intrusive_ptr<T> from base/smart_ptr.h if you want.
+    ///
+    /// If real_url is given, the movie's url will be set to that value.
+    ///
+    /// @param url
+    /// The URL to load the movie from.
+    ///
+    /// @param runInfo
+    /// A RunInfo containing resources needed for parsing, such as the
+    /// base URL for the run, the sound::sound_handler, and a StreamProvider.
+    ///
+    /// @param real_url
+    /// The url to encode as the _url member of the resulting
+    /// movie definition. Use NULL if it is not different from
+    /// the actual url (default). This is used to simulate a run from
+    /// the official publication url.
+    ///
+    /// @param startLoaderThread
+    /// If false only the header will be read, and you'll need to call
+    /// completeLoad on the returned movie_definition to actually start it.
+    /// This is typically used to postpone parsing until a VirtualMachine
+    /// is initialized. Initializing the VirtualMachine requires a target
+    /// SWF version, which can be found in the SWF header.
+    ///
+    /// @param postdata
+    /// If not NULL, use POST method (only valid for HTTP).
+    /// NOTE: when POSTing, the movies library won't be used.
     static DSOEXPORT movie_definition* makeMovie(const URL& url,
         const RunInfo& runInfo, const char* real_url = NULL,
         bool startLoaderThread = true, const std::string* postdata = NULL);
     
+    /// Load a movie from an already opened stream.
+    //
+    /// The movie can be both an SWF or JPEG, the url parameter
+    /// will be used to set the _url member of the resulting object.
+    ///
+    /// @param in
+    /// The stream to load the movie from. Ownership is transferred
+    /// to the returned object.
+    ///
+    /// @param url
+    /// The url to use as the _url member of the resulting
+    /// movie definition. This is required as it can not be
+    /// derived from the IOChannel.
+    ///
+    /// @param runInfo
+    /// A RunInfo containing resources needed for parsing, such as the
+    /// base URL for the run, the sound::sound_handler, and a StreamProvider.
+    ///
+    /// @param startLoaderThread
+    /// If false only the header will be read, and you'll need to call
+    /// completeLoad on the returned movie_definition to actually start it.
+    /// This is typically used to postpone parsing until a VirtualMachine
+    /// is initialized. Initializing the VirtualMachine requires a target
+    /// SWF version, which can be found in the SWF header.
     static DSOEXPORT movie_definition* makeMovie(std::auto_ptr<IOChannel> in,
             const std::string& url, const RunInfo& runInfo,
             bool startLoaderThread);
