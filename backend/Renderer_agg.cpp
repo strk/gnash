666a6bb77c071244c3abbed2e30fe292292d7f7e
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-07-11 14:06:51 +0200

    Get the agg renderer to build.

--- backend/Renderer_agg.cpp
+++ backend/Renderer_agg.cpp
@@ -1346,7 +1346,7 @@ public:
           }
 
           sh.add_bitmap(dynamic_cast<const agg_bitmap_info*> 
-            (fill_styles[fno].get_bitmap_info()), m, cx, 
+            (fill_styles[fno].get_bitmap_info(*this)), m, cx, 
             (fill_type==SWF::FILL_TILED_BITMAP) ||
             (fill_type==SWF::FILL_TILED_BITMAP_HARD),
             smooth);
