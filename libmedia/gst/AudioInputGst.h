5c9d2dfa88b9b12683fdc507defc571407032441
Ben Limmer <ben@l1m5-ubuntu>
2009-07-13 15:12:51 -0600

    added stubs for camera and microphone implementations

---
+++ libmedia/gst/AudioInputGst.h
@@ -0,0 +1,44 @@
+// AudioInput.h: Audio input processing using Gstreamer.
+// 
+//   Copyright (C) 2007, 2008, 2009 Free Software Foundation, Inc.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+#ifndef GNASH_AUDIOINPUTGST_H
+#define GNASH_AUDIOINPUTGST_H
+
+#include <boost/cstdint.hpp> // for C99 int types
+#include "AudioInput.h"
+
+namespace gnash {
+namespace media {
+namespace gst {
+
+
+class AudioInputGst : public AudioInput {
+	
+public:
+
+	AudioInputGst();
+
+	~AudioInputGst();
+
+};
+
+} // gst namespace
+} // gnash.media namespace 
+} // gnash namespace
+
+#endif // __AUDIOINPUT_H__
