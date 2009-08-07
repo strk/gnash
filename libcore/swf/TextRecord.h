53de9076a0b703c58117fb019c296d9fe873af7b
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-08-07 08:06:56 +0200

    Drop unused glyphsnonconst method. If such access is ever needed (hopefully not), it can simply overload glyphs() and the compiler will choose the appropriate version.

--- libcore/swf/TextRecord.h
+++ libcore/swf/TextRecord.h
@@ -96,10 +96,6 @@ public:
         return _glyphs;
     }
 
-    Glyphs& glyphsnonconst() {
-        return _glyphs;
-    }
-
     void addGlyph(const GlyphEntry& ge, Glyphs::size_type num = 1) {
         _glyphs.insert(_glyphs.end(), num, ge);
     }
