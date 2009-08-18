0adcf19d6ec9f4b7fa1e72b1cf0f0046a31a981e
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-08-18 13:02:07 +0200

    Use registerBuiltinObject more.

--- libcore/asobj/flash/display/GradientType_as.cpp
+++ libcore/asobj/flash/display/GradientType_as.cpp
@@ -43,12 +43,7 @@ namespace {
 void
 gradienttype_class_init(as_object& where, const ObjectURI& uri)
 {
-    Global_as* gl = getGlobal(where);
-    as_object* proto = getObjectInterface();
-    as_object* o = gl->createObject(proto);
-    attachGradientTypeStaticInterface(*o);
-    where.init_member(getName(uri), o, as_object::DefaultFlags,
-            getNamespace(uri));
+    registerBuiltinObject(where, attachGradientTypeStaticInterface, uri);
 }
 
 namespace {
