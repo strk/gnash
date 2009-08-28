f195c58f5e3ea5d019eb8b8e9fa39c9801d06e9d
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-08-28 13:21:24 +0200

    Correct Microphone.names.

--- libcore/asobj/flash/media/Microphone_as.cpp
+++ libcore/asobj/flash/media/Microphone_as.cpp
@@ -421,16 +421,8 @@ microphone_name(const fn_call& fn)
 as_value
 microphone_names(const fn_call& fn)
 {
-    // TODO: this is a static function, not a member function. Because there
-    // is no this pointer, it cannot use microphone_as_object to get the
-    // names. It will have to query the MediaHandler directly (much of the
-    // rest of the code should do this too).
-    boost::intrusive_ptr<microphone_as_object> ptr =
-        ensureType<microphone_as_object>(fn.this_ptr);
-    
-    //transfer from internal vector to AS array
+    // TODO: populate from MediaHandler like Camera.names.
     std::vector<std::string> vect;
-    vect = ptr->get_names();
     
     size_t size = vect.size();
     
