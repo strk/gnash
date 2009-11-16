837213b5788a9d0212a04d6b2bb34645d28b3427
Benjamin Wolsey <bwy@benjaminwolsey.de>
2009-11-16 13:18:30 +0100

    MovieLoader needs 4 arguments!

--- libcore/asobj/Global_as.h
+++ libcore/asobj/Global_as.h
@@ -257,7 +257,7 @@ callMethod(as_object* obj, string_table::key key BOOST_PP_COMMA_IF(n)\
 }
 
 /// The maximum number of as_value arguments allowed in callMethod functions.
-#define MAX_ARGS 3
+#define MAX_ARGS 4
 BOOST_PP_REPEAT(BOOST_PP_INC(MAX_ARGS), CALL_METHOD, )
 
 /// Convenience function for finding a class constructor.
