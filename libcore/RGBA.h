9ec79dce357f32e19b3b657348c22fa03f65f136
Benjamin Wolsey <bwy@benjaminwolsey.de>
2010-01-18 07:38:37 +0100

    Copyright.

--- libcore/RGBA.h
+++ libcore/RGBA.h
@@ -1,12 +1,25 @@
-// types.h    -- Thatcher Ulrich <tu@tulrich.com> 2003
+// RGBA.h: RGBA color handling.
+// 
+//   Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Free Software
+//   Foundation, Inc
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+// 
 
-// This source code has been donated to the Public Domain.  Do
-// whatever you want with it.
-
-// Some basic types.
-
-#ifndef GNASH_TYPES_H
-#define GNASH_TYPES_H
+#ifndef GNASH_RGBA_H
+#define GNASH_RGBA_H
 
 #include "SWF.h"
 
