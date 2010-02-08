76f545a8eba56552d73c9332650fc2d515393f4c
Benjamin Wolsey <bwy@benjaminwolsey.de>
2010-02-08 16:38:52 +0100

    Use legal header guard.

--- libbase/shm.h
+++ libbase/shm.h
@@ -16,8 +16,8 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-#ifndef __SHM_H__
-#define __SHM_H__
+#ifndef GNASH_SHM_H
+#define GNASH_SHM_H
 
 #include <string>
 
