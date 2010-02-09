9fb69d0c0f03f858ec0c1a804e89f7f6194bf705
Benjamin Wolsey <bwy@benjaminwolsey.de>
2010-02-09 16:32:53 +0100

    Add a way of running two instances simultaneously for testing LC with two Gnash instances.
    There are still problems communicating between Gnash and the pp (depends on
    the listener ID).

---
+++ testsuite/simultaneous-testrunner.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# 
+#   Copyright (C) 2005, 2006, 2009, 2010 Free Software Foundation, Inc.
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+
+
+top_builddir=$1
+shift
+t1=$1
+shift
+t2=$1
+
+
+echo "#!/bin/sh"
+echo
+
+echo "# Environment"
+env | grep GNASH | while read REPLY; do
+	echo "export ${REPLY}"
+done
+
+cat << EOF
+
+outlog1=${top_builddir}/testoutlog1.\$$
+outlog2=${top_builddir}/testoutlog2.\$$
+(
+    echo "Running first process"
+    exec ${top_builddir}/gui/gnash -v -r0 ${t1}
+    cat \${outlog1}
+    rm \${outlog1}
+) &
+(
+    echo "Running second process"
+    exec ${top_builddir}/gui/gnash -v -r0 ${t2}
+    cat \${outlog2}
+    rm \${outlog2}
+)
+EOF
