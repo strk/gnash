98c5d1051c54812851384a4b7f5bf4f8f6faec63
Benjamin Wolsey <bwy@benjaminwolsey.de>
2010-02-10 08:19:19 +0100

    Copyright.

--- libcore/AMF.cpp
+++ libcore/AMF.cpp
@@ -1,3 +1,20 @@
+// 
+//   Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Free Software
+//   Foundation, Inc
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 #include <map>
@@ -32,13 +49,11 @@ public:
     
     bool success() const { return !_error; }
 
-    bool accept(const ObjectURI& uri, const as_value& val) 
-    {
-        if ( _error ) return true;
+    bool accept(const ObjectURI& uri, const as_value& val) {
+        if (_error) return true;
 
         // Tested with SharedObject and AMFPHP
-        if (val.is_function())
-        {
+        if (val.is_function()) {
             log_debug("AMF0: skip serialization of FUNCTION property");
             return true;
         }
@@ -62,6 +77,7 @@ public:
 
         // write property name
         const std::string& name = _st.value(key);
+
 #ifdef GNASH_DEBUG_AMF_SERIALIZE
         log_debug(" serializing property %s", name);
 #endif
@@ -72,6 +88,7 @@ public:
         }
         return true;
     }
+
 private:
 
     Writer& _writer;
