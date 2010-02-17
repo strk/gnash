e7088b289ebc0b3e96ef99608f66075494f7ba09
Rob Savoye <rob@welcomehome.org>
2010-02-17 10:45:32 -0700

    don't include any headers, it's just a stub.

--- libbase/SharedMemHaiku.cpp
+++ libbase/SharedMemHaiku.cpp
@@ -16,37 +16,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-#ifdef HAVE_CONFIG_H
-#include "gnashconfig.h"
-#endif
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <dirent.h>
-#include <fcntl.h>
-#if !defined(HAVE_WINSOCK_H) && !defined(__riscos__) && !defined(__OS2__) && !defined(HAIKU_HOST)
-# include <sys/mman.h>
-# include <sys/shm.h>
-# include <sys/ipc.h>
-# include <sys/sem.h>
-#elif !defined(__riscos__) && !defined(__OS2__)
-# include <windows.h>
-# include <process.h>
-# include <io.h>
-#endif
-#include <string>
-#include <vector>
-#include <cerrno>
-
-#include "log.h"
 #include "SharedMem.h"
-
-#if (defined(USE_SYSV_SHM) && defined(HAVE_SHMGET)) || defined(_WIN32)
-# define ENABLE_SHARED_MEM 1
-#else
-# undef ENABLE_SHARED_MEM
-#endif
 
 namespace {
     gnash::RcInitFile& rcfile = gnash::RcInitFile::getDefaultInstance();
