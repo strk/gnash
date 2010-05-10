ff1ec76d666dbc7bd0fe55b19364c4982e19decd
Benjamin Wolsey <bwy@benjaminwolsey.de>
2010-05-10 09:58:53 +0200

    Swap two tests as the names referred to what the other one was testing.

--- testsuite/misc-ming.all/InitActionTest2.c
+++ testsuite/misc-ming.all/InitActionTest2.c
@@ -12,10 +12,10 @@ int
 main(int argc, char** argv)
 {
     SWFMovie mo;
-    SWFMovieClip mc3, mc2, dejagnuclip;
-    SWFAction ac, ac1;
+    SWFMovieClip mc1, mc2, mc3, mc4, dejagnuclip;
+    SWFAction ac;
     SWFDisplayItem it;
-    SWFShape sha;
+    SWFInitAction ia;
 
     const char *srcdir=".";
     if (argc > 1) srcdir = argv[1];
@@ -27,88 +27,75 @@ main(int argc, char** argv)
     Ming_init();
     mo = newSWFMovieWithVersion(OUTPUT_VERSION);
     SWFMovie_setDimension(mo, 800, 600);
+ 
     SWFMovie_setRate (mo, 12.0);
 
-    sha = newSWFShape();
-
-    // Character ID 2. Has 1 showframe. Is exported first.
+    mc1 = newSWFMovieClip();
+    SWFMovieClip_nextFrame(mc1);
+    
+    add_actions(mo, "onEnterFrame = function() { "
+            "trace(_level0.mc._currentframe + ':' + "
+            "           _level0.mc.Segments.a);"
+            "};"
+            );
+    
     mc2 = newSWFMovieClip();
-    SWFMovieClip_add(mc2, (SWFBlock)sha);
     SWFMovieClip_nextFrame(mc2);
-
-    // Export it.
-    SWFMovie_addExport(mo, (SWFBlock)mc2, "C2");
+    SWFMovie_addExport(mo, (SWFBlock)mc2, "MC1");
     SWFMovie_writeExports(mo);
 
-    // Main timeline actions for frame 1
-    add_actions(mo, "var c = 0; trace('frame 1'); gotoAndStop(3);");
-    
-    // ID 3 is defined here. It has no showframe. It is exported immediately.
     mc3 = newSWFMovieClip();
-    SWFMovie_addExport(mo, (SWFBlock)mc3, "ctor");
+    it = SWFMovieClip_add(mc3, (SWFBlock)mc2);
+    SWFDisplayItem_setName(it, "Segments");
+    SWFMovieClip_nextFrame(mc3);
+    SWFMovieClip_remove(mc3, it);
+    it = SWFMovieClip_add(mc3, (SWFBlock)mc1);
+    SWFMovieClip_nextFrame(mc3);
+
+    it = SWFMovie_add(mo, (SWFBlock)mc3);
+    SWFDisplayItem_setName(it, "mc");
+
+    mc4 = newSWFMovieClip();
+    
+    SWFMovie_addExport(mo, (SWFBlock)mc4, "MC4");
     SWFMovie_writeExports(mo);
 
-    dejagnuclip = get_dejagnu_clip((SWFBlock)get_default_font(srcdir), 10,
-    		    0, 0, 800, 600);
-    SWFMovie_add(mo, (SWFBlock)dejagnuclip);
-
-    // Init actions for ID 3
-    ac = newSWFAction(
-    "   _global.ctor = function () {"
-    "       super();"
-    "       trace('Object in Frame 2 is constructed');"
-    "       c += 1;"
-    "   };"
-    );
-    SWFInitAction ia = newSWFInitAction_withId(ac, 3);
+    ia = newSWFInitAction_withId(newSWFAction(
+                "if (!_global.Bug) {"
+                "   _global.Bug = function() {"
+                "       super();"
+                "       this.onLoad = function() {};"
+                "       this.onUnload = function() {};"
+                "       this.a = 5;"
+                "   };"
+                "} "
+                ), 4);
     SWFMovie_add(mo, (SWFBlock)ia);
     
-    // Init actions for ID 2 (registered class)
-    ac1 = newSWFAction("Object.registerClass('C2', ctor); "
-            "trace('Registered class');");
-    ia = newSWFInitAction_withId(ac1, 2);
+    mc4 = newSWFMovieClip();
+    //SWFMovie_add(mo, (SWFBlock)mc4);
+    mc4 = newSWFMovieClip();
+    //SWFMovie_add(mo, (SWFBlock)mc4);
+    SWFMovie_addExport(mo, (SWFBlock)mc4, "moo");
+    SWFMovie_writeExports(mo);
+    
+    
+    SWFMovie_add(mo, (SWFBlock)mc4);
+    
+    
+    SWFMovie_add(mo, (SWFBlock)mc4);
+
+    ia = newSWFInitAction_withId(newSWFAction(
+                "trace('hi');"
+                "Object.registerClass('MC1', Bug);"
+                ), 6);
     SWFMovie_add(mo, (SWFBlock)ia);
-	
-    
-    // Frame 2
-    SWFMovie_nextFrame(mo);
-    add_actions(mo, "trace('Frame 2');");
-    
-    // Place object ID 2.
-    it = SWFMovie_add(mo, (SWFBlock)mc2);
-    SWFDisplayItem_setName(it, "mc2");
 
-    // Frame 3
-    SWFMovie_nextFrame(mo);
 
-    // Remove object ID 2
-    SWFMovie_remove(mo, it);
 
-    add_actions(mo, "trace('frame 3');");
-    
-    // The class should not be constructed if the object is removed after
-    // being placed. It should be constructed if it's not removed.
-    xcheck(mo, "c == 0");
-    check(mo, "_root.mc2 == undefined");
-    add_actions(mo, "gotoAndStop(5);");
-
-    // Frame 4
-    SWFMovie_nextFrame(mo);
-    add_actions(mo, "trace('Frame 4');");
-    
-    // Place object ID 2 again
-    it = SWFMovie_add(mo, (SWFBlock)mc2);
-    SWFDisplayItem_setName(it, "mc2a");
-
-    // Frame 5
-    SWFMovie_nextFrame(mo);
-
-    // This time the MovieClip was not removed before we get here,
-    // so it should be present and the constructor should be
-    // called.
-    add_actions(mo, "trace('frame 5');");
-    xcheck(mo, "c == 1");
-    check(mo, "typeof(_root.mc2a) == 'movieclip'");
+    //dejagnuclip = get_dejagnu_clip((SWFBlock)get_default_font(srcdir), 10,
+    //		    0, 0, 800, 600);
+    //SWFMovie_add(mo, (SWFBlock)dejagnuclip);
 
     SWFMovie_nextFrame(mo);
   
--- testsuite/misc-ming.all/RegisterClassTest3.c
+++ testsuite/misc-ming.all/RegisterClassTest3.c
@@ -12,10 +12,10 @@ int
 main(int argc, char** argv)
 {
     SWFMovie mo;
-    SWFMovieClip mc1, mc2, mc3, mc4, dejagnuclip;
-    SWFAction ac;
+    SWFMovieClip mc3, mc2, dejagnuclip;
+    SWFAction ac, ac1;
     SWFDisplayItem it;
-    SWFInitAction ia;
+    SWFShape sha;
 
     const char *srcdir=".";
     if (argc > 1) srcdir = argv[1];
@@ -27,59 +27,88 @@ main(int argc, char** argv)
     Ming_init();
     mo = newSWFMovieWithVersion(OUTPUT_VERSION);
     SWFMovie_setDimension(mo, 800, 600);
- 
     SWFMovie_setRate (mo, 12.0);
 
-    mc1 = newSWFMovieClip();
-    SWFMovieClip_nextFrame(mc1);
-    
-    add_actions(mo, "onEnterFrame = function() { "
-            "trace(_level0.mc._currentframe + ':' + "
-            "           _level0.mc.Segments.a);"
-            "};"
-            );
-    
+    sha = newSWFShape();
+
+    // Character ID 2. Has 1 showframe. Is exported first.
     mc2 = newSWFMovieClip();
+    SWFMovieClip_add(mc2, (SWFBlock)sha);
     SWFMovieClip_nextFrame(mc2);
-    SWFMovie_addExport(mo, (SWFBlock)mc2, "MC1");
+
+    // Export it.
+    SWFMovie_addExport(mo, (SWFBlock)mc2, "C2");
     SWFMovie_writeExports(mo);
 
+    // Main timeline actions for frame 1
+    add_actions(mo, "var c = 0; trace('frame 1'); gotoAndStop(3);");
+    
+    // ID 3 is defined here. It has no showframe. It is exported immediately.
     mc3 = newSWFMovieClip();
-    it = SWFMovieClip_add(mc3, (SWFBlock)mc2);
-    SWFDisplayItem_setName(it, "Segments");
-    SWFMovieClip_nextFrame(mc3);
-    SWFMovieClip_remove(mc3, it);
-    it = SWFMovieClip_add(mc3, (SWFBlock)mc1);
-    SWFMovieClip_nextFrame(mc3);
-
-    it = SWFMovie_add(mo, (SWFBlock)mc3);
-    SWFDisplayItem_setName(it, "mc");
-
-    mc4 = newSWFMovieClip();
-    
-    SWFMovie_addExport(mo, (SWFBlock)mc4, "MC4");
+    SWFMovie_addExport(mo, (SWFBlock)mc3, "ctor");
     SWFMovie_writeExports(mo);
 
-    ia = newSWFInitAction_withId(newSWFAction(
-                "if (!_global.Bug) {"
-                "   _global.Bug = function() {"
-                "       super();"
-                "       this.onLoad = function() {};"
-                "       this.onUnload = function() {};"
-                "       this.a = 5;"
-                "   };"
-                "} "
-                ), 4);
+    dejagnuclip = get_dejagnu_clip((SWFBlock)get_default_font(srcdir), 10,
+    		    0, 0, 800, 600);
+    SWFMovie_add(mo, (SWFBlock)dejagnuclip);
+
+    // Init actions for ID 3
+    ac = newSWFAction(
+    "   _global.ctor = function () {"
+    "       super();"
+    "       trace('Object in Frame 2 is constructed');"
+    "       c += 1;"
+    "   };"
+    );
+    SWFInitAction ia = newSWFInitAction_withId(ac, 3);
     SWFMovie_add(mo, (SWFBlock)ia);
     
-    ia = newSWFInitAction_withId(newSWFAction(
-                "Object.registerClass('MC1', Bug);"
-                ), 1);
+    // Init actions for ID 2 (registered class)
+    ac1 = newSWFAction("Object.registerClass('C2', ctor); "
+            "trace('Registered class');");
+    ia = newSWFInitAction_withId(ac1, 2);
     SWFMovie_add(mo, (SWFBlock)ia);
+	
+    
+    // Frame 2
+    SWFMovie_nextFrame(mo);
+    add_actions(mo, "trace('Frame 2');");
+    
+    // Place object ID 2.
+    it = SWFMovie_add(mo, (SWFBlock)mc2);
+    SWFDisplayItem_setName(it, "mc2");
 
-    //dejagnuclip = get_dejagnu_clip((SWFBlock)get_default_font(srcdir), 10,
-    //		    0, 0, 800, 600);
-    //SWFMovie_add(mo, (SWFBlock)dejagnuclip);
+    // Frame 3
+    SWFMovie_nextFrame(mo);
+
+    // Remove object ID 2
+    SWFMovie_remove(mo, it);
+
+    add_actions(mo, "trace('frame 3');");
+    
+    // The class should not be constructed if the object is removed after
+    // being placed. It should be constructed if it's not removed.
+    xcheck(mo, "c == 0");
+    check(mo, "_root.mc2 == undefined");
+    add_actions(mo, "gotoAndStop(5);");
+
+    // Frame 4
+    SWFMovie_nextFrame(mo);
+    add_actions(mo, "trace('Frame 4');");
+    
+    // Place object ID 2 again
+    it = SWFMovie_add(mo, (SWFBlock)mc2);
+    SWFDisplayItem_setName(it, "mc2a");
+
+    // Frame 5
+    SWFMovie_nextFrame(mo);
+
+    // This time the MovieClip was not removed before we get here,
+    // so it should be present and the constructor should be
+    // called.
+    add_actions(mo, "trace('frame 5');");
+    xcheck(mo, "c == 1");
+    check(mo, "typeof(_root.mc2a) == 'movieclip'");
 
     SWFMovie_nextFrame(mo);
   
