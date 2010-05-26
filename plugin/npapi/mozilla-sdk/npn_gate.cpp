3b42a2b30f00287544502281baa2ec1b0e509397
Rob Savoye <rob@welcomehome.org>
2010-05-25 18:01:30 -0600

    Add support for Cookie handling functions in NPAPI. Fix code style

--- plugin/npapi/mozilla-sdk/npn_gate.cpp
+++ plugin/npapi/mozilla-sdk/npn_gate.cpp
@@ -1,44 +1,27 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: NPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Netscape Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/NPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is mozilla.org code.
- *
- * The Initial Developer of the Original Code is 
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1998
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or 
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the NPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the NPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+// 
+//   Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Free Software
+//   Foundation, Inc
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 
 ////////////////////////////////////////////////////////////
 //
 // Implementation of Netscape entry points (NPN_*)
 //
+#include "plugin.h"
 #include "npapi.h"
 #include "npfunctions.h"
 
@@ -52,269 +35,428 @@
 
 extern NPNetscapeFuncs NPNFuncs;
 
-void NPN_Version(int* plugin_major, int* plugin_minor, int* netscape_major, int* netscape_minor)
+void
+NPN_Version(int* plugin_major, int* plugin_minor,
+            int* netscape_major, int* netscape_minor)
 {
-  *plugin_major   = NP_VERSION_MAJOR;
-  *plugin_minor   = NP_VERSION_MINOR;
-  *netscape_major = HIBYTE(NPNFuncs.version);
-  *netscape_minor = LOBYTE(NPNFuncs.version);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    *plugin_major   = NP_VERSION_MAJOR;
+    *plugin_minor   = NP_VERSION_MINOR;
+    *netscape_major = HIBYTE(NPNFuncs.version);
+    *netscape_minor = LOBYTE(NPNFuncs.version);
 }
 
-NPError NPN_GetURLNotify(NPP instance, const char *url, const char *target, void* notifyData)
+NPError
+NPN_GetURLNotify(NPP instance,
+                 const char *url, const char *target, void* notifyData)
 {
-	int navMinorVers = NPNFuncs.version & 0xFF;
-  NPError rv = NPERR_NO_ERROR;
+    gnash::log_debug(__PRETTY_FUNCTION__);
 
-  if( navMinorVers >= NPVERS_HAS_NOTIFICATION )
-		rv = NPNFuncs.geturlnotify(instance, url, target, notifyData);
-	else
-		rv = NPERR_INCOMPATIBLE_VERSION_ERROR;
-
-  return rv;
+    int navMinorVers = NPNFuncs.version & 0xFF;
+    NPError rv = NPERR_NO_ERROR;
+    
+    if( navMinorVers >= NPVERS_HAS_NOTIFICATION ) {
+        rv = NPNFuncs.geturlnotify(instance, url, target, notifyData);
+    } else {
+        rv = NPERR_INCOMPATIBLE_VERSION_ERROR;
+    }
+    
+    return rv;
 }
 
-NPError NPN_GetURL(NPP instance, const char *url, const char *target)
+NPError
+NPN_GetURL(NPP instance, const char *url, const char *target)
 {
-  NPError rv = NPNFuncs.geturl(instance, url, target);
-  return rv;
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPError rv = NPNFuncs.geturl(instance, url, target);
+    return rv;
 }
 
-NPError NPN_PostURLNotify(NPP instance, const char* url, const char* window, uint32_t len, const char* buf, NPBool file, void* notifyData)
+NPError
+NPN_PostURLNotify(NPP instance, const char* url, const char* window,
+                  uint32_t len, const char* buf, NPBool file, void* notifyData)
 {
-	int navMinorVers = NPNFuncs.version & 0xFF;
-  NPError rv = NPERR_NO_ERROR;
+    gnash::log_debug(__PRETTY_FUNCTION__);
 
-	if( navMinorVers >= NPVERS_HAS_NOTIFICATION )
-		rv = NPNFuncs.posturlnotify(instance, url, window, len, buf, file, notifyData);
-	else
-		rv = NPERR_INCOMPATIBLE_VERSION_ERROR;
-
-  return rv;
+    int navMinorVers = NPNFuncs.version & 0xFF;
+    NPError rv = NPERR_NO_ERROR;
+    
+    if( navMinorVers >= NPVERS_HAS_NOTIFICATION ) {
+        rv = NPNFuncs.posturlnotify(instance, url, window, len, buf, file,
+                                    notifyData);
+    } else {
+        rv = NPERR_INCOMPATIBLE_VERSION_ERROR;
+    }
+    
+    return rv;
 }
 
-NPError NPN_PostURL(NPP instance, const char* url, const char* window, uint32_t len, const char* buf, NPBool file)
+NPError
+NPN_PostURL(NPP instance, const char* url, const char* window,
+            uint32_t len, const char* buf, NPBool file)
 {
-  NPError rv = NPNFuncs.posturl(instance, url, window, len, buf, file);
-  return rv;
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPError rv = NPNFuncs.posturl(instance, url, window, len, buf, file);
+    return rv;
 } 
 
-NPError NPN_RequestRead(NPStream* stream, NPByteRange* rangeList)
+NPError
+NPN_RequestRead(NPStream* stream, NPByteRange* rangeList)
 {
-  NPError rv = NPNFuncs.requestread(stream, rangeList);
-  return rv;
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPError rv = NPNFuncs.requestread(stream, rangeList);
+    return rv;
 }
 
-NPError NPN_NewStream(NPP instance, NPMIMEType type, const char* target, NPStream** stream)
+NPError
+NPN_NewStream(NPP instance, NPMIMEType type, const char* target,
+                      NPStream** stream)
 {
-	int navMinorVersion = NPNFuncs.version & 0xFF;
+    gnash::log_debug(__PRETTY_FUNCTION__);
 
-  NPError rv = NPERR_NO_ERROR;
+    int navMinorVersion = NPNFuncs.version & 0xFF;
 
-	if( navMinorVersion >= NPVERS_HAS_STREAMOUTPUT )
-		rv = NPNFuncs.newstream(instance, type, target, stream);
-	else
-		rv = NPERR_INCOMPATIBLE_VERSION_ERROR;
-
-  return rv;
+    NPError rv = NPERR_NO_ERROR;
+    
+    if( navMinorVersion >= NPVERS_HAS_STREAMOUTPUT ) {
+        rv = NPNFuncs.newstream(instance, type, target, stream);
+    } else {
+        rv = NPERR_INCOMPATIBLE_VERSION_ERROR;
+    }
+    
+    return rv;
 }
 
-int32_t NPN_Write(NPP instance, NPStream *stream, int32_t len, void *buffer)
+int32_t
+NPN_Write(NPP instance, NPStream *stream, int32_t len, void *buffer)
 {
-	int navMinorVersion = NPNFuncs.version & 0xFF;
-  int32_t rv = 0;
+    gnash::log_debug(__PRETTY_FUNCTION__);
 
-  if( navMinorVersion >= NPVERS_HAS_STREAMOUTPUT )
-		rv = NPNFuncs.write(instance, stream, len, buffer);
-	else
-		rv = -1;
-
-  return rv;
+    int navMinorVersion = NPNFuncs.version & 0xFF;
+    int32_t rv = 0;
+    
+    if( navMinorVersion >= NPVERS_HAS_STREAMOUTPUT ) {
+        rv = NPNFuncs.write(instance, stream, len, buffer);
+    } else {
+        rv = -1;
+    }
+    
+    return rv;
 }
 
-NPError NPN_DestroyStream(NPP instance, NPStream* stream, NPError reason)
+NPError
+NPN_DestroyStream(NPP instance, NPStream* stream, NPError reason)
 {
-	int navMinorVersion = NPNFuncs.version & 0xFF;
-  NPError rv = NPERR_NO_ERROR;
+    gnash::log_debug(__PRETTY_FUNCTION__);
 
-  if( navMinorVersion >= NPVERS_HAS_STREAMOUTPUT )
-		rv = NPNFuncs.destroystream(instance, stream, reason);
-	else
-		rv = NPERR_INCOMPATIBLE_VERSION_ERROR;
-
-  return rv;
+    int navMinorVersion = NPNFuncs.version & 0xFF;
+    NPError rv = NPERR_NO_ERROR;
+    
+    if( navMinorVersion >= NPVERS_HAS_STREAMOUTPUT ) {
+        rv = NPNFuncs.destroystream(instance, stream, reason);
+    } else {
+        rv = NPERR_INCOMPATIBLE_VERSION_ERROR;
+    }
+    
+    return rv;
 }
 
-void NPN_Status(NPP instance, const char *message)
+void
+NPN_Status(NPP instance, const char *message)
 {
-  NPNFuncs.status(instance, message);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPNFuncs.status(instance, message);
 }
 
-const char* NPN_UserAgent(NPP instance)
+const char *
+NPN_UserAgent(NPP instance)
 {
-  const char * rv = NULL;
-  rv = NPNFuncs.uagent(instance);
-  return rv;
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    const char * rv = NULL;
+    rv = NPNFuncs.uagent(instance);
+    return rv;
 }
 
-void* NPN_MemAlloc(uint32_t size)
+void *
+NPN_MemAlloc(uint32_t size)
 {
-  void * rv = NULL;
-  rv = NPNFuncs.memalloc(size);
-  return rv;
+    void * rv = NULL;
+    rv = NPNFuncs.memalloc(size);
+    return rv;
 }
 
-void NPN_MemFree(void* ptr)
+void
+NPN_MemFree(void* ptr)
 {
-  NPNFuncs.memfree(ptr);
+    NPNFuncs.memfree(ptr);
 }
 
-uint32_t NPN_MemFlush(uint32_t size)
+uint32_t
+NPN_MemFlush(uint32_t size)
 {
-  uint32_t rv = NPNFuncs.memflush(size);
-  return rv;
+    uint32_t rv = NPNFuncs.memflush(size);
+    return rv;
 }
 
-void NPN_ReloadPlugins(NPBool reloadPages)
+void
+NPN_ReloadPlugins(NPBool reloadPages)
 {
-  NPNFuncs.reloadplugins(reloadPages);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPNFuncs.reloadplugins(reloadPages);
 }
 
-NPError NPN_GetValue(NPP instance, NPNVariable variable, void *value)
+NPError
+NPN_GetValue(NPP instance, NPNVariable variable, void *value)
 {
-  NPError rv = NPNFuncs.getvalue(instance, variable, value);
-  return rv;
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPError rv = NPNFuncs.getvalue(instance, variable, value);
+    return rv;
 }
 
-NPError NPN_SetValue(NPP instance, NPPVariable variable, void *value)
+NPError
+NPN_SetValue(NPP instance, NPPVariable variable, void *value)
 {
-  NPError rv = NPNFuncs.setvalue(instance, variable, value);
-  return rv;
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPError rv = NPNFuncs.setvalue(instance, variable, value);
+    return rv;
 }
 
-void NPN_InvalidateRect(NPP instance, NPRect *invalidRect)
+void
+NPN_InvalidateRect(NPP instance, NPRect *invalidRect)
 {
-  NPNFuncs.invalidaterect(instance, invalidRect);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPNFuncs.invalidaterect(instance, invalidRect);
 }
 
-void NPN_InvalidateRegion(NPP instance, NPRegion invalidRegion)
+void
+NPN_InvalidateRegion(NPP instance, NPRegion invalidRegion)
 {
-  NPNFuncs.invalidateregion(instance, invalidRegion);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPNFuncs.invalidateregion(instance, invalidRegion);
 }
 
-void NPN_ForceRedraw(NPP instance)
+void
+NPN_ForceRedraw(NPP instance)
 {
-  NPNFuncs.forceredraw(instance);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPNFuncs.forceredraw(instance);
 }
 
-NPIdentifier NPN_GetStringIdentifier(const NPUTF8 *name)
+NPIdentifier
+NPN_GetStringIdentifier(const NPUTF8 *name)
 {
-  return NPNFuncs.getstringidentifier(name);
+    return NPNFuncs.getstringidentifier(name);
 }
 
-void NPN_GetStringIdentifiers(const NPUTF8 **names, uint32_t nameCount,
+void
+NPN_GetStringIdentifiers(const NPUTF8 **names, uint32_t nameCount,
                               NPIdentifier *identifiers)
 {
-  return NPNFuncs.getstringidentifiers(names, nameCount, identifiers);
+    return NPNFuncs.getstringidentifiers(names, nameCount, identifiers);
 }
 
-NPIdentifier NPN_GetStringIdentifier(int32_t intid)
+NPIdentifier
+NPN_GetStringIdentifier(int32_t intid)
 {
-  return NPNFuncs.getintidentifier(intid);
+    return NPNFuncs.getintidentifier(intid);
 }
 
-bool NPN_IdentifierIsString(NPIdentifier identifier)
+bool
+NPN_IdentifierIsString(NPIdentifier identifier)
 {
-  return NPNFuncs.identifierisstring(identifier);
+    return NPNFuncs.identifierisstring(identifier);
 }
 
-NPUTF8 *NPN_UTF8FromIdentifier(NPIdentifier identifier)
+NPUTF8 *
+NPN_UTF8FromIdentifier(NPIdentifier identifier)
 {
-  return NPNFuncs.utf8fromidentifier(identifier);
+    return NPNFuncs.utf8fromidentifier(identifier);
 }
 
-int32_t NPN_IntFromIdentifier(NPIdentifier identifier)
+int32_t
+NPN_IntFromIdentifier(NPIdentifier identifier)
 {
-  return NPNFuncs.intfromidentifier(identifier);
+    return NPNFuncs.intfromidentifier(identifier);
 }
 
-NPObject *NPN_CreateObject(NPP npp, NPClass *aClass)
+NPObject *
+NPN_CreateObject(NPP npp, NPClass *aClass)
 {
-  return NPNFuncs.createobject(npp, aClass);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.createobject(npp, aClass);
 }
 
-NPObject *NPN_RetainObject(NPObject *obj)
+NPObject *
+NPN_RetainObject(NPObject *obj)
 {
-  return NPNFuncs.retainobject(obj);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.retainobject(obj);
 }
 
-void NPN_ReleaseObject(NPObject *obj)
+void
+NPN_ReleaseObject(NPObject *obj)
 {
-  return NPNFuncs.releaseobject(obj);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.releaseobject(obj);
 }
 
-bool NPN_Invoke(NPP npp, NPObject* obj, NPIdentifier methodName,
-                const NPVariant *args, uint32_t argCount, NPVariant *result)
+bool
+NPN_Invoke(NPP npp, NPObject* obj, NPIdentifier methodName,
+           const NPVariant *args, uint32_t argCount, NPVariant *result)
 {
-  return NPNFuncs.invoke(npp, obj, methodName, args, argCount, result);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.invoke(npp, obj, methodName, args, argCount, result);
 }
 
-bool NPN_InvokeDefault(NPP npp, NPObject* obj, const NPVariant *args,
-                       uint32_t argCount, NPVariant *result)
+bool
+NPN_InvokeDefault(NPP npp, NPObject* obj, const NPVariant *args,
+                  uint32_t argCount, NPVariant *result)
 {
-  return NPNFuncs.invokeDefault(npp, obj, args, argCount, result);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.invokeDefault(npp, obj, args, argCount, result);
 }
 
-bool NPN_Evaluate(NPP npp, NPObject* obj, NPString *script,
-                  NPVariant *result)
+bool
+NPN_Evaluate(NPP npp, NPObject* obj, NPString *script,
+             NPVariant *result)
 {
-  return NPNFuncs.evaluate(npp, obj, script, result);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.evaluate(npp, obj, script, result);
 }
 
-bool NPN_GetProperty(NPP npp, NPObject* obj, NPIdentifier propertyName,
-                     NPVariant *result)
+bool
+NPN_GetProperty(NPP npp, NPObject* obj, NPIdentifier propertyName,
+                NPVariant *result)
 {
-  return NPNFuncs.getproperty(npp, obj, propertyName, result);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.getproperty(npp, obj, propertyName, result);
 }
 
-bool NPN_SetProperty(NPP npp, NPObject* obj, NPIdentifier propertyName,
-                     const NPVariant *value)
+bool
+NPN_SetProperty(NPP npp, NPObject* obj, NPIdentifier propertyName,
+                const NPVariant *value)
 {
-  return NPNFuncs.setproperty(npp, obj, propertyName, value);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.setproperty(npp, obj, propertyName, value);
 }
 
-bool NPN_RemoveProperty(NPP npp, NPObject* obj, NPIdentifier propertyName)
+bool
+NPN_RemoveProperty(NPP npp, NPObject* obj, NPIdentifier propertyName)
 {
-  return NPNFuncs.removeproperty(npp, obj, propertyName);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.removeproperty(npp, obj, propertyName);
 }
 
-bool NPN_Enumerate(NPP npp, NPObject *obj, NPIdentifier **identifier,
-                   uint32_t *count)
+bool
+NPN_Enumerate(NPP npp, NPObject *obj, NPIdentifier **identifier,
+              uint32_t *count)
 {
-  return NPNFuncs.enumerate(npp, obj, identifier, count);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.enumerate(npp, obj, identifier, count);
 }
 
-bool NPN_Construct(NPP npp, NPObject *obj, const NPVariant *args,
-                   uint32_t argCount, NPVariant *result)
+bool
+NPN_Construct(NPP npp, NPObject *obj, const NPVariant *args,
+              uint32_t argCount, NPVariant *result)
 {
-  return NPNFuncs.construct(npp, obj, args, argCount, result);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.construct(npp, obj, args, argCount, result);
 }
 
-bool NPN_HasProperty(NPP npp, NPObject* obj, NPIdentifier propertyName)
+bool
+NPN_HasProperty(NPP npp, NPObject* obj, NPIdentifier propertyName)
 {
-  return NPNFuncs.hasproperty(npp, obj, propertyName);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.hasproperty(npp, obj, propertyName);
 }
 
-bool NPN_HasMethod(NPP npp, NPObject* obj, NPIdentifier methodName)
+bool
+NPN_HasMethod(NPP npp, NPObject* obj, NPIdentifier methodName)
 {
-  return NPNFuncs.hasmethod(npp, obj, methodName);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.hasmethod(npp, obj, methodName);
 }
 
-void NPN_ReleaseVariantValue(NPVariant *variant)
+void
+NPN_ReleaseVariantValue(NPVariant *variant)
 {
-  NPNFuncs.releasevariantvalue(variant);
+    NPNFuncs.releasevariantvalue(variant);
 }
 
-void NPN_SetException(NPObject* obj, const NPUTF8 *message)
+void
+NPN_SetException(NPObject* obj, const NPUTF8 *message)
 {
-  NPNFuncs.setexception(obj, message);
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPNFuncs.setexception(obj, message);
 }
+
+NPError
+NPN_GetValueForURL(NPP instance, NPNURLVariable variable,
+                   const char *url, char **value, uint32_t *len)
+{
+    gnash::log_debug("%s: %s", __PRETTY_FUNCTION__, url);
+    
+    return NPNFuncs.getvalueforurl(instance, variable, url, value, len);
+}
+
+NPError
+NPN_SetValueForURL(NPP instance, NPNURLVariable variable,
+                   const char *url, const char *value, uint32_t len)
+{
+    gnash::log_debug("%s: %s, %s", __PRETTY_FUNCTION__, url, value);
+
+    return NPNFuncs.setvalueforurl(instance, variable, url, value, len);
+}
+
+NPError
+NPN_GetAuthenticationInfo(NPP instance, const char *protocol,
+                          const char *host, int32_t port,
+                          const char *scheme, const char *realm,
+                          char **username, uint32_t *ulen,
+                          char **password, uint32_t *plen)
+{
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    return NPNFuncs.getauthenticationinfo(instance, protocol, host, port,
+                                          scheme, realm, username, ulen,
+                                          password, plen);
+}
+  
+void
+NPN_PluginThreadAsyncCall(NPP plugin, void (*func)(void *), void *userData)
+{
+    gnash::log_debug(__PRETTY_FUNCTION__);
+
+    NPNFuncs.pluginthreadasynccall(plugin, func, userData);
+}
+
+// Local Variables:
+// mode: C++
+// indent-tabs-mode: nil
+// End:
