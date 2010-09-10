0dd6b94867ec42c4b8a5897cd12f4711d0dcc42f
Sandro Santilli <strk@keybit.net>
2010-09-10 04:13:20 +0200

    Add string_table stat machinery (dirty, but damn useful. disabled by default)

--- libbase/string_table.cpp
+++ libbase/string_table.cpp
@@ -20,7 +20,46 @@
 #include "string_table.h"
 #include <boost/algorithm/string/case_conv.hpp>
 
+//#define DEBUG_STRING_TABLE 1
+
+#ifdef DEBUG_STRING_TABLE
+# include <iostream>
+# include <iomanip>
+#endif
+
 namespace gnash {
+
+#ifdef DEBUG_STRING_TABLE
+    namespace {
+    class KeyCaseLookup {
+        typedef std::map<string_table::key, unsigned long int> Stat;
+        Stat stat;
+        const string_table* _st;
+    public:
+        KeyCaseLookup(const string_table* st) : _st(st) {}
+
+        void check(string_table::key k) {
+            ++stat[k];
+        }
+        ~KeyCaseLookup(){ 
+            typedef std::map<unsigned long int, string_table::key> Sorted;
+            Sorted sorted;
+            for (Stat::iterator i=stat.begin(), e=stat.end(); i!=e; ++i)
+                sorted[i->second] = i->first;
+            for (Sorted::reverse_iterator i=sorted.rbegin(), e=sorted.rend();
+                    i!=e; ++i)
+                std::cerr
+                          << std::setw(10)
+                          << i->first
+                          << ":"
+                          << _st->value(i->second) << "("
+                          << i->second << ")"
+                          << std::endl;
+        }
+    };
+    } // namespace anonymous
+#endif // DEBUG_STRING_TABLE
+
 
 const std::string string_table::_empty;
 
@@ -79,6 +118,10 @@ string_table::insert_group(const svt* l, std::size_t size)
             _caseTable[s.id] = already_locked_insert(t);
         }
     }
+#ifdef DEBUG_STRING_TABLE
+    std::cerr << "string_table group insert end -- size is " << _table.size() << " _caseTable size is " << _caseTable.size() << std::endl; 
+#endif
+
 
 }
 
@@ -86,6 +129,12 @@ string_table::key
 string_table::already_locked_insert(const std::string& to_insert)
 {
 	const key ret = _table.insert(svt(to_insert, ++_highestKey)).first->id;
+
+#ifdef DEBUG_STRING_TABLE
+    int tscp = 20; // table size checkpoint
+    size_t ts = _table.size();
+    if ( ! (ts % tscp) ) { std::cerr << "string_table size grew to " << ts << std::endl; }
+#endif
 
     const std::string lower = boost::to_lower_copy(to_insert);
 
@@ -100,6 +149,11 @@ string_table::already_locked_insert(const std::string& to_insert)
         const key nocase = (it == _table.end()) ? 
             _table.insert(svt(lower, ++_highestKey)).first->id : it->id;
 
+#ifdef DEBUG_STRING_TABLE
+        ++ts;
+        if ( ! (ts % tscp) ) { std::cerr << "string_table size grew to " << ts << std::endl; }
+#endif // DEBUG_STRING_TABLE
+
         _caseTable[ret] = nocase;
 
     }
@@ -110,6 +164,14 @@ string_table::already_locked_insert(const std::string& to_insert)
 string_table::key
 string_table::noCase(key a) const
 {
+#ifdef DEBUG_STRING_TABLE
+    static KeyCaseLookup kcl(this);
+    kcl.check(a);
+#endif // DEBUG_STRING_TABLE
+
+    // The empty string (key 0) is equal to its lowercase version..
+    if ( ! a ) return a;
+
     std::map<key, key>::const_iterator i = _caseTable.find(a);
     return i == _caseTable.end() ? a : i->second;
 }
