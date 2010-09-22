0daf55e4c76aae0db63a7c3644c82502cac08382
Sandro Santilli <strk@keybit.net>
2010-09-22 19:01:22 +0200

    Add stats debugging

--- libcore/ObjectURI.h
+++ libcore/ObjectURI.h
@@ -6,6 +6,12 @@
 #include <string>
 #include <ostream>
 
+#define GNASH_DEBUG_OBJECT_URI_NOCASE 1
+
+#ifdef GNASH_DEBUG_OBJECT_URI_NOCASE
+# include <iostream>
+#endif
+
 namespace gnash {
 
 /// A URI for describing as_objects.
@@ -42,8 +48,25 @@ struct ObjectURI
     }
 
 
+#ifdef GNASH_DEBUG_OBJECT_URI_NOCASE
+    struct Counter {
+        Counter(): count(0) {}
+        int count;
+        void operator++() { ++count; }
+        ~Counter () {
+            std::cerr << "Skipped " << count << " calls to noCase "<< std::endl;
+        }
+    };
+#endif
+
     string_table::key noCase(string_table& st) const {
+#ifdef GNASH_DEBUG_OBJECT_URI_NOCASE
+        static Counter skips;
+#endif
         if ( ! nameNoCase ) nameNoCase = st.noCase(name);
+#ifdef GNASH_DEBUG_OBJECT_URI_NOCASE
+        else ++skips;
+#endif
         return nameNoCase;
     }
 
