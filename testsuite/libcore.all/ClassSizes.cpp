3d96aaab326465b46ee9d4ac3fe78d67c3b953ab
Benjamin Wolsey <bwy@benjaminwolsey.de>
2010-09-28 14:00:23 +0200

    Add PropFlags to ClassSizes.

--- testsuite/libcore.all/ClassSizes.cpp
+++ testsuite/libcore.all/ClassSizes.cpp
@@ -82,7 +82,7 @@ using namespace gnash::SWF;
 (as_object) \
 (DisplayObject) (StaticText) (MorphShape) (Shape) \
 (InteractiveObject) (MovieClip) (TextField) (Button) (Movie) \
-(movie_root) 
+(movie_root) (PropFlags)
 
 int
 main(int /*argc*/, char** /*argv*/)
