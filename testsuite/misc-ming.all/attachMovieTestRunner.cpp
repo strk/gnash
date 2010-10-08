3618d5207a4476d3760af06def3c3050c6cee325
Benjamin Wolsey <bwy@benjaminwolsey.de>
2010-10-08 09:20:05 +0200

    Fix testsuite

--- testsuite/misc-ming.all/attachMovieTestRunner.cpp
+++ testsuite/misc-ming.all/attachMovieTestRunner.cpp
@@ -79,6 +79,8 @@ main(int /*argc*/, char** /*argv*/)
 	// check that the pixel under the mouse is white
 	check_pixel(100, 30, 2, rgba(255,255,255,255), 2);
 
+    VM& vm = VM::get();
+
 	string_table& st = VM::get().getStringTable();
 
 	getObject(root)->get_member(st.find("mousedown"), &tmp);
@@ -90,15 +92,15 @@ main(int /*argc*/, char** /*argv*/)
 	tester.pressMouseButton();
 
 	getObject(root)->get_member(st.find("mousedown"), &tmp);
-	check_equals(tmp.to_number(), 1);
+	check_equals(toNumber(tmp, vm), 1);
 	check ( ! getObject(root)->get_member(st.find("mouseup"), &tmp) );
 
 	tester.depressMouseButton();
 
 	getObject(root)->get_member(st.find("mousedown"), &tmp);
-	check_equals(tmp.to_number(), 1);
+	check_equals(toNumber(tmp, vm), 1);
 	getObject(root)->get_member(st.find("mouseup"), &tmp);
-	check_equals(tmp.to_number(), 1);
+	check_equals(toNumber(tmp, vm), 1);
 
 	tester.advance();
 
@@ -155,7 +157,7 @@ main(int /*argc*/, char** /*argv*/)
 	tester.pressMouseButton();
 
 	getObject(root)->get_member(st.find("mousedown"), &tmp);
-	check_equals(tmp.to_number(), 5);
+	check_equals(toNumber(tmp, vm), 5);
 
 }
 
