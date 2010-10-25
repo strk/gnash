ca933d82652edf1c34648173b244c54e563fdaba
Benjamin Wolsey <bwy@benjaminwolsey.de>
2010-10-25 14:08:05 +0200

    Tabs to spaces.

--- libcore/DragState.h
+++ libcore/DragState.h
@@ -34,100 +34,100 @@ class DragState
 {
 public:
 
-	DragState(DisplayObject* d)
-		:
-		_hasbounds(false),
-		_bounds(),
-		_displayObject(d),
-		_lock_centered(false)
-	{
-	}
+    DragState(DisplayObject* d)
+        :
+        _hasbounds(false),
+        _bounds(),
+        _displayObject(d),
+        _lock_centered(false)
+    {
+    }
 
-	bool isLockCentered() const {
-		return _lock_centered;
-	}
+    bool isLockCentered() const {
+        return _lock_centered;
+    }
 
-	void setLockCentered(bool lock) {
-		_lock_centered = lock;
-	}
+    void setLockCentered(bool lock) {
+        _lock_centered = lock;
+    }
 
-	/// Set displacement offset from origin
-	/// at time of drag start.
-	/// Coordinates are in stage space (twips)
-	///
-	void setOffset(boost::int32_t x, boost::int32_t y) {
-		_xoffset = x;
-		_yoffset = y;
-	}
+    /// Set displacement offset from origin
+    /// at time of drag start.
+    /// Coordinates are in stage space (twips)
+    ///
+    void setOffset(boost::int32_t x, boost::int32_t y) {
+        _xoffset = x;
+        _yoffset = y;
+    }
 
-	boost::int32_t xOffset() const { return _xoffset; }
-	boost::int32_t yOffset() const { return _yoffset; }
+    boost::int32_t xOffset() const { return _xoffset; }
+    boost::int32_t yOffset() const { return _yoffset; }
 
-	bool hasBounds() const {
+    bool hasBounds() const {
         return _hasbounds;
     }
 
-	/// \brief
-	/// Get the boundaries to constraint
-	/// the drag into.
-	//
-	/// Coordinates of the rectangle are
-	/// expected in TWIPS.
-	///
-	/// Note that if hasBounds() is false
-	/// the returned rectangle is the NULL
-	/// rectangle - see SWFRect::is_null().
-	///
-	const SWFRect& getBounds() const { return _bounds; }
+    /// \brief
+    /// Get the boundaries to constraint
+    /// the drag into.
+    //
+    /// Coordinates of the rectangle are
+    /// expected in TWIPS.
+    ///
+    /// Note that if hasBounds() is false
+    /// the returned rectangle is the NULL
+    /// rectangle - see SWFRect::is_null().
+    ///
+    const SWFRect& getBounds() const { return _bounds; }
 
-	/// \brief
-	/// Set the boundaries to constraint
-	/// the drag into.
-	//
-	/// Coordinates of the rectangle are
-	/// expected in TWIPS.
-	///
-	void setBounds(const SWFRect& bounds) {
-		_bounds = bounds;
-		_hasbounds = true;
-	}
+    /// \brief
+    /// Set the boundaries to constraint
+    /// the drag into.
+    //
+    /// Coordinates of the rectangle are
+    /// expected in TWIPS.
+    ///
+    void setBounds(const SWFRect& bounds) {
+        _bounds = bounds;
+        _hasbounds = true;
+    }
 
-	/// May return NULL !!
-	DisplayObject* getCharacter() const {
-		return _displayObject;
-	}
+    /// May return NULL !!
+    DisplayObject* getCharacter() const {
+        return _displayObject;
+    }
 
-	/// Reset drag state to its initial condition
-	void reset() {
-		_displayObject = 0;
-		_hasbounds = false;
-		_bounds.set_null();
-		_lock_centered = false;
-	}
+    /// Reset drag state to its initial condition
+    void reset() {
+        _displayObject = 0;
+        _hasbounds = false;
+        _bounds.set_null();
+        _lock_centered = false;
+    }
 
-	/// Mark DisplayObject as reachable (if any)
-	void markReachableResources() const {
-		if (_displayObject) _displayObject->setReachable();
-	}
+    /// Mark DisplayObject as reachable (if any)
+    void markReachableResources() const {
+        if (_displayObject) _displayObject->setReachable();
+    }
 
 private:
 
-	bool _hasbounds;
+    bool _hasbounds;
 
-	/// Boundaries to constrain the drag into.
-	/// Coordinates in TWIPS.
-	SWFRect _bounds;
+    /// Boundaries to constrain the drag into.
+    /// Coordinates in TWIPS.
+    SWFRect _bounds;
 
-	DisplayObject* _displayObject;
+    DisplayObject* _displayObject;
 
-	bool _lock_centered;
+    bool _lock_centered;
 
-	/// Offsets of displacement from DisplayObject origin
-	/// at time of drag start. These are used for non
-	/// lock-centered dragging.
-	/// Coordinates are in stage space (TWIPS)
-	boost::int32_t _xoffset;
-	boost::int32_t _yoffset;
+    /// Offsets of displacement from DisplayObject origin
+    /// at time of drag start. These are used for non
+    /// lock-centered dragging.
+    /// Coordinates are in stage space (TWIPS)
+    boost::int32_t _xoffset;
+    boost::int32_t _yoffset;
 
 };
 
