8178d1b50b8354a09bea59289d4e9840e85164be
Rob Savoye <rob@welcomehome.org>
2010-10-29 10:30:10 -0600

    hack in simple OpenVG drawing test, for debug only

--- librender/testr_gtk.cpp
+++ librender/testr_gtk.cpp
@@ -33,6 +33,26 @@
 # include <gtk/gtk.h>
 # include <gdk/gdk.h>
 #endif
+#ifdef RENDERER_AGG
+#include "Renderer_agg.h"
+#endif
+#ifdef RENDERER_OPENGL
+#include "Renderer_ogl.h"
+#endif
+#ifdef RENDERER_OPENVG
+#include "Renderer_ovg.h"
+#include <VG/openvg.h>
+#include <VG/vgu.h>
+#endif
+#ifdef RENDERER_GLES1
+#include "Renderer_gles1.h"
+#endif
+#ifdef RENDERER_GLES2
+#include "Renderer_gles2.h"
+#endif
+#ifdef RENDERER_CAIRO
+#include "Renderer_cairo.h"
+#endif
 
 #include "log.h"
 #include "SWFMatrix.h"
@@ -43,6 +63,12 @@
 
 using namespace gnash; 
 using namespace std; 
+
+const VGfloat white_color[4] = {1.0, 1.0, 1.0, 1.0};
+const VGfloat color[4] = {0.4, 0.1, 1.0, 1.0};
+
+VGPath path;
+VGPaint paint;
 
 // The debug log used by all the gnash libraries.
 static LogFile& dbglogfile = LogFile::getDefaultInstance();
@@ -63,9 +89,67 @@ void on_window1_realize (GtkWidget *widget, gpointer user_data);
 void on_window1_size_allocate (GtkWidget *widget, GdkRectangle *allocation,
                                         gpointer user_data);
 
-GtkWidget *
-create_GTK_window(int argc, char **argv)
+static void
+initVGDemo(void)
 {
+    VGfloat clearColor[] = {0.0f, 0.0f, 0.0f, 1.0f};/* black color */
+    VGfloat greenColor[] = {0.0f, 1.0f, 0.0f, 1.0f};/* green color */
+    VGint arcType = VGU_ARC_OPEN;
+    VGfloat x, y, w, h, startAngle, angleExtent;
+
+    x = 150;
+    y = 150;
+    w = 150;
+    h = 150;
+#if 0
+    startAngle  = -540.0f;
+    angleExtent = 270.0f;
+#else
+    startAngle  = 270.0f;
+    angleExtent = 90.0f;
+#endif
+    paint = vgCreatePaint();
+
+    vgSetPaint(paint, VG_STROKE_PATH);
+    vgSetParameterfv(paint, VG_PAINT_COLOR, 4, greenColor);
+    vgSetParameteri( paint, VG_PAINT_TYPE, VG_PAINT_TYPE_COLOR);
+    vgSetf(VG_STROKE_LINE_WIDTH, 6.0f);
+    vgSeti(VG_RENDERING_QUALITY, VG_RENDERING_QUALITY_NONANTIALIASED);
+    vgSetfv(VG_CLEAR_COLOR, 4, clearColor);
+
+    path  = vgCreatePath(VG_PATH_FORMAT_STANDARD, VG_PATH_DATATYPE_F,
+                         1.0f, 0.0f, 0, 0, VG_PATH_CAPABILITY_ALL);
+
+    vguArc(path, x, y, w, h, startAngle, angleExtent, (VGUArcType)arcType);
+
+    vgSeti(VG_STROKE_CAP_STYLE, VG_CAP_BUTT);
+    vgSeti(VG_STROKE_JOIN_STYLE, VG_JOIN_BEVEL);
+    vgSetf(VG_STROKE_MITER_LIMIT, 4.0f);
+}
+
+/* new window size or exposure */
+static void
+reshape(int w, int h)
+{
+   vgLoadIdentity();
+}
+
+static void
+draw(void)
+{
+   vgClear(0, 0, 500, 500);
+   vgDrawPath(path, VG_STROKE_PATH);
+
+   vgFlush();
+}
+
+GtkWidget *
+create_GTK_window()
+{
+    // As gtk_init() wants the command line arguments, we have to create
+    // fake ones, as we don't care about the X11 options at this point.
+    int argc = 0;
+    char **argv = 0;    
     gtk_init(&argc, &argv);
 
     GtkWidget *window;
@@ -84,7 +168,10 @@ create_GTK_window(int argc, char **argv)
     canvas = gtk_drawing_area_new();
     gtk_widget_show(canvas);
     gtk_container_add (GTK_CONTAINER (window), canvas);
-
+    // Disable double buffering, otherwise gtk tries to update widget
+    // contents from its internal offscreen buffer at the end of expose event
+    gtk_widget_set_double_buffered(canvas, FALSE);
+    
     g_signal_connect(GTK_OBJECT(window), "delete_event",
                      (GCallback)gtk_main_quit, NULL);
     g_signal_connect ((gpointer) window, "configure_event",
@@ -120,7 +207,7 @@ create_GTK_window(int argc, char **argv)
     // Just do one iteration of the main loop
     gtk_main_iteration_do(true);
     
-    return window;
+    return canvas;
 }
 
 void
@@ -171,7 +258,8 @@ on_window1_expose_event(GtkWidget *widget, GdkEventExpose  *event,
                         gpointer user_data)
 {
     GNASH_REPORT_FUNCTION;
-
+    vgLoadIdentity();
+    
     return FALSE;
 }
 
@@ -187,6 +275,7 @@ on_window1_size_allocate(GtkWidget *widget, GdkRectangle *allocation,
 {
     GNASH_REPORT_FUNCTION;
 }
+
 
 // Local Variables:
 // mode: C++
