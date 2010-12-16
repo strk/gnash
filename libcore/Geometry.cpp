a5347b059d3b24668449026411c032f048ffbb1a
Benjamin Wolsey <bwy@benjaminwolsey.de>
2010-12-16 11:33:40 +0100

    Clean up includes.

--- libcore/Geometry.cpp
+++ libcore/Geometry.cpp
@@ -16,8 +16,10 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
-#include <cmath>
 #include "Geometry.h"
+
+#include <cmath>
+
 #include "log.h"
 #include "LineStyle.h"
 
