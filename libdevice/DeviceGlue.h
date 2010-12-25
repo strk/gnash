37570697f54664a327af114b93727bb100daf477
Rob Savoye <rob@welcomehome.org>
2010-12-25 12:55:21 -0700

    add doxygen style comments

--- libdevice/DeviceGlue.h
+++ libdevice/DeviceGlue.h
@@ -46,7 +46,7 @@
 
 /// @note This file is a simple base class for any GUI glue layer code
 /// That needs to use libdevice devices. Currently this is used by both
-/// the GTK and Framebufffer GUIs for OpenVG, OpenGLES1, and OpenGLES2.
+/// the GTK and Framebuffer GUIs for OpenVG, OpenGLES1, and OpenGLES2.
 namespace gnash {
 
 class DeviceGlue {
@@ -54,6 +54,11 @@ public:
     DeviceGlue() {};
     ~DeviceGlue() {};
     
+    /// Probe the system to see what types of display devices exist. This
+    /// doesn't select a device, it merely returns a list of what is
+    /// available.
+    ///
+    /// @return a list of devices
     boost::shared_array<renderer::GnashDevice::dtype_t> probeDevices() {
         GNASH_REPORT_FUNCTION;
         
@@ -89,9 +94,11 @@ public:
 #endif
         return devs;
     }
-    
+
+    /// Reset the the current device, which disables output
     void resetDevice() { _device.reset(); };
     
+    /// Get the current active device type.
     renderer::GnashDevice::dtype_t getDevice()
     {
         if (_device) {
@@ -100,6 +107,8 @@ public:
         return renderer::GnashDevice::NODEV;
     }
     
+    /// Set the display device for later use. After this is called,
+    /// the display device is active.
     void setDevice(renderer::GnashDevice::dtype_t dtype) {
         switch (dtype) {
 #ifdef BUILD_EGL_DEVICE
@@ -140,27 +149,42 @@ public:
         // egl->printEGLSurface();
     }
 
+    /// Initialze the device
+    ///
+    /// @param argc The count of arguments from the command line
+    /// @param argv The array of command line arguments
+    /// @return status
     bool initDevice(int argc, char *argv[]) {
         return (_device) ? _device->initDevice(argc, argv) : false;
     };
 
+    /// Attach the area to draw in to the lower level device. This makes
+    /// the drawing area available to the dispaly device when binding the
+    /// display to the native windowing system or framebuffer.
     bool attachWindow(renderer::GnashDevice::native_window_t window) {
         return (_device) ? _device->attachWindow(window) : false;
     };
 
+    /// Bind the client API to the device. As EGL can support different
+    /// renderers, namely OpenG:, OpenGLES1, OpenGLES2, amd OpenVG. This
+    /// is how the underlying hardware knows which API to implement.
     bool bindClient(renderer::GnashDevice::rtype_t rtype) {
         return (_device) ? _device->bindClient(rtype) : false;
     };
 
-    /// \brief
-    ///  The Width of the drawing area, in pixels. For framebuffer
+    ///  Get the Width of the drawing area, in pixels. For framebuffer
     ///  based devices, this is the size of the display screen.
     size_t getWidth()  { return (_device) ? _device->getWidth() : 0; };
 
     /// Height of the drawing area, in pixels. For framebuffer
     ///  based devices, this is the size of the display screen.
     size_t getHeight() { return (_device) ? _device->getHeight() : 0; };
-    
+
+    /// Make the current buffer the active one.
+    bool swapBuffers() {
+        return (_device) ? _device->swapBuffers() : false;
+    }
+
 protected:
     boost::scoped_ptr<renderer::GnashDevice> _device;
 };
