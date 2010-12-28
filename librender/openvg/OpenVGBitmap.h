ede29ff2c92c8c8cfbdc333548e975c61f77840e
Rob Savoye <rob@welcomehome.org>
2010-12-27 19:38:40 -0700

    move code from header to be easier to debug and more compact

--- librender/openvg/OpenVGBitmap.h
+++ librender/openvg/OpenVGBitmap.h
@@ -30,98 +30,27 @@ namespace renderer {
 
 namespace openvg {
 
+/// This class is the OpenVG specific representation of a Gnash
+/// Cached Bitmap.
 class OpenVGBitmap : public CachedBitmap
 {
 public:
     /// Set line and fill styles for mesh & line_strip rendering.
     enum bitmap_wrap_mode { WRAP_REPEAT, WRAP_CLAMP };
     
-    OpenVGBitmap(std::auto_ptr<image::GnashImage> im)
-        : _image(im.release())
-    {
-        GNASH_REPORT_FUNCTION;
-    }
+    OpenVGBitmap(std::auto_ptr<image::GnashImage> im);
   
     OpenVGBitmap(std::auto_ptr<image::GnashImage> im,
-                    VGImageFormat pixelformat, VGPaint vgpaint)
-        : _image(im.release()),
-          _pixel_format(pixelformat),
-          _vgpaint(vgpaint)
-    {
-        GNASH_REPORT_FUNCTION;
-        
-        size_t width = _image->width();
-        size_t height = _image->height();
-        
-        _vgimage = vgCreateImage(VG_sRGB_565, width, height,
-                                 VG_IMAGE_QUALITY_FASTER);    
-        
-        vgImageSubData(_vgimage, _image->begin(), width * 4, VG_sRGB_565,
-                       0, 0, width, height);
-        
-        _tex_size += width * height * 4;
-        log_debug("Add Texture size:%d (%d x %d x %dbpp)", width * height * 4, 
-                  width, height, 4);
-        log_debug("Current Texture size: %d", _tex_size);
-    } 
-
-    ~OpenVGBitmap()
-    {
-        GNASH_REPORT_FUNCTION;
-        
-        _tex_size -= _image->width() * _image->height() * 4;
-        log_debug(_("Remove Texture size:%d (%d x %d x %dbpp)"),
-                  _image->width() * _image->height() * 4,
-                  _image->width(), _image->height(), 4);
-        log_debug(_("Current Texture size: %d"), _tex_size);
-        
-        vgDestroyImage(_vgimage);
-    }
+                 VGImageFormat pixelformat, VGPaint vgpaint);
+    ~OpenVGBitmap();
 
     void dispose()  { _image.reset(); }
     bool disposed() const { return !_image.get(); }
 
-    image::GnashImage& image() {
-        GNASH_REPORT_FUNCTION;
-        if (_image) {
-            return *_image;
-        }
-    }    
-
+    image::GnashImage& image();
+    
     void apply(const gnash::SWFMatrix& bitmap_matrix,
-               bitmap_wrap_mode wrap_mode) const
-    {
-        GNASH_REPORT_FUNCTION;
-        gnash::SWFMatrix mat;
-        VGfloat     vmat[9];
-        
-        mat = bitmap_matrix;
-        
-        vgSetParameteri (_vgpaint, VG_PAINT_TYPE, VG_PAINT_TYPE_PATTERN);
-        vgPaintPattern (_vgpaint, _vgimage);
-        
-        mat.invert();
-        memset(vmat, 0, sizeof(vmat));
-        vmat[0] = mat.sx  / 65536.0f;
-        vmat[1] = mat.shx / 65536.0f;
-        vmat[3] = mat.shy / 65536.0f;
-        vmat[4] = mat.sy  / 65536.0f;
-        vmat[6] = mat.tx;
-        vmat[7] = mat.ty;
-        
-        vgSeti (VG_MATRIX_MODE, VG_MATRIX_FILL_PAINT_TO_USER);
-        vgLoadMatrix (vmat);
-        vgSeti (VG_MATRIX_MODE, VG_MATRIX_STROKE_PAINT_TO_USER);
-        vgLoadMatrix (vmat);
-        vgSeti (VG_MATRIX_MODE, VG_MATRIX_PATH_USER_TO_SURFACE);
-        
-        if (wrap_mode == WRAP_CLAMP) {  
-            vgSetParameteri (_vgpaint, VG_PAINT_PATTERN_TILING_MODE, VG_TILE_PAD);
-        } else {
-            vgSetParameteri (_vgpaint, VG_PAINT_PATTERN_TILING_MODE, VG_TILE_REPEAT);
-        }
-    }
-
+               bitmap_wrap_mode wrap_mode) const;
     // Accessors for the GnashImage internal data
     int getWidth() { return _image->width(); };
     int getHeight() { return _image->height(); };
