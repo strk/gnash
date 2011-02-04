099e7b3640ea00f06f161a1b3312fd6ec3a1b184
Rob Savoye <rob@welcomehome.org>
2011-02-04 09:34:16 -0700

    update copyright date

--- libvaapi/VaapiDisplayX11.h
+++ libvaapi/VaapiDisplayX11.h
@@ -1,6 +1,6 @@
 // VaapiDisplayX11.h: VA/X11 display representation
 // 
-// Copyright (C) 2009, 2010 Free Software Foundation, Inc.
+// Copyright (C) 2009, 2010, 2011 Free Software Foundation, Inc.
 // 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
--- libvaapi/vaapi_common.h
+++ libvaapi/vaapi_common.h
@@ -1,6 +1,6 @@
 // vaapi_common.h: VA API common (internal) includes and definitions
 // 
-// Copyright (C) 2009, 2010 Free Software Foundation, Inc.
+// Copyright (C) 2009, 2010, 2011 Free Software Foundation, Inc.
 // 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
