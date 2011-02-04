59bbf946fb8702c0c0a4cba1b64aad38bc868894
Rob Savoye <rob@welcomehome.org>
2011-02-04 14:14:51 -0700

    update copyright date

--- cygnal/libnet/sslserver.cpp
+++ cygnal/libnet/sslserver.cpp
@@ -1,6 +1,6 @@
 // ssl.cpp:  HyperText Transport Protocol handler for Cygnal, for Gnash.
 // 
-//   Copyright (C) 2009, 2010 Free Software Foundation, Inc.
+//   Copyright (C) 2009, 2010, 2011 Free Software Foundation, Inc.
 // 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
