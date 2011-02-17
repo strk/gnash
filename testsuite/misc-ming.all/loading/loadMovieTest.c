12a026434bc252d59b338485e4c4a5a0e79e2819
Sandro Santilli <strk@keybit.net>
2011-02-17 17:15:19 +0100

    Avoid bug #32516 for now. I don't have a full week to squash all these bugs..

--- testsuite/misc-ming.all/loading/loadMovieTest.c
+++ testsuite/misc-ming.all/loading/loadMovieTest.c
@@ -347,7 +347,7 @@ main(int argc, char** argv)
 	add_button(mo, 50, 220, "Clear", newSWFAction(
 				" art=_root.coverart;"
 				" for (i=0; i<art.lastdepth; ++i) {"
-				"  removeMovieClip('_root.coverart.child'+i);" 
+				"  removeMovieClip('_root.cont.coverart.child'+i);" 
 				" }"
 				" art.lastdepth=0;"
 				" art.clear(); "
