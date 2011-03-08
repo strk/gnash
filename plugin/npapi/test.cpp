e1e31f625e4dc1bc07c5236454a243e9427995ce
Hicham HAOUARI <hicham.haouari@gmail.com>
2011-03-08 18:38:11 +0000

    Add gnashconfig.h to plugin/npapi/test.cpp

--- plugin/npapi/test.cpp
+++ plugin/npapi/test.cpp
@@ -15,6 +15,9 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
+#ifdef HAVE_CONFIG_H
+#include "gnashconfig.h"
+#endif
 
 #include <iostream>
 #include <string>
