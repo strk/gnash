4b75a4faa7aa7c8b9a83338bbef6c1ee05925b90
Sandro Santilli <strk@keybit.net>
2011-03-18 16:43:55 +0100

    Automate runner for test of bug #32833

---
+++ testsuite/misc-swfmill.all/missing_bitmapTestRunner.cpp
@@ -0,0 +1,71 @@
+/* 
+ *   Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Free Software Foundation, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ */ 
+
+#define INPUT_FILENAME "missing_bitmap.swf"
+
+#include "MovieTester.h"
+#include "GnashException.h"
+#include "MovieClip.h"
+#include "DisplayObject.h"
+#include "DisplayList.h"
+#include "log.h"
+
+#include "check.h"
+#include <string>
+#include <cassert>
+
+using namespace gnash;
+using namespace std;
+
+int
+main(int /*argc*/, char** /*argv*/)
+{
+	string filename = string(TGTDIR) + string("/") + string(INPUT_FILENAME);
+	auto_ptr<MovieTester> t;
+
+	gnash::LogFile& dbglogfile = gnash::LogFile::getDefaultInstance();
+	dbglogfile.setVerbosity(1);
+
+	try
+	{
+		t.reset(new MovieTester(filename));
+	}
+	catch (const GnashException& e)
+	{
+		std::cerr << "Error initializing MovieTester: " << e.what() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	
+	MovieTester& tester = *t;
+
+	// Colors used for pixel checking
+	rgba red(255,0,0,255);
+	rgba white(255,255,255,255);
+
+	int tol = 15;
+
+	xcheck_pixel(100, 100, 20, red, tol);
+	check_pixel(300, 100, 20, white, tol);
+	check_pixel(100, 300, 20, white, tol);
+
+	tester.advance();
+
+}
+
