99cf84a7fc6ba1778e9ce505cbad5a1c7af0a1fc
Benjamin Wolsey <bwy@benjaminwolsey.de>
2011-03-22 09:22:53 +0100

    Fix mangled copyright notice.

--- gui/kde/Kde4Glue.cpp
+++ gui/kde/Kde4Glue.cpp
@@ -4,12 +4,18 @@
 //   Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Free Software
 //   Foundation, Inc                                       
 //                 
-// This program is free software; you can redistribute it and/or modify         // it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 3 of the License, or            // (at your option) any later version.
-//                                                                              // This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of               // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.                                 //
-// You should have received a copy of the GNU General Public License            // along with this program; if not, write to the Free Software
+// This program is free software; you can redistribute it and/or modify         
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.                                                                             
+//                                                                              
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of               
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.                                 
+//
+// Yoe should have received a copy of the GNU General Public License            
+// along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
@@ -23,8 +29,7 @@
 #include <QWidget>
 #include <QPaintEvent>
 
-namespace gnash
-{
+namespace gnash {
 
 /// DrawingWidget implementation
 DrawingWidget::DrawingWidget(Kde4Gui& gui)
@@ -38,13 +43,11 @@ DrawingWidget::paintEvent(QPaintEvent *event)
     _gui.renderWidget(event->rect());
 }
 
-
 void
 DrawingWidget::timerEvent(QTimerEvent*)
 {
     Gui::advance_movie(&_gui);
 }
-
 
 void
 DrawingWidget::mouseMoveEvent(QMouseEvent *event)
@@ -53,13 +56,11 @@ DrawingWidget::mouseMoveEvent(QMouseEvent *event)
     _gui.notifyMouseMove(position.x(), position.y());
 }
 
-
 void
 DrawingWidget::contextMenuEvent(QContextMenuEvent* event)
 {
     _gui.popupMenu(event->globalPos());
 }
-
 
 void
 DrawingWidget::mousePressEvent(QMouseEvent* /* event */)
@@ -67,13 +68,11 @@ DrawingWidget::mousePressEvent(QMouseEvent* /* event */)
     _gui.notifyMouseClick(true);
 }
 
-
 void
 DrawingWidget::mouseReleaseEvent(QMouseEvent* /* event */)
 {
     _gui.notifyMouseClick(false);
 }
-
 
 void
 DrawingWidget::keyPressEvent(QKeyEvent *event)
@@ -87,7 +86,6 @@ DrawingWidget::keyReleaseEvent(QKeyEvent *event)
     _gui.handleKeyEvent(event, false);
 }
 
-
 void
 DrawingWidget::resizeEvent(QResizeEvent *event)
 {
@@ -95,13 +93,11 @@ DrawingWidget::resizeEvent(QResizeEvent *event)
     update();
 }
 
-
 void
 DrawingWidget::properties()
 {
     _gui.showProperties();
 }
-
 
 void
 DrawingWidget::preferences()
@@ -109,13 +105,11 @@ DrawingWidget::preferences()
     _gui.showPreferences();
 }
 
-
 void
 DrawingWidget::play()
 {
     _gui.play();
 }
-
 
 void
 DrawingWidget::pause()
@@ -123,21 +117,17 @@ DrawingWidget::pause()
     _gui.pause();
 }
 
-
 void
 DrawingWidget::restart()
 {
     _gui.restart();
 }
 
-
 void
 DrawingWidget::stop()
 {
     _gui.stop();
 }                                                             
-
-
 
 void
 DrawingWidget::refresh()
