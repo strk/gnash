d022137a18f9443b5d80f486656b51645725a4f6
Benjamin Wolsey <bwy@benjaminwolsey.de>
2011-05-26 10:09:17 +0200

    Use legal identifier.

--- libmedia/SoundInfo.h
+++ libmedia/SoundInfo.h
@@ -15,12 +15,8 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-// 
-//
-
-
-#ifndef __SOUNDINFO_H__
-#define __SOUNDINFO_H__
+#ifndef GNASH_LIBMEDIA_SOUNDINFO_H
+#define GNASH_LIBMEDIA_SOUNDINFO_H
 
 #include "MediaParser.h" // for audioCodecType enum and AudioInfo
 
