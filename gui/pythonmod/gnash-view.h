a7c6bcd0770d57315d0208f855e5dc5abbc507a9
Benjamin Wolsey <bwy@benjaminwolsey.de>
2011-07-04 08:34:34 +0200

    Use C comments in C.

--- gui/pythonmod/gnash-view.h
+++ gui/pythonmod/gnash-view.h
@@ -1,21 +1,21 @@
-// gnash-view.h: Gtk view widget for gnash
-// 
-//   Copyright (C) 2009, 2010, 2011 Free Software Foundation, Inc.
-// 
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-//
+/* gnash-view.h: Gtk view widget for gnash
+** 
+**   Copyright (C) 2009, 2010, 2011 Free Software Foundation, Inc.
+** 
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 3 of the License, or
+** (at your option) any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 #ifndef __GNASH_VIEW_H__
 #define __GNASH_VIEW_H__
