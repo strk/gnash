29aa95ca3ed88d5e68fd57d9ac53ebf985040804
Sandro Santilli <strk@keybit.net>
2011-07-12 23:31:16 +0200

    I guess you need ConstantPool class files too...

---
+++ libcore/ConstantPool.h
@@ -0,0 +1,52 @@
+// ConstantPool.h: a parsed ConstantPool 
+// 
+//   Copyright (C) 2011 Free Software Foundation, Inc
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+#ifndef GNASH_CONSTANTPOOL_H
+#define GNASH_CONSTANTPOOL_H
+
+#include <vector>
+#include <iosfwd>
+
+namespace gnash {
+
+class VM;
+
+/// An indexed list of strings (must match the definition in action_buffer.h)
+typedef std::vector<const char*> ConstantPool;
+
+std::ostream& operator<<(std::ostream& os, const ConstantPool& p);
+
+class PoolGuard
+{
+public:
+
+	// @param pool : ConstantPool to set temporarely
+	PoolGuard(VM& vm, const ConstantPool* pool);
+	~PoolGuard();
+
+private:
+
+    VM& _vm;
+    const ConstantPool* _from;
+
+};
+
+} // namespace gnash
+
+
+#endif 
