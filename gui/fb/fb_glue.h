069fc55e4a4cdf18aa09f2d97874d7b6d24c8b8d
Rob Savoye <rob@welcomehome.org>
2011-12-04 17:30:11 -0700

    fix copyright date

--- gui/fb/fb_glue.h
+++ gui/fb/fb_glue.h
@@ -1,5 +1,5 @@
 //
-//   Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Free Software
+//   Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011 Free Software
 //   Foundation, Inc
 //
 // This program is free software; you can redistribute it and/or modify
