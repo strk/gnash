ce522eb697570a4f05d45db649ec52c58d1f97cb
Rob Savoye <rob@welcomehome.org>
2012-01-16 18:31:28 -0700

    update copyright date

--- testsuite/dejagnu.h
+++ testsuite/dejagnu.h
@@ -1,6 +1,6 @@
 /* DejaGnu unit testing header.
    Copyright (C) 2000, 2001, 2002, 2004, 2006, 207, 2008, 2009,
-   2010, 2011 Free Software Foundation, Inc.
+   2010, 2011, 2012 Free Software Foundation, Inc.
 
 This file is part of DejaGnu.
 
--- testsuite/misc-ming.all/ming_utils.h
+++ testsuite/misc-ming.all/ming_utils.h
@@ -1,6 +1,6 @@
 /* 
- *   Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010,
- *   2011 Free Software Foundation, Inc.
+ *   Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012
+ *   Free Software Foundation, Inc.
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -11,8 +11,8 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
+ * 
+ * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
