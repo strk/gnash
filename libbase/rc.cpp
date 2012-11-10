66bf53991385bd34576a36fc104b19a2001b6f61
Bastiaan Jacques <bastiaan@bjacques.org>
2012-11-10 23:39:56 +0100

    Permit compilation in C++11 mode.
    GCC in C++11 mode understands quotes followed immediately by
    an expression to be an invocation of operator", so this is adding
    some spaces to prevent that behaviour.

--- libbase/rc.cpp
+++ libbase/rc.cpp
@@ -83,7 +83,7 @@ RcInitFile::RcInitFile()
         DEFAULT_FLASH_REV_NUMBER ",0"),
     // An empty string leaves detection to VM.cpp:
     _flashSystemOS(""),
-    _flashSystemManufacturer("Gnash "DEFAULT_FLASH_SYSTEM_OS),
+    _flashSystemManufacturer("Gnash " DEFAULT_FLASH_SYSTEM_OS),
     _actionDump(false),
     _parserDump(false),
     _verboseASCodingErrors(false),
