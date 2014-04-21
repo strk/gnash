3262b459bfd8a438f38b8eba3d275f34cdcf6185
Markus Gothe <nietzsche@lysator.liu.se>
2014-04-21 22:32:45 +0200

    std:setlocale went cocobahia with intl and gettext. Only 'setlocale' worked though...

--- gui/gnash.cpp
+++ gui/gnash.cpp
@@ -1,7 +1,7 @@
 // gnash.cpp:  Main routine for top-level SWF player, for Gnash.
 // 
 //   Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010,
-//   2011 Free Software Foundation, Inc
+//   2011, 2014 Free Software Foundation, Inc
 // 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -100,7 +100,7 @@ main(int argc, char *argv[])
 
     // Enable native language support, i.e. internationalization
 #ifdef ENABLE_NLS
-    std::setlocale (LC_ALL, "");
+    setlocale (LC_ALL, "");
     bindtextdomain (PACKAGE, LOCALEDIR);
     textdomain (PACKAGE);
 #endif
