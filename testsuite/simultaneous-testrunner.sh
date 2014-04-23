6d87a60ae886280d40c1797431d8b706c03153ee
Gabriele Giacone <1o5g4r8o@gmail.com>
2014-04-23 20:35:13 +0200

    Make LCTestRunner output while it runs to understand timing better.
    Analyse logs once they both finished. Fix indentation.

--- testsuite/simultaneous-testrunner.sh
+++ testsuite/simultaneous-testrunner.sh
@@ -18,19 +18,19 @@
 # 
 
 while getopts c:C: name; do
-	case $name in
-		c) endtagpat="$OPTARG" ;;
-		C) endtagpat="$OPTARG"; endtagexp=X ;;
-		?)
-		   {
-		   echo "Usage: $0 [-r <runs>] [-f <advances>] [-c <string>]  <swf> ..." 
-		   echo "   -c <pattern>    : verify that the test ends with a trace "
-           echo "                     matching <pattern>, or print a failure" 
-		   echo "   -C <pattern>    : same as -c <pattern> but a failure is "
-           echo "                     expected" 
-		   } >&2
-		   exit 1;;
-	esac
+  case $name in
+    c) endtagpat="$OPTARG" ;;
+    C) endtagpat="$OPTARG"; endtagexp=X ;;
+    ?)
+      {
+      echo "Usage: $0 [-r <runs>] [-f <advances>] [-c <string>]  <swf> ..."
+      echo "   -c <pattern>    : verify that the test ends with a trace "
+      echo "                     matching <pattern>, or print a failure"
+      echo "   -C <pattern>    : same as -c <pattern> but a failure is "
+      echo "                     expected"
+      } >&2
+      exit 1;;
+  esac
 done
 shift $(($OPTIND - 1))
 
@@ -47,7 +47,7 @@ echo
 
 echo "# Environment"
 env | grep GNASH | while read REPLY; do
-	echo "export ${REPLY}"
+  echo "export ${REPLY}"
 done
 
 timeout=40
@@ -55,32 +55,22 @@ cat << EOF
 
 outlog1=${top_builddir}/testoutlog.\$$
 outlog2=${top_builddir}/testoutlog2.\$$
-(
-    echo "Running first process"
-    ${top_builddir}/gui/gnash -v -r0 ${t1} -t ${timeout} > \${outlog1}
-    cat \${outlog1}
-	if test "x${endtagpat}" != x; then
-		lasttrace=\`grep TRACE \${outlog1} | tail -1 | sed 's/.*TRACE: //'\`
-		if ! expr "\${lasttrace}" : '${endtagpat}' > /dev/null; then
-			echo "${endtagexp}FAILED: consistency check: last trace from run of test \${t} (\${lasttrace}) doesn't match pattern (${endtagpat})"
-		else
-			echo "${endtagexp}PASSED: consistency check: last trace from run of test \${t} (\${lasttrace}) matches pattern (${endtagpat})"
-		fi
-	fi
-    rm \${outlog1}
-) &
-(
-    echo "Running second process"
-    ${top_builddir}/gui/gnash -v -r0 ${t2} -t ${timeout} > \${outlog2}
-    cat \${outlog2}
-	if test "x${endtagpat}" != x; then
-		lasttrace=\`grep TRACE \${outlog2} | tail -1 | sed 's/.*TRACE: //'\`
-		if ! expr "\${lasttrace}" : '${endtagpat}' > /dev/null; then
-			echo "${endtagexp}FAILED: consistency check: last trace from run of test \${t} (\${lasttrace}) doesn't match pattern (${endtagpat})"
-		else
-			echo "${endtagexp}PASSED: consistency check: last trace from run of test \${t} (\${lasttrace}) matches pattern (${endtagpat})"
-		fi
-	fi
-    rm \${outlog2}
-)
+
+echo "Running first process"
+${top_builddir}/gui/gnash -v -r0 ${t1} -t ${timeout} | tee \${outlog1} &
+echo "Running second process"
+${top_builddir}/gui/gnash -v -r0 ${t2} -t ${timeout} | tee \${outlog2} &
+wait
+
+for log in \${outlog1} \${outlog2}; do
+  if test "x${endtagpat}" != x; then
+    lasttrace=\`grep TRACE \$log | tail -1 | sed 's/.*TRACE: //'\`
+    if ! expr "\${lasttrace}" : '${endtagpat}' > /dev/null; then
+      echo "${endtagexp}FAILED: consistency check: last trace from run of test \${t} (\${lasttrace}) doesn't match pattern (${endtagpat})"
+    else
+      echo "${endtagexp}PASSED: consistency check: last trace from run of test \${t} (\${lasttrace}) matches pattern (${endtagpat})"
+    fi
+  fi
+  rm \$log
+done
 EOF
