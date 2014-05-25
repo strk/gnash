54940130147e9471f3a2fdc92fb36eb9a40570da
Bastiaan Jacques <bastiaan@bjacques.org>
2014-05-25 20:40:50 +0200

    Replace the hodgepodge of formatting with a single style.

--- libcore/LoadVariablesThread.cpp
+++ libcore/LoadVariablesThread.cpp
@@ -1,4 +1,4 @@
-// 
+//
 //   Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012
 //   Free Software Foundation, Inc
 //
@@ -33,110 +33,105 @@ namespace gnash {
 // static
 LoadVariablesThread::ValuesMap
 LoadVariablesThread::completeLoad(IOChannel* varstream,
-   std::atomic<bool>& canceled)
+                                  std::atomic<bool>& canceled)
 {
     std::unique_ptr<IOChannel> stream(varstream);
 #ifdef DEBUG_LOAD_VARIABLES
     log_debug("completeLoad called");
 #endif
-        ValuesMap parseResult;
+    ValuesMap parseResult;
 
-	int bytesLoaded = 0;
-	int bytesTotal = stream->size();
+    int bytesLoaded = 0;
+    int bytesTotal = stream->size();
 
-	std::string toparse;
+    std::string toparse;
 
-	const size_t chunkSize = 1024;
-	std::unique_ptr<char[]> buf(new char[chunkSize]);
-	unsigned int parsedLines = 0;
-	// TODO: use read_string ?
-	while ( size_t bytesRead = stream->read(buf.get(), chunkSize) )
-	{
+    const size_t chunkSize = 1024;
+    std::unique_ptr<char[]> buf(new char[chunkSize]);
+    unsigned int parsedLines = 0;
+    // TODO: use read_string ?
+    while ( size_t bytesRead = stream->read(buf.get(), chunkSize) ) {
 #ifdef DEBUG_LOAD_VARIABLES
-            log_debug("Read %u bytes", bytesRead);
+        log_debug("Read %u bytes", bytesRead);
 #endif
 
-		if ( bytesLoaded )
-		{
-			std::string chunk(buf.get(), bytesRead);
-			toparse += chunk;
-		}
-		else
-		{
-			size_t dataSize = bytesRead;
-			utf8::TextEncoding encoding;
-			char* ptr = utf8::stripBOM(buf.get(), dataSize,
-					encoding);
-			if ( encoding != utf8::encUTF8 &&
-			     encoding != utf8::encUNSPECIFIED )
-			{
-                  log_unimpl(_("%s to UTF8 conversion in "
-					    "MovieClip.loadVariables "
-                                         "input parsing"),
-					    utf8::textEncodingName(encoding));
-			}
-			std::string chunk(ptr, dataSize);
-			toparse += chunk;
-		}
+        if ( bytesLoaded ) {
+            std::string chunk(buf.get(), bytesRead);
+            toparse += chunk;
+        } else {
+            size_t dataSize = bytesRead;
+            utf8::TextEncoding encoding;
+            char* ptr = utf8::stripBOM(buf.get(), dataSize,
+                                       encoding);
+            if ( encoding != utf8::encUTF8 &&
+                    encoding != utf8::encUNSPECIFIED ) {
+                log_unimpl(_("%s to UTF8 conversion in "
+                             "MovieClip.loadVariables "
+                             "input parsing"),
+                           utf8::textEncodingName(encoding));
+            }
+            std::string chunk(ptr, dataSize);
+            toparse += chunk;
+        }
 
 #ifdef DEBUG_LOAD_VARIABLES
-		log_debug("toparse: %s", toparse);
+        log_debug("toparse: %s", toparse);
 #endif
 
-		// parse remainder
-		size_t lastamp = toparse.rfind('&');
-		if ( lastamp != std::string::npos )
-		{
-			std::string parseable = toparse.substr(0, lastamp);
+        // parse remainder
+        size_t lastamp = toparse.rfind('&');
+        if ( lastamp != std::string::npos ) {
+            std::string parseable = toparse.substr(0, lastamp);
 #ifdef DEBUG_LOAD_VARIABLES
-			log_debug("parseable: %s", parseable);
+            log_debug("parseable: %s", parseable);
 #endif
-                        URL::parse_querystring(toparse, parseResult);
-			toparse = toparse.substr(lastamp+1);
-#ifdef DEBUG_LOAD_VARIABLES
-			log_debug("toparse nextline: %s", toparse);
-#endif
-			++parsedLines;
-		}
-
-		bytesLoaded += bytesRead;
-
-		// eof, get out !
-		if ( stream->eof() ) break;
-
-		if ( canceled.load() ) {
-                    log_debug("Cancelling LoadVariables download thread...");
-			stream.reset();
-			return parseResult;
-		}
-	}
-
-	if ( ! toparse.empty() ) {
             URL::parse_querystring(toparse, parseResult);
-	}
+            toparse = toparse.substr(lastamp+1);
+#ifdef DEBUG_LOAD_VARIABLES
+            log_debug("toparse nextline: %s", toparse);
+#endif
+            ++parsedLines;
+        }
 
-	try {
-		stream->go_to_end();
-	}
-        catch (IOException& ex) {
+        bytesLoaded += bytesRead;
+
+        // eof, get out !
+        if ( stream->eof() ) {
+            break;
+        }
+
+        if ( canceled.load() ) {
+            log_debug("Cancelling LoadVariables download thread...");
+            stream.reset();
+            return parseResult;
+        }
+    }
+
+    if ( ! toparse.empty() ) {
+        URL::parse_querystring(toparse, parseResult);
+    }
+
+    try {
+        stream->go_to_end();
+    } catch (IOException& ex) {
         log_error(_("Stream couldn't seek to end: %s"), ex.what());
-	}
-	
-        bytesLoaded = stream->tell();
-	if ( bytesTotal !=  bytesLoaded ) {
-            log_error(_("Size of 'variables' stream advertised to be %d bytes,"
-                          " but turned out to be %d bytes."),
-			bytesTotal, bytesLoaded);
-	}
+    }
 
-	//dispatchLoadEvent();
+    bytesLoaded = stream->tell();
+    if ( bytesTotal !=  bytesLoaded ) {
+        log_error(_("Size of 'variables' stream advertised to be %d bytes,"
+                    " but turned out to be %d bytes."),
+                  bytesTotal, bytesLoaded);
+    }
 
-        return parseResult;
+    //dispatchLoadEvent();
+
+    return parseResult;
 }
 
 LoadVariablesThread::LoadVariablesThread(const StreamProvider& sp,
         const URL& url, const std::string& postdata)
-	: _canceled(false)
+    : _canceled(false)
 {
     startThread(sp.getStream(url, postdata));
 }
@@ -155,18 +150,17 @@ LoadVariablesThread::startThread(std::unique_ptr<IOChannel> stream)
         throw NetworkException();
     }
 
-    _vals = std::async(std::launch::async, completeLoad, stream.release(), 
-        std::ref(_canceled));
+    _vals = std::async(std::launch::async, completeLoad, stream.release(),
+                       std::ref(_canceled));
 }
 
 LoadVariablesThread::~LoadVariablesThread()
 {
-	if ( _vals.valid() )
-	{
-		_canceled = true;
-		_vals.wait();
-	}
+    if ( _vals.valid() ) {
+        _canceled = true;
+        _vals.wait();
+    }
 }
 
 
-} // namespace gnash 
+} // namespace gnash
