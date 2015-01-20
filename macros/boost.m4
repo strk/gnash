strk/gnash

commit 8603e08253b3c60aca5c775ccf1cd576535c7cf0

Make boost iostreams linked by npapi plugin only. Thanks bjacques.

macros/boost.m4
@@ -27,7 +27,8 @@ AC_DEFUN([GNASH_PATH_BOOST],
   gnash_boost_libdir=""
   missing_headers=""
   missing_libs=""
-  cygnal_missing_libs=""
+  missing_plugin_libs=""
+  missing_cygnal_libs=""
   gcc_version=""
   dirname=""
   libname=""
@@ -36,12 +37,13 @@ AC_DEFUN([GNASH_PATH_BOOST],
   boost_headers="detail/lightweight_mutex.hpp thread/thread.hpp multi_index_container.hpp multi_index/key_extractors.hpp thread/mutex.hpp program_options/options_description.hpp iostreams/stream.hpp"
   dnl this is a list of *required* libraries. If any of these are missing, this
   dnl test will return a failure, and Gnash won't build.
-  boost_libs="thread program_options iostreams system"
+  boost_libs="thread program_options system"
+  boost_plugin_libs="iostreams"
 
   dnl this is a list of *recommended* libraries. If any of these are missing,
   dnl this test will return a warning, and Gnash will build, but testing
   dnl won't work.
-  cygnal_boost_libs="serialization date_time"
+  boost_cygnal_libs="serialization date_time"
 
   dnl this is the default list for paths to search. This gets
   dnl redefined if --with-boost-incl= is specified.
@@ -154,19 +156,28 @@ AC_DEFUN([GNASH_PATH_BOOST],
         fi
       done
     done
-    for j in ${cygnal_boost_libs}; do
+    for j in ${boost_plugin_libs}; do
+      dirs="`ls -dr ${dirname}/libboost_${j}*.${shlibext} ${dirname}/libboost_${j}*.a 2>/dev/null`"
+      if test -n "${dirs}"; then
+          libname="`echo ${dirs} | sed -e 's:^.*/lib::' -e "s:\.${shlibext}::" -e "s:\.a::"`"
+        ac_cv_path_boost_plugin_lib="${ac_cv_path_boost_plugin_lib} -l${libname}"
+      else
+        missing_plugin_libs="${missing_plugin_libs} $j"
+      fi
+    done
+    for j in ${boost_cygnal_libs}; do
       dirs="`ls -dr ${dirname}/libboost_${j}*.${shlibext} ${dirname}/libboost_${j}*.a 2>/dev/null`"
       if test -n "${dirs}"; then
           libname="`echo ${dirs} | sed -e 's:^.*/lib::' -e "s:\.${shlibext}::" -e "s:\.a::"`"
         ac_cv_path_boost_cygnal_lib="${ac_cv_path_boost_cygnal_lib} -l${libname}"
       else
-        cygnal_missing_libs="${cygnal_missing_libs} $j"
+        missing_cygnal_libs="${missing_cygnal_libs} $j"
       fi
     done
   fi
 
   if test x"${missing_libs}" != x ; then
-    AC_MSG_WARN([Libraries ${missing_libs} ${cygnal_missing_libs} aren't installed ])
+    AC_MSG_WARN([Libraries ${missing_libs} ${missing_plugin_libs} ${missing_cygnal_libs} aren't installed ])
   fi
   AC_MSG_RESULT(${ac_cv_path_boost_lib})
 
@@ -176,6 +187,10 @@ AC_DEFUN([GNASH_PATH_BOOST],
 
   if test x"${ac_cv_path_boost_lib}" != x; then
     BOOST_LIBS="$ac_cv_path_boost_lib"
+  fi
+
+  if test x"${ac_cv_path_boost_plugin_lib}" != x; then
+    BOOST_PLUGIN_LIBS="$ac_cv_path_boost_plugin_lib"
   fi
 
   if test x"${ac_cv_path_boost_cygnal_lib}" != x; then
@@ -188,6 +203,7 @@ AC_DEFUN([GNASH_PATH_BOOST],
 
   AC_SUBST(BOOST_CFLAGS)
   AC_SUBST(BOOST_LIBS)
+  AC_SUBST(BOOST_PLUGIN_LIBS)
   AC_SUBST(BOOST_CYGNAL_LIBS)
 
   dnl This isn't right: you don't need boot date-time installed unless u build

plugin/npapi/Makefile.am
@@ -78,7 +78,7 @@ libgnashplugin_la_SOURCES  = plugin.cpp \
 
 libgnashplugin_la_LIBADD   = \
 	$(GLIB_LIBS) \
-	-lboost_iostreams \
+	$(BOOST_PLUGIN_LIBS) \
 	$(NULL)
 
 # Scriptable plugin support

commit 9957d022bfa63e87a2d5dd9719c2d0c071d977d6

Do not allow building a broken npapi plugin
The plugin relies on boost iostream being installed, with this commit
the presence is checked. It also fixes checks for cygnal-required libs
which was broken by commit  8603e08253b3c60aca5c775ccf1cd576535c7cf0

configure.ac
@@ -2926,8 +2926,8 @@ AC_SUBST(HWACCEL_CONFIG)
 
 dnl check for missing libraries and disable them.
 if test x"$BOOST_LIBS" != x; then
-  if test x"${cygnal_missing_libs}" != x; then
-    for i in ${cygnal_missing_libs}; do
+  if test x"${missing_cygnal_libs}" != x; then
+    for i in ${missing_cygnal_libs}; do
       if test $i = serialization; then
         AC_DEFINE([BOOST_MULTI_INDEX_DISABLE_SERIALIZATION], ["1"], [if the library is missing, don't use it.])
       fi
@@ -3819,13 +3819,25 @@ if test x"$BOOST_LIBS" != x; then
   fi
 
   if test x"${cygnal}" = x"yes"; then
-    if test x"${cygnal_missing_libs}" != x; then
-      for i in ${cygnal_missing_libs}; do
+    if test x"${missing_cygnal_libs}" != x; then
+      for i in ${missing_cygnal_libs}; do
         PKG_ERR([No Boost $i package installed])
         PKG_SUGGEST([Install it from http://www.boost.org])
         debtmppkg="libboost-`echo ${i} | sed 's/_/-/g'`-dev"
         DEB_INSTALL([$debtmppkg])
         PKG_ALTERNATIVE([or configure with --disable-cygnal])
+      done
+    fi
+  fi
+
+  if test x"${npapi}" = x"yes"; then
+    if test x"${missing_plugin_libs}" != x; then
+      for i in ${missing_plugin_libs}; do
+        PKG_ERR([No Boost $i package installed])
+        PKG_SUGGEST([Install it from http://www.boost.org])
+        debtmppkg="libboost-`echo ${i} | sed 's/_/-/g'`-dev"
+        DEB_INSTALL([$debtmppkg])
+        PKG_ALTERNATIVE([or configure with --disable-npapi])
       done
     fi
   fi

macros/boost.m4
@@ -168,7 +168,7 @@ AC_DEFUN([GNASH_PATH_BOOST],
     done
   fi
 
-  if test x"${missing_libs}" != x ; then
+  if test x"${missing_libs}" != x -o x"${missing_plugin_libs}" != x -o x"${missing_cygnal_libs}" != x; then
     AC_MSG_WARN([Libraries ${missing_libs} ${missing_plugin_libs} ${missing_cygnal_libs} aren't installed ])
   fi
   AC_MSG_RESULT(${ac_cv_path_boost_lib})