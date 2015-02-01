4a9aa741130b8b482e066abc1b80240605408f9b
Bastiaan Jacques <bastiaan@bjacques.org>
2015-02-01 19:31:43 +0100

    Savannah #43198. Work around boost 1.56+ incompatibility.
    A breaking change was introduced in boost 1.56 [1]: the implicit
    conversion to bool was removed. Instead, an explicit operator
    bool was provided. The getters were also changed, so we cannot
    use them and retain compatibility with older boost versions.
    
    By putting the would-be boolean expression in a conditional, we
    now invoke the explicit operator bool (also known as contextual
    conversion to bool) for boost >= 1.56 and the implicit conversion
    to bool for < 1.56.
    
    [1] https://svn.boost.org/trac/boost/ticket/4227

--- libcore/DragState.h
+++ libcore/DragState.h
@@ -62,7 +62,7 @@ public:
     std::int32_t yOffset() const { return _yoffset; }
 
     bool hasBounds() const {
-        return (_bounds);
+        return _bounds ? true : false;
     }
 
     /// \brief
