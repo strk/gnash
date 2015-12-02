1d8449214a9bb609381d85724dae5741ee814849
Sandro Santilli <strk@keybit.net>
2015-12-02 22:32:40 +0100

    Do not force SDL initialization when using dump-gnash

--- gui/Player.cpp
+++ gui/Player.cpp
@@ -275,6 +275,8 @@ void
 Player::init_sound()
 {
 
+#ifndef GUI_DUMP
+    // Dump gui will initialize the sound handler itself
     if (_doSound) {
         try {
 #ifdef SOUND_SDL
@@ -296,6 +298,7 @@ Player::init_sound()
                 " Will continue without sound."), ex.what());
         }
     }
+#endif // ndef GUI_DUMP
 }
 
 void
